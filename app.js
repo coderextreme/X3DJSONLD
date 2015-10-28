f0a87f79230f6152ae7c5d7d96143567d385e838
carlsonsolutiondesign <john@carlsonsolutiondsign.com>
2015-10-28 13:00:29 -0500

    Added link and configuration

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var config = require("./config");
 var port = process.env.PORT || 3000;
+var path = require('path');
+
+require("fs").symlink(
+path.resolve(config.examples),
+path.resolve(__dirname + "/examples"),
+'junction',
+ function (err) {
+        if (err) {
+                console.log( err.code === 'EEXIST' ? "Go to the link above!\n" : err);
+        }
+  }
+);
+
 app.use(express.static(__dirname));
 
 http.listen(port, function () {
