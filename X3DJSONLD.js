ed8c387e91a16057c7fff626e551a833956d5df3
carlsonsolutiondesign <john@carlsonsolutiondsign.com>
2015-11-16 05:23:36 -0600

    Move routes after other keys

--- X3DJSONLD.js
+++ X3DJSONLD.js
@@ -13,6 +13,26 @@ function ConvertChildren(object, indent, parentkey, element) {
 	}
 }
 
+function ConvertAttribute(key, object, indent, element) {
+	if (typeof object[key] === 'object') {
+		if (key.substr(0,1) === '@') {
+			ConvertToX3DOM(object[key], indent, key, element);
+		} else if (key.substr(0,1) === '-') {
+			ConvertChildren(object[key], indent, key, element);
+		} else if (key.substr(0,1) === '#') {
+			for (var comment in object[key]) {
+				var child = document.createComment(object[key][comment]);
+				element.appendChild(child);
+			}
+		} else {
+			var createKey = key;
+			var child = document.createElement(createKey);
+			ConvertToX3DOM(object[key], indent, key, child);
+			element.appendChild(child);
+		}
+	}
+}
+
 function ConvertToX3DOM(object, indent, parentkey, element) {
 	var key;
 	var localArray = [];
@@ -38,20 +58,8 @@ function ConvertToX3DOM(object, indent, parentkey, element) {
 				console.log("Unknown type found in array "+typeof object[key]);
 			}
 		} else if (typeof object[key] === 'object') {
-			if (key.substr(0,1) === '@') {
-				ConvertToX3DOM(object[key], indent, key, element);
-			} else if (key.substr(0,1) === '-') {
-				ConvertChildren(object[key], indent, key, element);
-			} else if (key.substr(0,1) === '#') {
-				for (var comment in object[key]) {
-					var child = document.createComment(object[key][comment]);
-					element.appendChild(child);
-				}
-			} else {
-				var createKey = key;
-				var child = document.createElement(createKey);
-				ConvertToX3DOM(object[key], indent, key, child);
-				element.appendChild(child);
+			if (key !== 'ROUTE') {
+				ConvertAttribute(key, object, indent, element)
 			}
 		} else if (typeof object[key] === 'number') {
 			element.setAttribute(key.substr(1),object[key]);
@@ -65,6 +73,7 @@ function ConvertToX3DOM(object, indent, parentkey, element) {
 			console.log("Unknown type found in object "+typeof object[key]);
 		}
 	}
+	ConvertAttribute('ROUTE', object, indent, element)
 	if (isArray) {
 		if (parentkey.substr(0,1) === '@') {
 			if (arrayOfStrings) {
