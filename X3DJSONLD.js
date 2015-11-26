9acf7c97e40f7e51b85dd6a839262b62cdc9bb46
carlsonsolutiondesign <john@carlsonsolutiondsign.com>
2015-11-25 19:02:44 -0600

    Major updates to loader, including XML output

--- X3DJSONLD.js
+++ X3DJSONLD.js
@@ -4,51 +4,112 @@
 
 // Load X3D JSON into web page
 
-function ConvertFieldsRoutes(object, indent, parentkey, element, path) {
-	var key;
-	for (key in object) {  // for each field
-		if (typeof object[key] === 'object') {
-			var child = document.createElement(parentkey);
-			ConvertToX3DOM(object[key], indent, key, child, path);
-			element.appendChild(child);
-		}
-	}
+function printElement(el, xml) {
+        var child;
+        var key;
+        var attrs = "";
+        if (typeof el === 'string') {
+                xml.push(el);
+                return;
+        }
+        if (el.key) {
+                key = el.key;
+        }
+	for (var a in el.attributes) {
+                attrs += " "+a+"='"+el.attributes[a]+"'";
+        }
+        if (el.children && typeof key !== 'undefined' && attrs !== '' ) {
+                xml.push("<"+key+attrs+">");
+        } else if (el.children && typeof key !== 'undefined' ) {
+                xml.push("<"+key+">");
+        } else if (typeof key !== 'undefined' && attrs !== '' ) {
+                xml.push("<"+key+attrs+"/>");
+        } else if (typeof key !== 'undefined' ) {
+                xml.push("<"+key+"/>");
+        }
+        for (child in el) {
+                if (child === "children") {
+                        printElement(el[child], xml);
+                } else {
+                        if (isNaN(parseInt(child))) {
+                        } else {
+                                printElement(el[child], xml);
+                        }
+                }
+        }
+        if (el.children && typeof key !== 'undefined') {
+                xml.push("</"+key+">");
+        }
 }
 
-function ConvertChildren(object, indent, parentkey, element, path) {
+function elementSetAttribute(element, key, value, attributes) {
+	element.setAttribute(key, value);
+	attributes[key] = value;
+}
+
+function ConvertChildren(parentkey, object, element, path) {
 	var key;
+	var children = [];
 	for (key in object) {
 		if (typeof object[key] === 'object') {
-			ConvertToX3DOM(object[key], indent, key, element, path);
+			var el = ConvertToX3DOM(object[key], key, element, path);
+			children.push(el);
 		}
 	}
+	return { attributes: {},  children: children };
 }
 
-function ConvertAttribute(key, object, indent, element, path) {
+function ConvertObject(key, object, element, path) {
+	var children = [];
+	var attributes = {};
 	if (object !== null && typeof object[key] === 'object') {
 		if (key.substr(0,1) === '@') {
-			ConvertToX3DOM(object[key], indent, key, element, path);
+			var el = ConvertToX3DOM(object[key], key, element, path);
+			for (var a in el.attributes) {
+				attributes[a] = el.attributes[a];
+			}
 		} else if (key.substr(0,1) === '-') {
-			ConvertChildren(object[key], indent, key, element, path);
+			var el = ConvertChildren(key, object[key], element, path);
+			children.push.apply(children, el.children);
 		} else if (key === '#comment') {
+			// a comment within a script
 			var child = document.createComment(object[key].join("\n"));
+			children.push("<!--"+object[key].join("\n")+"-->");
 			element.appendChild(child);
 		} else if (key === '#sourceText') {
 			var child = document.createTextNode(object[key].join("\n"));
+			children.push(object[key].join("\n"));
 			element.appendChild(child);
 		} else {
-			var child = document.createElement(key);
-			ConvertToX3DOM(object[key], indent, key, child, path);
-			element.appendChild(child);
+			if (key === 'ROUTE' || key === 'field' || key === 'meta') {
+				for (var childkey in object[key]) {  // for each field
+					if (typeof object[key][childkey] === 'object') {
+						var child = document.createElement(key);
+						var el = ConvertToX3DOM(object[key][childkey], childkey, child, path);
+						el.key = key;
+						children.push(el);
+						element.appendChild(child);
+					}
+				}
+			} else {
+				var child = document.createElement(key);
+				var el = ConvertToX3DOM(object[key], key, child, path);
+				el.key = key;
+				children.push(el);
+				element.appendChild(child);
+			}
 		}
 	}
+	return { attributes: attributes, children: children };
 }
 
-function ConvertToX3DOM(object, indent, parentkey, element, path) {
+function ConvertToX3DOM(object, parentkey, element, path) {
 	var key;
 	var localArray = [];
 	var isArray = false;
 	var arrayOfStrings = false;
+	var attributes = {};
+	var children = [];
 	for (key in object) {
 		if (isNaN(parseInt(key))) {
 			isArray = false;
@@ -64,29 +125,32 @@ function ConvertToX3DOM(object, indent, parentkey, element, path) {
 			} else if (typeof object[key] === 'boolean') {
 				localArray.push(object[key]);
 			} else if (typeof object[key] === 'object') {
-				ConvertToX3DOM(object[key], indent, key, element, path);
+				children.push(ConvertToX3DOM(object[key], key, element, path));
 			} else {
 				console.log("Unknown type found in array "+typeof object[key]);
 			}
 		} else if (typeof object[key] === 'object') {
-			if (key === 'field' || key === 'ROUTE') {
-				ConvertFieldsRoutes(object[key], indent, key, element, path);
-			} else {
-				ConvertAttribute(key, object, indent, element, path)
+			var el = ConvertObject(key, object, element, path);
+			for (var a in el.attributes) {
+				attributes[a] = el.attributes[a];
 			}
+			children.push(el);
 		} else if (typeof object[key] === 'number') {
-			element.setAttribute(key.substr(1),object[key]);
+			elementSetAttribute(element, key.substr(1),object[key], attributes);
 		} else if (typeof object[key] === 'string') {
 			if (key !== '#comment') {
-				element.setAttribute(key.substr(1),object[key]);
+				elementSetAttribute(element, key.substr(1),object[key], attributes);
+			} else {
+				var child = document.createComment(object[key]);
+				children.push("<!--"+object[key]+"-->");
+				element.appendChild(child);
 			}
 		} else if (typeof object[key] === 'boolean') {
-			element.setAttribute(key.substr(1),object[key]);
+			elementSetAttribute(element, key.substr(1),object[key], attributes);
 		} else {
 			console.log("Unknown type found in object "+typeof object[key]);
 		}
 	}
-	// ConvertAttribute('ROUTE', object, indent, element, path)
 	if (isArray) {
 		if (parentkey.substr(0,1) === '@') {
 			if (arrayOfStrings) {
@@ -97,73 +161,68 @@ function ConvertToX3DOM(object, indent, parentkey, element, path) {
 					// localArray = localArray[0].split(/" "/);
 					for (url in localArray) {
 						// localArray[url].replace(/"/g, '');
-						console.log('ORIGINAL', localArray[url]);
 						if (localArray[url].indexOf("http://") === 0
 						 || localArray[url].indexOf("https://") === 0) {
-							console.log('HTTP', localArray[url]);
 						} else if (localArray[url].indexOf("urn:web3d:media:textures/panoramas/") === 0) {
 							var ls = localArray[url].lastIndexOf("/");
 							if (ls > 0) {
 								localArray[url] = 'examples/Basic/UniversalMediaPanoramas/'+localArray[url].substring(ls+1);
 							}
-							console.log('URN', localArray[url]);
 
 						} else {
 							var pe = path.lastIndexOf('/');
 							var pc = path.substring(0, pe);
 							localArray[url] = pc+'/'+localArray[url];
-							console.log('NO HTTP', localArray[url]);
 						}
-/*
-						if (localArray[url].lastIndexOf('.x3d') === localArray[url].length-4
-						 || localArray[url].lastIndexOf('.wrl') === localArray[url].length-4) {
-							localArray[url] = localArray[url].substring(0, localArray[url].length-4)+".json";
-							var id = localArray[url].substring(localArray[url].lastIndexOf("/")+1, localArray[url].length-5);
-							element.setAttribute("id", id);
-							console.log("SUBSCENE", localArray[url]);
-							loadSubscene('#'+id, localArray[url], path);
-							break;
-						}
-*/
 							
                                        }
 					// if URL
 					console.log("Loading URL",'"'+localArray.join('" "')+'"');
-					element.setAttribute(parentkey.substr(1),'"'+localArray.join('" "')+'"');
+					elementSetAttribute(element, parentkey.substr(1),'"'+localArray.join('" "')+'"', attributes);
                                 } else {
 					// if string array
-					element.setAttribute(parentkey.substr(1),'"'+localArray.join('" "')+'"');
+					elementSetAttribute(element, parentkey.substr(1),'"'+localArray.join('" "')+'"', attributes);
 				}
 			} else {
 				// if non string array
-				element.setAttribute(parentkey.substr(1),localArray.join(" "));
+				elementSetAttribute(element, parentkey.substr(1),localArray.join(" "), attributes);
 			}
 		}
 		isArray = false;
 	}
-	return element;
+	return { attributes: attributes, children: children };
 }
 
-function loadX3DJS(selector, json, path) {
+function loadX3DJS(selector, json, path, xml) {
 	var element = document.querySelector(selector);
 	if (element === null) {
 		console.log("selector found nothing in document", selector);
 	} else {
-		ConvertToX3DOM(json, "", "", element, path);
-		x3dom.reload();
+		var el = ConvertToX3DOM(json, "", element, path);
+		xml = xml || [];
+		xml.push('<?xml version="1.0" encoding="UTF-8"?>');
+		xml.push('<!DOCTYPE X3D PUBLIC "ISO//Web3D//DTD X3D 3.3//EN" "http://www.web3d.org/specifications/x3d-3.3.dtd">');
+/*
+		console.log(el);
+		console.log(el.children[0]);
+		console.log(el.children[0].children[0]);
+		console.log(el.children[0].children[0].attributes);
+*/
+		el.children[0].children[0].attributes["id"] = "x3dele"; // for Cobweb
+		printElement(el, xml);
+		if (typeof x3dom !== 'undefined') {
+			x3dom.reload();
+		}
 	}
 }
-/*
-function loadSubscene(selector, url, path) {
+
+function loadX3DJSON(selector, url, callback) {
 	$.getJSON(url, function(json) {
-		loadX3DJS(selector, json["X3D"]["Scene"], path);
-	})
-	.fail(function(jqXHR, textStatus, errorThrown) { alert('getJSON request failed! ' + textStatus + ' ' + errorThrown); });
-}
-*/
-function loadX3DJSON(selector, url) {
-	$.getJSON(url, function(json) {
-		loadX3DJS(selector, json, url);
+		var xml = [];
+		loadX3DJS(selector, json, url, xml);
+		if (typeof callback === 'function') {
+			callback(xml);
+		}
 	})
 	.fail(function(jqXHR, textStatus, errorThrown) { alert('getJSON request failed! ' + textStatus + ' ' + errorThrown); });
 }
