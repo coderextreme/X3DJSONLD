0485f555c7ab5cdf357aee64d088b94c412620c8
carlsonsolutiondesign <john@carlsonsolutiondsign.com>
2015-12-04 16:53:44 -0600

    Added JSON to X3D converter

---
+++ json2x3d.js
@@ -0,0 +1,13 @@
+var loadX3DJS = require('./X3DJSONLD');
+var content = '';
+
+// read content into buffer
+process.stdin.resume();
+process.stdin.on('data', function(buf) { content += buf.toString(); });
+
+process.stdin.on('end', function() {
+	var json = JSON.parse(content);
+	var xml = [];
+	loadX3DJS(null, json, 'pp3s.json', xml);
+	console.log(xml.join(""));
+});
