4bf7a22ece41e2832f09edf832d16dad9b49f94e
carlsonsolutiondesign <john@carlsonsolutiondsign.com>
2015-12-04 16:49:17 -0600

    Updates to Script.js

--- Script.js
+++ Script.js
@@ -9,8 +9,10 @@ var instancecount = 0;
 
 function Package(package, name) {
 	this.initializers = [];
-	this.getters = [];
-	this.setters = [];
+	this.getters = {};
+	this.setters = {};
+	this.values = {};
+	this.types = {};
 	this.packages = {};
 	if (typeof package === 'undefined' || package.name === "") {
 		if (typeof name === 'undefined') {
@@ -70,43 +72,31 @@ function processPrototypes(object, clazz, package) {
 				instancecount += 1;
 			} else if (p.toLowerCase() === 'connect') {
 				processPrototypes(object[p], clazz, package);
-			} else if (p.toLowerCase() === 'field') {
-				var setters = [];
-				var getters = [];
+			} else if (p.toLowerCase() === 'route') {
+				processRoutes(object[p], clazz, package);
+			} else if (p.toLowerCase() === 'field' && object['@language'] !== 'GLSL') {
 				processFields(object[p], package);
 				clazz.push('var ' +  package.name +  ' = function(' +  package.initializers.join(', ') + ') {');
 					clazz.push('var that = this;');
-					if (setters.length > 0) {
-						clazz.push('this.setters = {};');
-					}
-					if (getters.length > 0) {
-						clazz.push('this.getters = {};');
-					}
-					for (var v in package.initializers) {
-						clazz.push('var that.' + package.initializers[v] + ' = '+ package.initializers[v] + ';');
+					clazz.push('this.setters = {};');
+					clazz.push('this.getters = {};');
+					for (var v in package.values) {
+						if (package.types[v].indexOf("MF") === 0) {
+							clazz.push('var that.' + v + ' = ['+ package.values[v] + '];');
+						} else {
+							clazz.push('var that.' + v + ' = '+ package.values[v] + ';');
+						}
 					
-					}
-					for (var v in package.setters) {
-						clazz.push('this.setters.' + package.setters[v] +  ' = function (value) { that.' +  package.setters[v] +  ' = value; };');
 					}
 					for (var v in package.getters) {
-						clazz.push('this.getters.' + package.getters[v] +  ' = function () { return that.' +  package.getters[v] +  '; };');
+						clazz.push('this.getters.' + v +  ' = function () { return that.' +  v +  '; };');
 					
 					}
+				processSource(object['#sourceText'], clazz, package);
 				clazz.push('}');
 				processPrototypes(object[p], clazz, package);
 			} else if (p.toLowerCase() === 'is') {
 				processPrototypes(object[p], clazz, package);
-			} else if (p.toLowerCase() === '@fromnode') {
-				var name = object["@fromNode"];
-				var node = package.find(name);
-				object["@fromNode"] = node.name;
-				// object[p] is not an object
-			} else if (p.toLowerCase() === '@tonode') {
-				var name = object["@toNode"];
-				var node = package.find(name);
-				object["@toNode"] = node.name;
-				// object[p] is not an object
 			} else if (p.toLowerCase() === '@name') {
 				var name = object["@name"];
 				// object[p] is not an object
@@ -125,27 +115,65 @@ function processPrototypes(object, clazz, package) {
 	}
 }
 
+function processRoutes(routes, clazz, package) {
+	clazz.push("this.runRoute = function() {");
+	var r;
+	for (r in routes) {
+		var route = routes[r];
+		var fromNode = route["@fromNode"];
+		var fromField = route["@fromField"];
+		var toNode = route["@toNode"];
+		var toField = route["@toField"];
+		clazz.push(package.name+'.this.'+toNode+'.setters.'+toField+'('+package.name+'.this.'+fromNode+'.getters.'+fromField+'());');
+	}
+	clazz.push("};");
+}
+
 function processFields(fields, package) {
 	var f;
 	for (f in fields) {
 		var object = fields[f];
 		var name = object["@name"];
+		package.types[name] = object["@type"];
 		switch(object['@accessType']) {
 		case 'initializeOnly':
+			// these should be in order, so it's an array
 			package.initializers.push(name);
+			package.values[name] = object["@value"];
 			break;
 		case 'inputOutput':
-			package.setters.push(name);
-			package.getters.push(name);
+			// setters should be looked up by name
+			package.setters[name] = object;
+			package.getters[name] = object;
+			package.values[name] = object["@value"];
 			break;
 		case 'inputOnly':
-			package.setters.push(name);
+			// setters should be looked up by name
+			package.setters[name] = object;
+			package.values[name] = object["@value"];
 			break;
 		case 'outputOnly':
 			break;
-			package.getters.push(name);
+			package.getters[name] = object;
 		default:
 			break;
+		}
+	}
+}
+
+function processSource(lines, clazz, package) {
+	if (typeof lines !== 'undefined') {
+		var functions = lines.join("\n").split("function");
+		var f;
+		for (var f = 1; f < functions.length; f++) {  // skip ecmascript:
+			var func = functions[f];
+			var sp = func.indexOf('(');
+			var name = func.substr(0, sp).trim();
+			var funcvar = 'this.' + name;  //  a non setter function
+			if (typeof package.setters[name] !== 'undefined') {
+				funcvar = 'this.setters.' + name; // a setter function
+			}
+			clazz.push(funcvar + ' = function ' + func.substr(sp));
 		}
 	}
 }
