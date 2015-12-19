22345efae942e68fd54d6a39d237256624d63c5a
carlsonsolutiondesign <john@carlsonsolutiondsign.com>
2015-12-19 02:18:58 -0500

    Seems like a good time to commit.  Got rid of logging for now.  getting ready to fully integrate scripts

--- PPP.js
+++ PPP.js
@@ -1,6 +1,9 @@
-// X3D JSON Prototype preprocessor
+// X3D JSON Prototype and Script preprocessor
 
 var prototypeExpander = require('./PrototypeExpander');
+var script = require('./Script');
+processScripts = script.processScripts;
+runRoutes = script.runRoutes;
 
 var content = '';
 // read content into buffer
@@ -11,4 +14,10 @@ process.stdin.on('end', function() {
 	var object = JSON.parse(content);
 	prototypeExpander(object, "");
 	console.log(JSON.stringify(object, null, 2));
+
+	var classes = [];
+	processScripts(object, classes);
+	runRoutes(classes);
+	console.log(classes.join('\n'));
+	// eval(classes.join('\n'));
 });
