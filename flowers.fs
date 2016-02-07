515dc190f78b08b2449388908a1fb7e8ba2999c3
carlsonsolutiondesign <john@carlsonsolutiondsign.com>
2016-02-07 15:59:14 -0500

    Flowers stuff

---
+++ flowers.fs
@@ -0,0 +1,23 @@
+#ifdef GL_ES
+  precision highp float;
+#endif
+
+uniform samplerCube cube;
+
+varying vec3 t;
+varying vec3 tr;
+varying vec3 tg;
+varying vec3 tb;
+varying float rfac;
+
+void main()
+{
+    vec4 ref = textureCube(cube, t);
+    vec4 ret = vec4(1.0);
+
+    ret.r = textureCube(cube, tr).r;
+    ret.g = textureCube(cube, tg).g;
+    ret.b = textureCube(cube, tb).b;
+
+    gl_FragColor = ret * rfac + ref * (1.0 - rfac);
+}
