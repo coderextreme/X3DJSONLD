865e002fa7be95a8428a9ffa830eae6fb5c22572
carlsonsolutiondesign <john@carlsonsolutiondsign.com>
2016-02-16 19:32:25 -0500

    Checked in more orbital stuff and a preliminary Standard.js for translating xml to json

---
+++ Standard.js
@@ -0,0 +1,261 @@
+var fs = require('fs');
+var xml2js = require('xml2js');
+
+function handleAttribute(json, key) {
+	if (typeof json[key] === 'object') {
+		if (key === '$') {
+			for (var attr in json[key]) {
+				if (isNaN(parseInt(attr))) {
+					var value = json[key][attr].split(/[, ]+/);
+					var foundString = false;
+					for (var v in value) {
+						var f = parseFloat(value[v]);
+						if (isNaN(value[v])) {
+							foundString = true;
+						} else {
+							value[v] = f;
+						}
+					}
+					if (foundString) {
+						json['@'+attr] = json[key][attr];
+					} else if (value.length > 1){
+						json['@'+attr] = value;
+					} else {
+						json['@'+attr] = parseFloat(json[key][attr]);
+					}
+				}
+			}
+		}
+		handleObject(json[key]);
+	} else if (isNaN(parseInt(key))) {
+		if (key === '_') {
+			json['#sourceText'] = json[key].split(/\n/);
+		} else {
+			json['@'+key] = json[key];
+		}
+	}
+}
+
+function zap(json) {
+	for (var key in json) {
+		if (typeof json[key] === 'object') {
+			if (key === '$') {
+				delete json[key];
+			}
+			zap(json[key]);
+		} else if (isNaN(parseInt(key))) {
+			if (key.lastIndexOf("@") != 0) {
+				delete json[key];
+			}
+		}
+	}
+}
+
+function handleObject(json) {
+	var attrcnt = 0;
+	for (var key in json) {
+		attrcnt++;
+	}
+	if (attrcnt === 1) {
+		handleAttribute(json, key);
+	} else {
+		for (var key in json) {
+			handleAttribute(json, key);
+		}
+	}
+}
+
+function addContainerObject(path, node, key, parentkey, gparentkey) {
+	var newparent = path[path.length-3];
+	if (typeof newparent['-'+node[key]] === 'undefined') {
+		newparent['-'+node[key]] = [];
+	}
+	var arr = path[path.length-2];
+	obj = {};
+	obj[gparentkey] = arr[0];
+	newparent['-'+node[key]].push(JSON.parse(JSON.stringify(obj)));
+}
+
+function handleContainerField(path, node, parentkey, gparentkey) {
+	path.push(node);
+	for (var key in node) {
+		if (typeof node[key] === 'object') {
+			handleContainerField(path, node[key], key, parentkey);
+		} else if (key === '@containerField') {
+			addContainerObject(path, node, key, parentkey, gparentkey);
+			delete path[path.length-3][gparentkey];
+		}
+	}
+	path.pop();
+}
+
+function addContainerField(node) {
+	var containers = {
+		"Sphere": "geometry",
+		"Text": "geometry",
+		"Extrusion": "geometry",
+		"FontStyle": "fontStyle",
+		"ImageTexture": "texture",
+		"Appearance": "appearance",
+		"Material": "material"
+	};
+	for (var key in node) {
+		if (typeof node[key] === 'object') {
+			addContainerField(node[key]);
+		}
+		if (typeof containers[key] !== 'undefined' && typeof node[key]["@containerField"] === 'undefined') {
+			if (node[key][0] === "") {
+				node[key][0] = {};
+			}
+			node[key][0]["@containerField"] = containers[key];
+		}
+	}
+}
+
+function routes(node, parent) {
+	for (var key in node) {
+		if (key === "ROUTE") {
+			for (var route in node[key]) {
+				var r = node[key][route];
+				node[key][route] = {};
+				node[key][route]["ROUTE"] = r;
+				parent.push(node[key][route]);
+			}
+			delete node[key];
+		} else if (typeof node[key] === 'object') {
+			routes(node[key], node);
+		}
+	}
+}
+
+function proto(node, parent) {
+	for (var key in node) {
+		// if (key === "ProtoInstance") {
+		if (key === "ProtoInstance" || key === "head" || key === 'ProtoDeclare' || key === 'ProtoInterface' || key === 'PositionInterpolator' || key === 'Script' || key === 'TimeSensor' || key === 'IS' || key === 'Shape') {
+			for (var instance in node[key]) {
+				if (typeof parent.push === 'function') {
+					var i = node[key][instance];
+					var pi = {};
+					pi[key] = JSON.parse(JSON.stringify(i));
+					console.log('pushing', pi);
+					parent.push(pi);
+					delete i;
+				} else {
+					node[key] = node[key][0]; // make first element the attribute value
+				}
+			}
+			proto(node[key], node);
+			if (typeof parent.push === 'function') {
+				delete node[key];
+			}
+		} else if (typeof node[key] === 'object') {
+			proto(node[key], node);
+		}
+	}
+}
+
+function arrayToObj(node, parent) {
+	for (var key in node) {
+		if (key === "head" || key === 'ProtoDeclare' || key === 'ProtoInterface' || key === 'PositionInterpolator' || key === 'Script' || key === 'TimeSensor' || key === 'IS' || key === 'Shape') {
+			node[key] = node[key][0]; // make first element the attribute value
+			arrayToObj(node[key], node);
+		} else if (typeof node[key] === 'object') {
+			arrayToObj(node[key], node);
+		}
+	}
+}
+
+function addChildren(node) {
+	for (var key in node) {
+		if (key === "Scene" || key === 'ProtoBody' || key === 'Transform') {
+			var children = node[key];
+			node[key] = {};
+			if (typeof node[key]["-children"] === 'undefined') {
+				node[key]["-children"] = [];
+			}
+			for (var ind in children) {
+				if (key === 'Scene') {
+					// console.log('ind', ind, key);
+				}
+				for (var childkey in children[ind]) {
+					if (childkey.indexOf("@") === 0) {
+						// copy simple attributes to main object
+						node[key][childkey] = children[ind][childkey];
+						children[ind][childkey] = undefined;
+					} else if (childkey === 'IS') {
+						// copy the IS
+						var newchild = {};
+						newchild[childkey] = children[ind][childkey];
+						node[key]["IS"] = newchild;
+					} else {
+						// copy the objects
+						var newchild = {};
+						newchild[childkey] = children[ind][childkey];
+						// console.log('pushing', newchild);
+						node[key]["-children"].push(newchild);
+					}
+				}
+			}
+			addChildren(node[key]);
+		} else if (typeof node[key] === 'object') {
+			if (key != '-children') {
+				addChildren(node[key]);
+			}
+		}
+	}
+}
+
+
+function parseXML(xmlfile) {
+	var parser = new xml2js.Parser();
+	var data = fs.readFileSync(__dirname + '/'+xmlfile);
+	parser.parseString(data, function (err, result) {
+		console.log(xmlfile, 'parsing');
+		// console.log(JSON.stringify(result, null, 2));
+
+		handleObject(result);
+		zap(result);
+		addContainerField(result);
+		handleContainerField([], result, "X3D", "X3D")
+		routes(result);
+		proto(result);
+		// arrayToObj(result);
+		addChildren(result);
+
+		var ext = xmlfile.lastIndexOf(".");
+		fs.writeFileSync(__dirname+'/'+xmlfile.substr(0, ext)+'.json2', JSON.stringify(result, null, 2));
+		console.log(xmlfile, 'parsed');
+	});
+}
+
+parseXML('force.x3d');
+/*
+parseXML('HelloWorldEncryptionResult.xml');
+parseXML('HelloWorldSignedCanonical.xml');
+parseXML('HelloWorldSignedEncryptionResult.xml');
+parseXML('CoordinateAxes.x3d');
+parseXML('bubbles.x3d');
+parseXML('cobweb.x3d');
+parseXML('cobwebs.x3d');
+parseXML('extrusion.x3d');
+parseXML('flipper.x3d');
+parseXML('flowers2.x3d');
+parseXML('flowers4.x3d');
+parseXML('for.x3d');
+parseXML('forces.x3d');
+parseXML('geo.x3d');
+parseXML('geobubbles.x3d');
+parseXML('john.x3d');
+parseXML('john2.x3d');
+parseXML('mirror.x3d');
+parseXML('mirror2.x3d');
+parseXML('pp3.x3d');
+parseXML('pp3s.x3d');
+parseXML('qq3.x3d');
+parseXML('qq3s.x3d');
+parseXML('rubik.x3d');
+parseXML('rubikFurnace.x3d');
+parseXML('rubikOnFire.x3d');
+parseXML('rubikOnFires.x3d');
+parseXML('rubiks.x3d');
+*/
--- flowers.vs
+++ flowers.vs
@@ -1,3 +1,7 @@
+#ifdef GL_ES
+  precision highp float;
+#endif
+
 attribute vec3 position;
 attribute vec3 normal;
 attribute vec2 texcoord;
@@ -7,7 +11,7 @@ uniform mat4 modelViewMatrix;
 uniform mat4 modelViewMatrixInverse;
 uniform mat4 normalMatrix;
 
-uniform vec3 chromaticDispertion;
+uniform vec3 chromaticDispersion;
 uniform float bias;
 uniform float scale;
 uniform float power;
@@ -25,17 +29,17 @@ varying vec3 tb;
 varying float rfac;
 
 vec3 cart2sphere(vec3 p) {
-     float r = pow(p.x*p.x + p.y*p.y + p.z*p.z, 0.5);
-     float theta = acos(p.z/r);
-     float phi = atan(p.y, p.x);
+     float r = sqrt(p.x*p.x + p.y*p.y + p.z*p.z);
+     float theta = acos(p.y/r);
+     float phi = atan(p.z, p.x);
      return vec3(r, theta, phi);
 }
      
 vec3 rose(vec3 p) {
      float rho = a + b * cos(c * p.y + tdelta) * cos(d * p.z + pdelta);
      float x = rho * cos(p.z) * cos(p.y);
-     float y = rho * cos(p.z) * sin(p.y);
-     float z = rho * sin(p.z);
+     float z = rho * cos(p.z) * sin(p.y);
+     float y = rho * sin(p.z);
      return vec3(x, y, z);
 }
 
@@ -43,9 +47,9 @@ vec3 rose_normal(vec3 p) {
      /* convert cartesian position to spherical coordinates */
      vec3 base = cart2sphere(p);
      /* add a little to phi */
-     vec3 td = base + vec3(0.0, 0.01, 0.0);
+     vec3 td = base + vec3(0.0, 0.0001, 0.0);
      /* add a little to theta */
-     vec3 pd = base + vec3(0.0, 0.0, 0.01);
+     vec3 pd = base + vec3(0.0, 0.0, 0.0001);
 
      /* convert back to cartesian coordinates */
      vec3 br = rose(base);
@@ -53,6 +57,11 @@ vec3 rose_normal(vec3 p) {
      vec3 bp = rose(pd);
 
      return normalize(cross(bt - br, bp - br));
+}
+
+vec4 rose_position(vec3 p) {
+	return vec4(rose(cart2sphere(p)), 1.0);
+	/*return vec4(position, 1.0);*/
 }
 
 void main()
@@ -69,13 +78,13 @@ void main()
 		modelViewMatrix[2].z
     );
     vec3 fragNormal = mvm3*rose_normal(position);
-    gl_Position = modelViewProjectionMatrix*vec4(rose(cart2sphere(position)), 1.0);
-    vec3 incident = normalize((modelViewMatrix * vec4(rose(cart2sphere(position)), 1.0)).xyz);
+    gl_Position = modelViewProjectionMatrix*rose_position(position);
+    vec3 incident = normalize((modelViewMatrix * rose_position(position)).xyz);
 
     t = reflect(incident, fragNormal)*mvm3;
-    tr = refract(incident, fragNormal, chromaticDispertion.x)*mvm3;
-    tg = refract(incident, fragNormal, chromaticDispertion.y)*mvm3;
-    tb = refract(incident, fragNormal, chromaticDispertion.z)*mvm3;
+    tr = refract(incident, fragNormal, chromaticDispersion.x)*mvm3;
+    tg = refract(incident, fragNormal, chromaticDispersion.y)*mvm3;
+    tb = refract(incident, fragNormal, chromaticDispersion.z)*mvm3;
 
     rfac = bias + scale * pow(0.5+0.5*dot(incident, fragNormal), power);
 }
