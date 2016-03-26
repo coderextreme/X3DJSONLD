6b43d2d822dbf7cc9f3caec67026b6ae2cab7f1d
carlsonsolutiondesign <john@carlsonsolutiondsign.com>
2016-03-26 00:26:08 -0400

    Numerous changes, including support for encoding and versions of X3D

--- json2x3d.js
+++ json2x3d.js
@@ -5,27 +5,27 @@
 // Convert X3D JSON to XML
 
 var fs = require('fs');
-
 var xmldom = require('xmldom');
-var DOMImplementation = new xmldom.DOMImplementation();
-var XMLSerializer = new xmldom.XMLSerializer();
-var docType = DOMImplementation.createDocumentType("X3D", 'ISO//Web3D//DTD X3D 3.3//EN" "http://www.web3d.org/specifications/x3d-3.3.dtd', null);
-var document = DOMImplementation.createDocument(null, "X3D", docType);
-document.insertBefore(document.createProcessingInstruction('xml', 'version="1.0" encoding="UTF-8"'), document.doctype);
-var element = document.getElementsByTagNameNS(null, "X3D")[0];
-element.setAttribute("xmlns:xsd", 'http://www.w3.org/2001/XMLSchema-instance');
-
 var X3DJSONLD = require('./X3DJSONLD');
-X3DJSONLD.setDocument(document);
-var ConvertToX3DOM = X3DJSONLD.ConvertToX3DOM;
-var fixXML = X3DJSONLD.fixXML;
 
 function loadX3DJS(json, path, xml) {
+	var version = json.X3D["@version"];
+	var DOMImplementation = new xmldom.DOMImplementation();
+	var XMLSerializer = new xmldom.XMLSerializer();
+	var docType = DOMImplementation.createDocumentType("X3D", 'ISO//Web3D//DTD X3D '+version+'//EN" "http://www.web3d.org/specifications/x3d-'+version+'.dtd', null);
+	var document = DOMImplementation.createDocument(null, "X3D", docType);
+        X3DJSONLD.setDocument(document);
+	var element = document.getElementsByTagNameNS(null, "X3D")[0];
+	element.setAttribute("xmlns:xsd", 'http://www.w3.org/2001/XMLSchema-instance');
+	var ConvertToX3DOM = X3DJSONLD.ConvertToX3DOM;
+ 	var fixXML = X3DJSONLD.fixXML;
+
+
 	ConvertToX3DOM(json, "", element, path);
 	var xmlstr = XMLSerializer.serializeToString(element);
 	xmlstr = fixXML(xmlstr);
-	xml.push('<?xml version="1.0" encoding="UTF-8"?>');
-	xml.push('<!DOCTYPE X3D PUBLIC "ISO//Web3D//DTD X3D 3.3//EN" "http://www.web3d.org/specifications/x3d-3.3.dtd">');
+	xml.push('<?xml version="1.0" encoding="'+X3DJSONLD.getEncoding()+'"?>');
+	xml.push('<!DOCTYPE X3D PUBLIC "ISO//Web3D//DTD X3D '+version+'//EN" "http://www.web3d.org/specifications/x3d-'+version+'.dtd">');
 	xml.push(xmlstr);
 }
 
