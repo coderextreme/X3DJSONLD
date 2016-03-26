9214b96daaaf2f650d5d730800daa235894c0296
carlsonsolutiondesign <john@carlsonsolutiondsign.com>
2016-03-25 21:01:50 -0400

    ignore case on replaces

--- X3DJSONLD.js
+++ X3DJSONLD.js
@@ -199,7 +199,7 @@ function fixXML(xmlstr) {
 	xmlstr = xmlstr.replace(/xmlns="[^"]*"/g, "");
 
 	// strip out schema
-	xmlstr = xmlstr.replace(/xsd:noNamespaceSchemaLocation="[^"]*"/g, "");
+	xmlstr = xmlstr.replace(/xsd:noNamespaceSchemaLocation="[^"]*"/gi, "");
 
 
 	// Fix CDATA sections
@@ -207,11 +207,11 @@ function fixXML(xmlstr) {
 	xmlstr = xmlstr.replace(/\]\]&gt;/g, "]]>");
 	do {
 		var xmlstr2 = xmlstr;
-		xmlstr = xmlstr2.replace(/(\<\!\[CDATA\[(.|\n)*)&lt;((.|\n)*\]\]\>)/g, "$1<$3");
+		xmlstr = xmlstr2.replace(/(\<\!\[CDATA\[(.|\n)*)&lt;((.|\n)*\]\]\>)/gi, "$1<$3");
 	} while (xmlstr !== xmlstr2);
 	do {
 		xmlstr2 = xmlstr;
-		xmlstr = xmlstr2.replace(/(\<\!\[CDATA\[(.|\n)*)&gt;((.|\n)*\]\]\>)/g, "$1>$3");
+		xmlstr = xmlstr2.replace(/(\<\!\[CDATA\[(.|\n)*)&gt;((.|\n)*\]\]\>)/gi, "$1>$3");
 	} while (xmlstr !== xmlstr2);
 	return xmlstr;
 }
