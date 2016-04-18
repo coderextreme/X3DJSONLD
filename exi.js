1f732772b7f211c90bfd574a36a32e04d9ddb546
carlsonsolutiondesign <john@carlsonsolutiondsign.com>
2016-04-18 01:09:11 -0400

    Use Hexadecimal and spaces instead of decimal and commas

--- exi.js
+++ exi.js
@@ -1,15 +1,31 @@
+function writeUint18ArrayBufferToEXIEditor(uint8Array, uint8ArrayLength) {
+	var hex = "";
+	for(var i=0; i<uint8ArrayLength; i++) {
+		var sh = uint8Array[i].toString(16); // base 16 (hex)
+		if(sh.length == 1) {
+			sh = "0" + sh;
+		}
+		// hex +=	"0x" + sh + " ";
+		hex +=	sh + " ";			
+		if((i+1) % 16 == 0) {
+			hex += "\n"
+		}
+	}
+	// editorEXI.setValue("len = " + uint8ArrayLength + "\n" + hex);
+	$("#exi").val(hex);
+}
+
 function encodeJSON() {
 	var textJSON = $("#json").val(); // JSON text input
 	if(textJSON.trim().length == 0) {
 		alert("No JSON provided");
 	} else {
 		try {
-			var jsonObj = JSON.parse(textJSON);
 			var exiEncoder = new EXI4JSONEncoder();	
-			exiEncoder.encodeJsonObject(jsonObj);
+			exiEncoder.encodeJsonText(textJSON);
 			var uint8ArrayLength = exiEncoder.getUint8ArrayLength();
 			var uint8Array = exiEncoder.getUint8Array();
-			$("#exi").val(uint8Array);
+			writeUint18ArrayBufferToEXIEditor(uint8Array, uint8ArrayLength);
 		} catch(err) {
 			alert("Error while encoding JSON. " + err);
 		}
@@ -25,11 +41,11 @@ function decodeEXI4JSON() {
 		alert("No EXI provided");
 	} else {
 		try {
-			var sp = textEXI.split(","); 
+			var sp = textEXI.split(/[ \n\r\t]+/); 
 			var arrayBuffer = new ArrayBuffer(sp.length);
 			var arrayBufferView = new Uint8Array(arrayBuffer);
 			for(var i=0; i<sp.length; i++) {
-				arrayBufferView[i] = parseInt(sp[i], 10);
+				arrayBufferView[i] = parseInt(sp[i], 16);
 			}
 			
 			var exiDecoder = new EXI4JSONDecoder();
