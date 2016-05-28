30dade97a8661ab6f8e816a8752034fd4ff7b706
carlsonsolutiondesign <john@carlsonsolutiondsign.com>
2016-05-28 17:22:52 -0400

    Added Box.json for testing

--- runpppex.sh
+++ runpppex.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-# for i in examples/Basic/UniversalMediaMaterials/AutumnExamples.json
-for i in `find examples/X3dForWebAuthors/Chapter14-Prototypes examples/Vrml2.0Sourcebook/Chapter31-Prototypes examples/Basic/UniversalMediaMaterials -type f -name '*json' | xargs grep -ilw ProtoInstance`
+for i in box.json
+# for i in `find examples/X3dForWebAuthors/Chapter14-Prototypes examples/Vrml2.0Sourcebook/Chapter31-Prototypes examples/Basic/UniversalMediaMaterials -type f -name '*json' | xargs grep -ilw ProtoInstance`
 do
 	echo "=========================$i====================="
 	mkdir -p ppp/`dirname $i`
