b8a809ff248a44453a14be39b23b14bbe1027303
carlsonsolutiondesign <john@carlsonsolutiondsign.com>
2016-05-29 07:29:27 -0400

    Pretty good version

--- runppp.sh
+++ runppp.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -x
 
-# for i in force.json
-for i in for.json rubikOnFire.json bubbles.json ArchPrototype.json ObliqueStrategies.json rubik.json pp3.json qq3.json cobweb.json force.json NancyPrototypes.json examples/X3dForWebAuthors/KelpForestExhibit/KelpForestMain.json 
+# for i in for.json
+for i in ArchPrototype.json ObliqueStrategies.json  NancyPrototypes.json for.json rubikOnFire.json bubbles.json rubik.json pp3.json qq3.json cobweb.json force.json
 do
 	echo "=========================$i====================="
-	read me
+	// read me
 	node PPP.js $i > `basename $i .json`s.json
 done
