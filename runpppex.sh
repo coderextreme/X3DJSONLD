5177c03f8eb13d0a64c1286166a60d5deda5a42f
carlsonsolutiondesign <john@carlsonsolutiondsign.com>
2016-05-29 08:00:07 -0400

    Added ppp folder for my own expanded prototypes

--- runpppex.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-for i in examples/X3dForWebAuthors/Chapter14-Prototypes/HudKelpForest.json
-# for i in `find examples/X3dForWebAuthors/Chapter14-Prototypes examples/Vrml2.0Sourcebook/Chapter31-Prototypes examples/Basic/UniversalMediaMaterials -type f -name '*json' | xargs grep -ilw ProtoInstance`
-do
-	echo "=========================$i====================="
-	mkdir -p ppp/`dirname $i`
-	node PPP.js $i > ppp/`dirname $i`/ppp`basename $i`
-done
