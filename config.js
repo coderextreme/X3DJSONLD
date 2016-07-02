2d5751109620416a354665e96c57dd643360534d
John Carlson <yottzumm@gmail.com>
2016-07-02 09:00:42 -0400

    Added Flattener.js, updated examples

--- config.js
+++ config.js
@@ -1,1 +1,2 @@
-module.exports.examples = "C:/www.web3d.org/x3d/content/examples/";
+module.exports.examples = "c:/Users/coderextreme/Documents/NetBeansProjects/x3d-code/www.web3d.org/x3d/content/examples/";
+
