65d759fd85b3c45138cc3650593b804c3c87bc96
John Carlson <yottzumm@gmail.com>
2016-07-15 09:09:44 -0400

    Major mods to CompConv.js

---
+++ CompConv.js
@@ -0,0 +1,84 @@
+// Complete X3D XML to X3D XML Prototype Expander
+
+// set up XML DOM
+var xmldom = require('xmldom');
+var fs = require("fs");
+
+var X3DJSONLD = require('./X3DJSONLD.js');
+var loadURLs = X3DJSONLD.loadURLs;
+var PE = require('./PrototypeExpander')
+var FL = require('./Flattener')
+PE.setLoadURLs(loadURLs);
+var prototypeExpander = PE.prototypeExpander;
+var externPrototypeExpander = PE.externPrototypeExpander;
+var flattener = FL.flattener;
+var ConvertToX3DOM = X3DJSONLD.ConvertToX3DOM;
+var Browser = X3DJSONLD.Browser;
+
+var DOMImplementation = new xmldom.DOMImplementation();
+var XMLSerializer = new xmldom.XMLSerializer();
+
+function fixXML(xmlstr) {
+	// get rid of self-closing tags
+	xmlstr = xmlstr.replace(/(<[ \t]*)([A-Za-z0-9]+)([^>]*)\/>/g, "$1$2$3></$2>");
+	// strip out namespace
+	xmlstr = xmlstr.replace(/xmlns="[^"]*"/g, "");
+
+	// strip out schema
+	// xmlstr = xmlstr.replace(/xsd:noNamespaceSchemaLocation="[^"]*"/gi, "");
+
+
+	// Fix CDATA sections
+	xmlstr = xmlstr.replace(/&lt;!\[CDATA\[/g, "<![CDATA[");
+	xmlstr = xmlstr.replace(/\]\]&gt;/g, "]]>");
+	do {
+		var xmlstr2 = xmlstr;
+		xmlstr = xmlstr2.replace(/(<!\[CDATA\[(.|\n)*)&lt;((.|\n)*\]\]>)/gi, "$1<$3");
+	} while (xmlstr !== xmlstr2);
+	do {
+		xmlstr2 = xmlstr;
+		xmlstr = xmlstr2.replace(/(<!\[CDATA\[(.|\n)*)&gt;((.|\n)*\]\]>)/gi, "$1>$3");
+	} while (xmlstr !== xmlstr2);
+	return xmlstr;
+}
+
+function loadX3DJS(json, path, xml) {
+	if (typeof json === 'undefined') {
+		console.error('json undefined.  Look in', path, 'for hints');
+	}
+	var version = json.X3D["@version"];
+	var document = DOMImplementation.createDocument(null, "X3D", docType);
+	var docType = DOMImplementation.createDocumentType("X3D", 'ISO//Web3D//DTD X3D '+version+'//EN" "http://www.web3d.org/specifications/x3d-'+version+'.dtd', null);
+	var document = DOMImplementation.createDocument(null, "X3D", docType);
+	document.insertBefore(document.createProcessingInstruction('xml', 'version="1.0" encoding="UTF-8"'), document.doctype);
+	var element = document.getElementsByTagNameNS(null, "X3D")[0];
+	element.setAttribute("xmlns:xsd", 'http://www.w3.org/2001/XMLSchema-instance');
+
+	// Bring in JSON to DOM/XML conversion --  used to build DOM/XML.
+	X3DJSONLD.setDocument(document);
+
+	ConvertToX3DOM(json, "", element, path);
+	var xmlstr = XMLSerializer.serializeToString(element);
+	xmlstr = fixXML(xmlstr);
+	xml.push('<?xml version="1.0" encoding="'+X3DJSONLD.getEncoding()+'"?>');
+	xml.push('<!DOCTYPE X3D PUBLIC "ISO//Web3D//DTD X3D '+version+'//EN" "http://www.web3d.org/specifications/x3d-'+version+'.dtd">');
+	xml.push(xmlstr);
+}
+
+
+var runsaxon = require('./allsaxon');
+process.argv.shift();
+process.argv.shift();
+runsaxon(process.argv);
+for (var arg in process.argv) {
+	var file = process.argv[arg];
+	var file = file.substr(0, file.lastIndexOf("."))+".json";
+	var object = JSON.parse(fs.readFileSync(file).toString());
+	object = externPrototypeExpander(file, object);
+	object = prototypeExpander(file, object, "");
+	object = flattener(object);
+	var xml = [];
+	loadX3DJS(object, file, xml);
+	var newfile = "ppp/"+file.substr(0, file.lastIndexOf("."))+"Expanded.x3d";
+	fs.writeFileSync(newfile, xml.join("\n"));
+}
