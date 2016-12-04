2bc3e27e5602d4e38dc115345271c3c5c8d84052
Andreas Plesch <andreasplesch@users.noreply.github.com>
2016-12-04 16:01:10 -0500

    Update X3DJSONLD.js

--- X3DJSONLD.js
+++ X3DJSONLD.js
@@ -231,10 +231,14 @@ function CDATACreateFunction(document, element, str) {
 	element.appendChild(child);
 	element.appendChild(close);
 	*/
-	///*
+	/*
 	var child = document.createCDATASection(str);
 	element.appendChild(child);
-	//*/
+	*/
+	var domParser = new DOMParser();
+	var scriptDoc = domParser .parseFromString ('<![CDATA['+str+']]>', 'application/xml');
+	var cdata = scriptDoc .children[0] .childNodes[0];
+	element.appendChild(cdata);
 }
 
 function setCDATACreateFunction(fnc) {
