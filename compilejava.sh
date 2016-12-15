8b4f3e49b1b342dfd60875f211af314a970f31bf
John Carlson <yottzumm@gmail.com>
2016-12-14 21:20:12 -0500

    More mods for cleaner compile

--- compilejava.sh
+++ compilejava.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 X3DJSONLD=./../pythonSAI/
 export CLASSPATH="${X3DJSONLD}jslint4java-2.0.5.jar;${X3DJSONLD}json-schema-validator-2.2.6-lib.jar;${X3DJSONLD}X3dJavaSceneAccessInterface3.3.classes.jar;${X3DJSONLD}X3dJavaSceneAccessInterface3.3.full.jar;${CLASSPATH}"
-for i in  abox.java arc.java ArchHalf.java ArchPrototype.java box.java bubbles.java cobweb.java CoordinateAxes.java ExtrusionHeart.java flipper.java flowers2.java flowers4.java flowers7.java force.java geo.java geobubbles.java IcosahedronSubdivisionLevel5.java mirror.java mirror2.java NancyNativeTags.java NancyPrototypes.java ObliqueStrategies.java pp3.java qq3.java rubik.java rubikFurnace.java rubikOnFire.java RunSaxon.java
+tar -czf JavaSAIExamples.tar.gz abox.java arc.java ArchHalf.java ArchPrototype.java box.java bubbles.java cobweb.java CoordinateAxes.java ExtrusionHeart.java flipper.java flowers2.java flowers4.java flowers7.java force.java geo.java geobubbles.java IcosahedronSubdivisionLevel5.java mirror.java mirror2.java NancyNativeTags.java NancyPrototypes.java ObliqueStrategies.java pp3.java qq3.java rubik.java rubikFurnace.java rubikOnFire.java JavaSerializer.js
+for i in  abox.java arc.java ArchHalf.java ArchPrototype.java box.java bubbles.java cobweb.java CoordinateAxes.java ExtrusionHeart.java flipper.java flowers2.java flowers4.java flowers7.java force.java geo.java geobubbles.java IcosahedronSubdivisionLevel5.java mirror.java mirror2.java NancyNativeTags.java NancyPrototypes.java ObliqueStrategies.java pp3.java qq3.java rubik.java rubikFurnace.java rubikOnFire.java
 do
 	javac -cp ${CLASSPATH} $i
 done
