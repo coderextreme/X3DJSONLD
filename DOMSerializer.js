25ae56822d961abcc89b2057cf5d68777ec32776
John Carlson <yottzumm@gmail.com>
2016-12-22 18:41:01 -0500

    Updated DOM Serializer

---
+++ DOMSerializer.js
@@ -0,0 +1,50 @@
+
+"use strict";
+
+var xmldom = require('xmldom');
+var DOMImplementation = new xmldom.DOMImplementation();
+var XMLSerializer = new xmldom.XMLSerializer();
+
+function fixXML(xmlstr) {
+	// get rid of self-closing tags
+	xmlstr = xmlstr.replace(/(<)([A-Za-z0-9]+)([^>]*)\/>/g, "$1$2$3></$2>");
+	// strip out namespace
+	xmlstr = xmlstr.replace(/xmlns="[^"]*"/g, "");
+
+	// strip out schema
+	// xmlstr = xmlstr.replace(/xsd:noNamespaceSchemaLocation="[^"]*"/gi, "");
+
+
+	// Fix CDATA sections
+	xmlstr = xmlstr.replace(/&lt;!\[CDATA\[/g, "<![CDATA[");
+	xmlstr = xmlstr.replace(/\]\]&gt;/g, "]]>");
+	do {
+		var xmlstr2 = xmlstr;
+		xmlstr = xmlstr2.replace(/(<!\[CDATA\[(.|\n)*)&lt;((.|\n)*\]\]>)/gi, "$1<$3");
+	} while (xmlstr !== xmlstr2);
+	do {
+		xmlstr2 = xmlstr;
+		xmlstr = xmlstr2.replace(/(<!\[CDATA\[(.|\n)*)&gt;((.|\n)*\]\]>)/gi, "$1>$3");
+	} while (xmlstr !== xmlstr2);
+	return xmlstr;
+}
+
+
+function serializeDOM(json, element) {
+	var version = json.X3D["@version"];
+	var xml = '<?xml version="1.0" encoding="'+json.X3D["encoding"]+'"?>\n';
+	xml += '<!DOCTYPE X3D PUBLIC "ISO//Web3D//DTD X3D '+version+'//EN" "http://www.web3d.org/specifications/x3d-'+version+'.dtd">\n';
+	xml += XMLSerializer.serializeToString(element);
+	xml = fixXML(xml);
+	return xml;
+}
+
+DOMSerializer = {};
+DOMSerializer.serializeToString(json, element) {
+	var str = serializeDOM(json, element));
+	return str;
+}
+
+if (typeof module === 'object')  {
+	module.exports = DOMSerializer;
+}
