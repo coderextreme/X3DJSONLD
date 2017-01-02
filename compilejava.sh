669b11592c0d5bb425cfa8543adea31a68cb0560
John Carlson <yottzumm@gmail.com>
2017-01-01 22:09:27 -0500

    Updated

--- compilejava.sh
+++ compilejava.sh
@@ -2,7 +2,12 @@
 X3DJSONLD=./../pythonSAI/
 export CLASSPATH="${X3DJSONLD}jslint4java-2.0.5.jar;${X3DJSONLD}json-schema-validator-2.2.6-lib.jar;${X3DJSONLD}X3dJavaSceneAccessInterface3.3.classes.jar;${X3DJSONLD}X3dJavaSceneAccessInterface3.3.full.jar;${CLASSPATH}"
 tar -czf JavaSAIExamples.tar.gz abox.java arc.java ArchHalf.java ArchPrototype.java box.java bubbles.java cobweb.java CoordinateAxes.java ExtrusionHeart.java flipper.java flowers2.java flowers4.java flowers7.java force.java geo.java geobubbles.java IcosahedronSubdivisionLevel5.java mirror.java mirror2.java NancyNativeTags.java NancyPrototypes.java ObliqueStrategies.java pp3.java qq3.java rubik.java rubikFurnace.java rubikOnFire.java JavaSerializer.js
-for i in `find examples/ -name '*.java'` abox.java arc.java ArchHalf.java ArchPrototype.java box.java bubbles.java cobweb.java CoordinateAxes.java ExtrusionHeart.java flipper.java flowers2.java flowers4.java flowers7.java force.java geo.java geobubbles.java IcosahedronSubdivisionLevel5.java mirror.java mirror2.java NancyNativeTags.java NancyPrototypes.java ObliqueStrategies.java pp3.java qq3.java rubik.java rubikFurnace.java rubikOnFire.java
+for i in abox.java arc.java ArchHalf.java ArchPrototype.java box.java bubbles.java cobweb.java CoordinateAxes.java ExtrusionHeart.java flipper.java flowers2.java flowers4.java flowers7.java force.java geo.java geobubbles.java IcosahedronSubdivisionLevel5.java mirror.java mirror2.java NancyNativeTags.java NancyPrototypes.java ObliqueStrategies.java pp3.java qq3.java rubik.java rubikFurnace.java rubikOnFire.java `find examples/ -name '*.java'`
 do
-	javac -cp ${CLASSPATH} $i
+	if javac -cp ${CLASSPATH} $i
+	then
+		tar -rf GoodJava.tar $i
+	else
+		tar -rf BadJava.tar $i
+	fi
 done
