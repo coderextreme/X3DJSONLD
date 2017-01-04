3f1096cb9701edbcac2a1b6beadd4928bd7e4b14
John Carlson <yottzumm@gmail.com>
2017-01-04 17:52:45 -0500

    Now executing Java files

--- compilejava.sh
+++ compilejava.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 X3DJSONLD=./../pythonSAI/
-export CLASSPATH="${X3DJSONLD}jslint4java-2.0.5.jar;${X3DJSONLD}json-schema-validator-2.2.6-lib.jar;${X3DJSONLD}X3dJavaSceneAccessInterface3.3.classes.jar;${X3DJSONLD}X3dJavaSceneAccessInterface3.3.full.jar;${CLASSPATH}"
+export CLASSPATH=".;${X3DJSONLD}saxon9he.jar;${X3DJSONLD}jslint4java-2.0.5.jar;${X3DJSONLD}json-schema-validator-2.2.6-lib.jar;${X3DJSONLD}X3dJavaSceneAccessInterface3.3.classes.jar;${X3DJSONLD}X3dJavaSceneAccessInterface3.3.full.jar;${CLASSPATH}"
 for i in `ls *.java | grep -v RunSaxon.java` `find examples/ -name '*.java'`
 do
-	if javac -cp ${CLASSPATH} $i
+	CLASS=`dirname $i`/`basename $i .java` 
+	CLASS=`echo $CLASS | sed 's/^[\.\/]*//'`
+	echo $CLASS
+	if javac $i && java $CLASS
 	then
 		tar -rf GoodJava.tar $i
 	else
