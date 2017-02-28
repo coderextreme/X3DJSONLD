601e87e463bbc0f13e0cda4201ee1e7651bff36e
John Carlson <yottzumm@gmail.com>
2017-02-27 21:11:59 -0500

    replaced \n with \\n in serverside CDATA sections.  Moved JavaSerializer code around

--- compilejava.sh
+++ compilejava.sh
@@ -3,7 +3,7 @@ jar -cMf GoodJava.zip RunSaxon.java
 jar -cMf BadJava.zip compilejava.sh runjson.sh
 jar -cMf DiffJSON.zip compilejava.sh runjson.sh
 X3DJSONLD=./../pythonSAI/
-export CLASSPATH=".;${X3DJSONLD}saxon9he.jar;${X3DJSONLD}jslint4java-2.0.5.jar;${X3DJSONLD}json-schema-validator-2.2.6-lib.jar;${X3DJSONLD}X3dJavaSceneAccessInterface3.3.classes.jar;${X3DJSONLD}X3dJavaSceneAccessInterface3.3.full.jar;${CLASSPATH}"
+export CLASSPATH=".;${X3DJSONLD}saxon9he.jar;${X3DJSONLD}jslint4java-2.0.5.jar;${X3DJSONLD}json-schema-validator-2.2.6-lib.jar;${X3DJSONLD}X3DJSAIL.3.3.classes.jar;${X3DJSONLD}X3DJAIL.3.3.full.jar;${CLASSPATH}"
 for i in `ls *.java | grep -v RunSaxon.java` `find www_web3d_org/ -name '*.java'`
 do
 	BASE=`dirname $i`/`basename $i .java` 
