cea59ad802ca9cd928b9f5984225fcceafcac001
John Carlson <yottzumm@gmail.com>
2017-03-23 04:32:27 -0400

    Added -d64 switch

--- compilejava.sh
+++ compilejava.sh
@@ -5,7 +5,7 @@ X3DJSONLD=./../pythonSAI/
 export CLASSPATH=".;${X3DJSONLD}saxon9he.jar;${X3DJSONLD}jslint4java-2.0.5.jar;${X3DJSONLD}json-schema-validator-2.2.6-lib.jar;${X3DJSONLD}X3DJSAIL.3.3.classes.jar;${X3DJSONLD}X3DJAIL.3.3.full.jar;${CLASSPATH}"
 (ls *.java | grep -v RunSaxon; find www_web3d_org/ -name '*.java') | xargs -L 1 -P 8 javac
 
-(ls *.class | grep -v RunSaxon; find www_web3d_org/ -name '*.class') | sed 's/\.class$//' | xargs -L 1 -P 8 java
+(ls *.class | grep -v RunSaxon; find www_web3d_org/ -name '*.class') | sed 's/\.class$//' | xargs -L 1 -P 8 java -d64
 for NEW in `find . -name '*.new.json'`
 	JSON=`dirname $NEW`/`basename $NEW .new.json`.json
 	DIFF=`dirname $NEW`/`basename $NEW .new.json`.diff
