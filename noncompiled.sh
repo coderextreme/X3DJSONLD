4fda09a9c036be219353870f9c0110db42c51fa8
John Carlson <yottzumm@gmail.com>
2017-03-29 18:41:49 -0400

    Updated README.md

--- noncompiled.sh
+++ noncompiled.sh
@@ -1,1 +1,1 @@
-find . -name '*.java'| grep -v " "| sed 's/\.java$/.class/'| xargs ls 2>&1| grep "No such file or directory" | awk '{ print $4; }'| sed "s/^'\.\///" | sed "s/\.class':/.java/"
+find www_web3d_org -name '*.java'| grep -v " "| sed 's/\.java$/.class/'| xargs ls 2>&1| grep "No such file or directory" | awk '{ print $4; }'| sed "s/^'\.\///" | sed "s/\.class':/.java/" | sed "s/^'//" # | xargs -L 1 -P 8 javac -J-Xss16m -J-Xmx4096M
