60059129496a5edb8b02513fe4aad2e852c80541
John Carlson <yottzumm@gmail.com>
2017-03-29 21:51:26 -0400

    Changed serializers in runjson.sh

--- json2all.js
+++ json2all.js
@@ -8,7 +8,7 @@ process.argv.shift();
 var convertJSON = require('./convertJSON.js');
 
 convertJSON({
-	'./JavaSerializerNew.js' : ".java",
+	'./JavaSerializerAppend.js' : ".java",
 	'./PythonSerializer.js': ".py",
 	'./DOMSerializer.js': ".x3d.new"
 });
