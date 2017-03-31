d4120ca6ad43ae7c59e7747b2e1864a57bb40243
John Carlson <yottzumm@gmail.com>
2017-03-31 01:52:19 -0400

    Updated prototype expander code with event time.

--- allsaxon.js
+++ allsaxon.js
@@ -2,7 +2,7 @@ var java = require("java");
 java.options.push("-Djava.awt.headless=true");
 java.options.push("-Xmx1000m");
 //java.options.push('-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005');
-java.classpath.push("saxon9B.jar");
+java.classpath.push("saxon9he.jar");
 java.classpath.push(".");
 
 function translate(infiles) {
