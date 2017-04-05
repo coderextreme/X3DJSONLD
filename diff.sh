f512556c4cded0569eab1c6947caaa087472fe62
John Carlson <yottzumm@gmail.com>
2017-04-05 05:25:24 -0400

    updated

--- diff.sh
+++ diff.sh
@@ -2,6 +2,6 @@
 for i in `find . -name '*.new.x3d'`
 do
 	echo ================$i ==========================
-	echo diff -w `dirname $i  | sed 's/www_web3d_org/www.web3d.org/'`/`basename $i .new.x3d`.x3d $i
-	diff -w `dirname $i  | sed 's/www_web3d_org/www.web3d.org/'`/`basename $i .new.x3d`.x3d $i
+	echo node xmldiff.js -w `dirname $i  | sed 's/.\/www_web3d_org/\/c\/x3d-code\/www.web3d.org/'`/`basename $i .new.x3d`.x3d $i
+	node xmldiff.js `dirname $i  | sed 's/.\/www_web3d_org/\/c\/x3d-code\/www.web3d.org/'`/`basename $i .new.x3d`.x3d $i
 done
