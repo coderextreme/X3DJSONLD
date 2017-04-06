583505e52eafb7760691079e49628f3039d834e2
John Carlson <yottzumm@gmail.com>
2017-04-05 23:48:30 -0400

    now runs on 8 processors

--- runjson.sh
+++ runjson.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 export X3DRESOURCES=www.web3d.org/
-find `ls *.json | grep -v new |grep -v package.json |grep -v for.json` $X3DRESOURCES -name '*.json' -print0 | xargs -0 ${1-node} json2all.js
+find `ls *.json | grep -v new |grep -v package.json` $X3DRESOURCES -name '*.json' -print0 | xargs -0 -P 8 ${1-node} json2all.js
