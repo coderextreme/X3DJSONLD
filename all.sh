1ed4cf18d771ddde5b41351a77b91ed0b71acab9
John Carlson <yottzumm@gmail.com>
2017-04-07 08:02:02 -0400

    More mods for several.sh, C:\x3d-code testing

--- all.sh
+++ all.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-ls ./*.x3d | xargs -P 8 ./several.sh
+export PROCESSORS=1
+# ls ./*.x3d | xargs -P 8 ./several.sh
+find /c/x3d-code/www.web3d.org/x3d/content/examples/ -name '*.x3d' | xargs -P $PROCESSORS ./several.sh
+
