647adba92a3353c07e13948f7a3f9d17ae043a75
John Carlson <yottzumm@gmail.com>
2017-04-07 07:46:43 -0400

    Updated several.sh with new stuff, made to work on C:\x3d-code

--- several.sh
+++ several.sh
@@ -7,22 +7,30 @@
 javac RunSaxon.java
 python classes.py
 
-(ls "$@" | grep -v intermediate | grep -v new) | xargs -P 8 java RunSaxon
-(ls "$@" | grep -v intermediate | grep -v new) | sed 's/\.x3d$/.json/' | xargs -P 8 node json2all.js
-(ls "$@" | grep -v intermediate | grep -v new) | sed 's/\.x3d$/.java/' | xargs -L 1 -P 8 javac -J-Xss16m -J-Xmx4096M
-(ls "$@" | grep -v intermediate | grep -v new) | sed 's/\.x3d$//' | sed 's/^\.*\///' | xargs -L 1 -P 8 java -d64 -Xss16m -Xmx4096M
-for NEW in `(ls "$@" | grep -v intermediate | grep -v new) | sed 's/\.x3d$/.new.json/'`
+(ls "$@" | grep -v intermediate | grep -v new) | xargs -P $PROCESSORS java RunSaxon
+(ls "$@" | grep -v intermediate | grep -v new) | sed 's/\.x3d$/.json/' | xargs -P $PROCESSORS node json2all.js
+(ls "$@" | grep -v intermediate | grep -v new) | sed 's/\.x3d$/.java/' | sed 's/\/c\/x3d-code\/www.web3d.org/www_web3d_org/' | xargs -L 1 -P $PROCESSORS javac -J-Xss16m -J-Xmx4096M
+(ls "$@" | grep -v intermediate | grep -v new) | sed 's/\.x3d$//' | sed 's/^\.*\///' | sed 's/c\/x3d-code\/www.web3d.org/www_web3d_org/' | xargs -L 1 -P $PROCESSORS java -d64 -Xss16m -Xmx4096M
+for NEW in `(ls "$@" | grep -v intermediate | grep -v new) | sed 's/\.x3d$/.new.json/'| sed 's/\/c\/x3d-code\/www.web3d.org/www_web3d_org/'`
 do
-	JSON=`dirname $NEW`/`basename $NEW .new.json`.json
-	echo node jsondiff.js $JSON $NEW
-	node jsondiff.js $JSON $NEW
+	JSON=`dirname $NEW | sed 's/www_web3d_org/\/c\/x3d-code\/www.web3d.org/' `/`basename $NEW .new.json`.json
+	if [ -n "`node jsondiff.js $JSON $NEW`" ]
+	then
+		echo ================================================================================
+		echo node jsondiff.js $JSON $NEW
+		node jsondiff.js $JSON $NEW
+	fi
 done
 
-(ls "$@" | grep -v intermediate | grep -v new) | sed 's/\.x3d$/.new.x3d/' | xargs ls 2>&1| grep "No such file or directory" | awk '{ print $4; }'| sed "s/^'\.\///" | sed "s/\.new\.x3d':/.sail.js/" | sed "s/^'//" | xargs -L 1 -P 8 jjs -Dnashorn.args="-classpath ${CLASSPATH}"
+(ls "$@" | grep -v intermediate | grep -v new) | sed "s/\.x3d$/.sail.js/" | sed 's/\/c\/x3d-code\/www.web3d.org/www_web3d_org/' | xargs -L 1 -P $PROCESSORS jjs -Dnashorn.args="-classpath ${CLASSPATH}"
 
-for i in `(ls "$@" | grep -v intermediate | grep -v new) | sed 's/\.x3d$/.new.x3d/'`
+for i in `(ls "$@" | grep -v intermediate | grep -v new) | sed 's/\.x3d$/.new.x3d/'| sed 's/\/c\/x3d-code\/www.web3d.org/www_web3d_org/'`
 do
-	echo ================================================================================
-	echo node xmldiff.js `basename $i .new.x3d`.x3d $i
-	node xmldiff.js `basename $i .new.x3d`.x3d $i
+	X3D=`dirname $i | sed 's/www_web3d_org/\/c\/x3d-code\/www.web3d.org/' `/`basename $i .new.x3d`.X3d
+	if [ -n "`node xmldiff.js $X3D $i`" ]
+	then
+		echo ================================================================================
+		echo node xmldiff.js $X3D $i
+		node xmldiff.js $X3D $i
+	fi
 done
