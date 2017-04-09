a692009a1fc68fe1e205f30500b0096163afb21f
John Carlson <yottzumm@gmail.com>
2017-04-08 20:54:19 -0400

    This files were deleted or updated

--- JavaScriptSerializer.js
+++ JavaScriptSerializer.js
@@ -34,7 +34,7 @@ JavaScriptSerializer.prototype = {
 
 		str += "load('X3Dautoclass.js');\n";
 		str += "var ConfigurationProperties = Packages.org.web3d.x3d.jsail.ConfigurationProperties;\n";
-		str += "ConfigurationProperties.showDefaultAttributes = true;\n";
+		str += "ConfigurationProperties.showDefaultAttributes = false;\n";
 		str += "ConfigurationProperties.indentCharacter = ConfigurationProperties.indentCharacter_DEFAULT;\n";
 		str += "ConfigurationProperties.indentIncrement = ConfigurationProperties.indentIncrement_DEFAULT;\n";
 		str += "ConfigurationProperties.setX3dCanonicalForm();\n";
