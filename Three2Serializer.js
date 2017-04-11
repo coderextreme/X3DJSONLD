abc4e90550e652b7e14b284dd39f5faf014edc4b
John Carlson <yottzumm@gmail.com>
2017-04-11 13:59:26 -0400

    Checked in more three-x3d-load changes.  Seems to work with JSON

--- Three2Serializer.js
+++ Three2Serializer.js
@@ -6,227 +6,266 @@ function ThreeSerializer () {
 
 ThreeSerializer.prototype = {
 	serializeToString : function(json, element, clazz, mapToMethod, fieldTypes) {
-		let obj = parseObject(false, "Scene", fieldTypes, json["X3D"]["Scene"]);
-		console.log(JSON.stringify(obj));
+		let parentobj = {};
+		parentobj.children = [];
+		parentobj.string = "X3D";
+		let obj = this.parseObject(parentobj, "Scene", fieldTypes, json["X3D"]["Scene"]);
+		this.deltree(obj);
+		console.log(JSON.stringify(obj, null, 2));
 		// dump after we find first scene
 		return JSON.stringify(obj, null, 2);
 	},
 	parseFromString : function(data) {
 		let fieldTypes = require('./fieldTypes.js');
-		return  parseObject(true, "Scene", fieldTypes, JSON.parse(data)["X3D"]["Scene"]);
-	}
-}
+		let parentobj = {};
+		parentobj.children = [];
+		parentobj.string = "X3D";
+		return  this.parseObject(parentobj, "Scene", fieldTypes, JSON.parse(data)["X3D"]["Scene"]);
+	},
+	deltree : function (tree) {
+		if (typeof tree === 'object') {
+			delete tree.parent;
+			for (let t in tree) {
+				this.deltree(tree[t]);
+			}
+		}
+	},
+	parseSubObject : function(attrType, type, values) {
+		if (attrType.startsWith("MF")) {
+			let newArray = [];
+			for (let j = 0; j < values.length;) {
+				let newObject = {};
+				for (let letter in type) {
+					newObject[type[letter]] = parseFloat(values[j++]);
+				}
+				newArray.push(newObject);
+			}
+			return newArray;
 
-function parseSubObject(attrType, type, values) {
-	if (attrType.startsWith("MF")) {
-		let newArray = [];
-		for (let j = 0; j < values.length;) {
+		} else {
 			let newObject = {};
+			let j = 0;
 			for (let letter in type) {
 				newObject[type[letter]] = parseFloat(values[j++]);
 			}
-			newArray.push(newObject);
+			return newObject;
 		}
-		return newArray;
-
-	} else {
-		let newObject = {};
-		let j = 0;
-		for (let letter in type) {
-			newObject[type[letter]] = parseFloat(values[j++]);
-		}
-		return newObject;
-	}
-}
-
-function parseSubArray(attrType, numpersub, values) {
-	if (numpersub > 1 && attrType.startsWith("MF")) {
-		let newArrays = [];
-		for (let j = 0; j < values.length;) {
-			let newArray = [];
-			for (let i = 0; i < numpersub; i++) {
-				if (attrType === "MFString") {
-					newArray[i] = values[j++];
-				} else {
-					newArray[i] = parseFloat(values[j++]);
-				}
-			}
-			newArrays.push(newArray);
-		}
-		return newArrays;
-	} else if (numpersub === 0) { // index
-		let newArrays = [];
-		let newArray = [];
-		let i = 0;
-		for (let j = 0; j < values.length;) {
-			if (values[j] == -1) {
-				newArrays.push(newArray);
-				newArray = [];
-				j++;
-				i = 0;
-			} else {
-				if (attrType === "MFString") {
-					newArray[i++] = values[j++];
-				} else {
-					newArray[i++] = parseFloat(values[j++]);
-				}
-			}
-		}
-		return newArrays;
-	} else {
-		return values;
-	}
-}
-
-function parseObject(setParent, name, fieldTypes, json, containerField) {
-	let obj = {};
-	let children = [];
-	obj["string"] = name;
-	if (name != "Scene") {
-		obj["string"] = name.toLowerCase();
-		obj["nodeType"] = name.toLowerCase();
-		if (containerField !== obj["nodeType"]) {
-			obj["string"] = containerField+" "+obj["nodeType"];
-		}
-	}
-	for (let field in json) {
-		if (field.startsWith("@")) {
-			if (field == "@xmlns:xsd" || field == "@xsd:noNamespaceSchemaLocation") {
-				continue;
-			}
-			let attr = field.substr(1);
-			if (attr !== '@containerField') {
-				// look at object model
-				let attrType = "SFString";
-				if (typeof fieldTypes[name] !== 'undefined') {
-					attrType = fieldTypes[name][attr];
-				}
-
-				let value = "";
-				if (json[field] === 'NULL') {
-					value = null;
-				} else if (attrType === "SFString") {
-					value = json[field];
-				} else if (attrType === "SFInt32") {
-					value = json[field];
-				} else if (attrType === "SFFloat") {
-					value = json[field];
-				} else if (attrType === "SFDouble") {
-					value = json[field];
-				} else if (attrType === "SFBool") {
-					value = json[field];
-				} else if (attrType === "MFString") {
-					value = parseSubArray(attrType, 1, json[field]);
-				} else if (attrType === "MFInt32") {
-					if (attr.endsWith("Index")) {
-						value = parseSubArray(attrType, 0, json[field]);
+	},
+	parseSubArray : function(attrType, numpersub, values) {
+		if (numpersub > 1 && attrType.startsWith("MF")) {
+			let newArrays = [];
+			for (let j = 0; j < values.length;) {
+				let newArray = [];
+				for (let i = 0; i < numpersub; i++) {
+					if (attrType === "MFString") {
+						newArray[i] = values[j++];
 					} else {
-						value = parseSubArray(attrType, 1, json[field]);
+						newArray[i] = parseFloat(values[j++]);
 					}
-				} else if (
-					attrType === "MFImage"||
-					attrType === "SFImage") {
-					value = parseSubArray(attrType, 1, json[field]);
-				} else if (
-					attrType === "SFColor"||
-					attrType === "MFColor") {
-					value = parseSubObject(attrType, "rgb", json[field]);
-				} else if (
-					attrType === "SFColorRGBA"||
-					attrType === "MFColorRGBA") {
-					value = parseSubObject(attrType, "rgba", json[field]);
-				} else if (
-					attrType === "SFVec2d"||
-					attrType === "MFVec2d"||
-					attrType === "SFVec2f"||
-					attrType === "MFVec2f") {
-					value = parseSubObject(attrType, "xy", json[field]);
-				} else if (
-					attrType === "SFVec3d"||
-					attrType === "MFVec3d"||
-					attrType === "SFVec3f"||
-					attrType === "MFVec3f") {
-					value = parseSubObject(attrType, "xyz", json[field]);
-				} else if (
-					attrType === "SFVec4d"||
-					attrType === "MFVec4d"||
-					attrType === "SFVec4f"||
-					attrType === "MFVec4f") {
-					value = parseSubObject(attrType, "xyzw", json[field]);
-				} else if (
-					attrType === "SFRotation"||
-					attrType === "MFRotation") {
-					value = parseSubObject(attrType, "xyzw", json[field]);
-				} else if (
-					attrType === "SFMatrix3d"||
-					attrType === "MFMatrix3d"||
-					attrType === "SFMatrix3f"||
-					attrType === "MFMatrix3f") {
-					value = parseSubArray(attrType, 9, json[field]);
-				} else if (
-					attrType === "SFMatrix4d"||
-					attrType === "MFMatrix4d"||
-					attrType === "SFMatrix4f"||
-					attrType === "MFMatrix4f") {
-					value = parseSubArray(attrType, 16, json[field]);
-				} else if (
-					attrType === "MFFloat") {
-					value = parseSubArray(attrType, 1, json[field]);
-				} else if (
-					attrType === "MFDouble") {
-					value = parseSubArray(attrType, 1, json[field]);
-				} else if (attrType === "MFBool") {
-					value = parseSubArray(attrType, 1, json[field]);
+				}
+				newArrays.push(newArray);
+			}
+			return newArrays;
+		} else if (numpersub === 0) { // index
+			let newArrays = [];
+			let newArray = [];
+			let i = 0;
+			for (let j = 0; j < values.length;) {
+				if (values[j] == -1) {
+					newArrays.push(newArray);
+					newArray = [];
+					j++;
+					i = 0;
 				} else {
-					value = json[field];
-				}
-				obj[attr] = value;
-
-				// Now do something with the attribute
-				if (obj.DEF) {
-					obj.string += " DEF "+obj.DEF;
-				}
-				if (obj.USE) {
-					obj.string += " USE "+obj.USE;
-				}
-				if (obj.point) {
-					obj.points = obj.point;
+					if (attrType === "MFString") {
+						newArray[i++] = values[j++];
+					} else {
+						newArray[i++] = parseFloat(values[j++]);
+					}
 				}
 			}
-		} else if (field === "-children") {
-			let node = json[field];
-			children.push(parseObject(setParent, field, fieldTypes, node, ""));
-			if (setParent) {
-				children[children.length-1].parent = obj;
-			}
-		} else if (field.startsWith("-")) {
-			// container field
-			let node = json[field];
-			let container = parseObject(setParent, field, fieldTypes, node, containerField === "appearance" ? containerField : field.substr(1));
-			children.push(container);
-			if (setParent) {
-				children[children.length-1].parent = obj;
-			}
-		} else if (field === "#comment") {
-			children.push("/*"+json[field]+"*/");
-		} else if (field === "#sourceText") {
-			children.push("<script>"+json[field].join("\r\n")+'</Script>');
+			return newArrays;
 		} else {
-			// array
-			let node = json[field];
-			if (typeof node === 'object') {
-				let tmpobj = obj;
-				obj = parseObject(setParent, field, fieldTypes, node, containerField);
-				if (setParent) {
-					obj.parent = tmpobj;
+			return values;
+		}
+	},
+	parseObject : function(parentobj, name, fieldTypes, json, containerField) {
+		let obj = {};
+		obj.string = name;
+		if (name != "Scene") {
+			obj.string = name.toLowerCase();
+			obj.nodeType = name.toLowerCase();
+			if (containerField !== obj.nodeType) {
+				obj.string = containerField+" "+obj.nodeType;
+			}
+		}
+		obj.children = [];
+		let retobj = obj;
+		for (let field in json) {
+			if (field.startsWith("@")) {
+				if (field == "@xmlns:xsd" || field == "@xsd:noNamespaceSchemaLocation") {
+					continue;
+				}
+				let attr = field.substr(1);
+				if (attr !== '@containerField') {
+					// look at object model
+					let attrType = "SFString";
+					if (typeof fieldTypes[name] !== 'undefined') {
+						attrType = fieldTypes[name][attr];
+					}
+
+					let value = "";
+					if (json[field] === 'NULL') {
+						value = null;
+					} else if (attrType === "SFString") {
+						value = json[field];
+					} else if (attrType === "SFInt32") {
+						value = json[field];
+					} else if (attrType === "SFFloat") {
+						value = json[field];
+					} else if (attrType === "SFDouble") {
+						value = json[field];
+					} else if (attrType === "SFBool") {
+						value = json[field];
+					} else if (attrType === "MFString") {
+						value = this.parseSubArray(attrType, 1, json[field]);
+					} else if (attrType === "MFInt32") {
+						if (attr.endsWith("Index")) {
+							value = this.parseSubArray(attrType, 0, json[field]);
+						} else {
+							value = this.parseSubArray(attrType, 1, json[field]);
+						}
+					} else if (
+						attrType === "MFImage"||
+						attrType === "SFImage") {
+						value = this.parseSubArray(attrType, 1, json[field]);
+					} else if (
+						attrType === "SFColor"||
+						attrType === "MFColor") {
+						value = this.parseSubObject(attrType, "rgb", json[field]);
+					} else if (
+						attrType === "SFColorRGBA"||
+						attrType === "MFColorRGBA") {
+						value = this.parseSubObject(attrType, "rgba", json[field]);
+					} else if (
+						attrType === "SFVec2d"||
+						attrType === "MFVec2d"||
+						attrType === "SFVec2f"||
+						attrType === "MFVec2f") {
+						value = this.parseSubObject(attrType, "xy", json[field]);
+					} else if (
+						attrType === "SFVec3d"||
+						attrType === "MFVec3d"||
+						attrType === "SFVec3f"||
+						attrType === "MFVec3f") {
+						value = this.parseSubObject(attrType, "xyz", json[field]);
+					} else if (
+						attrType === "SFVec4d"||
+						attrType === "MFVec4d"||
+						attrType === "SFVec4f"||
+						attrType === "MFVec4f") {
+						value = this.parseSubObject(attrType, "xyzw", json[field]);
+					} else if (
+						attrType === "SFRotation"||
+						attrType === "MFRotation") {
+						value = this.parseSubObject(attrType, "xyzw", json[field]);
+					} else if (
+						attrType === "SFMatrix3d"||
+						attrType === "MFMatrix3d"||
+						attrType === "SFMatrix3f"||
+						attrType === "MFMatrix3f") {
+						value = this.parseSubArray(attrType, 9, json[field]);
+					} else if (
+						attrType === "SFMatrix4d"||
+						attrType === "MFMatrix4d"||
+						attrType === "SFMatrix4f"||
+						attrType === "MFMatrix4f") {
+						value = this.parseSubArray(attrType, 16, json[field]);
+					} else if (
+						attrType === "MFFloat") {
+						value = this.parseSubArray(attrType, 1, json[field]);
+					} else if (
+						attrType === "MFDouble") {
+						value = this.parseSubArray(attrType, 1, json[field]);
+					} else if (attrType === "MFBool") {
+						value = this.parseSubArray(attrType, 1, json[field]);
+					} else {
+						value = json[field];
+					}
+					obj[attr] = value;
+
+					// Now do something with the attribute
+					if (obj.DEF) {
+						obj.string += " DEF "+obj.DEF;
+					}
+					if (obj.USE) {
+						obj.string += " USE "+obj.USE;
+					}
+					if (obj.point) {
+						obj.points = obj.point;
+					}
+				}
+			} else if (field === "#comment") {
+				obj.children.push("/*"+json[field]+"*/");
+			} else if (field === "#sourceText") {
+				obj.children.push("<script>"+json[field].join("\r\n")+'</Script>');
+			} else {
+				let node = json[field];
+				if (typeof node === 'object') {
+					let cf = "";
+					if (field === "-children") {
+						cf = "";
+					} else if (field.startsWith("-")) {
+						cf = containerField === "appearance"
+							? containerField
+							: field.substr(1);
+					} else {
+						cf = containerField;
+					}
+					let container = this.parseObject(obj, field, fieldTypes, node, cf);
+
+					let ct = container.nodeType || "-";
+					let nct = parseInt(ct.trim());
+					let ict = ct.startsWith("-") || !isNaN(nct); // illegal if true
+					/*
+					let ot =  obj.nodeType || "-";
+					let not = parseInt(ot.trim());
+					let iot =  ot.startsWith("-") || !isNaN(not); // illegal if true 
+					*/
+					/*
+					if (ict && iot) {
+						parentobj.children = parentobj.children.concat(container.children);
+						console.log("A", JSON.stringify(parentobj, null, 2));
+					} else
+					*/
+					if (ict) {
+						obj.children = obj.children.concat(container.children);
+						container.parent = obj;
+						// console.log("B", obj);
+					/*
+					} else if (iot) {
+						parentobj.children = parentobj.children.concat(container.children);
+						console.log("C", JSON.stringify(parentobj, null, 2));
+					*/
+					} else if (obj.nodeType) {
+						obj.children.push(container);
+						container.parent = obj;
+						// console.log("D", obj);
+					/*
+					} else {
+						obj.children.push(container);
+						container.parent = obj;
+						console.log("E", JSON.stringify(obj, null, 2));
+					*/
+					}
+					/*
+					console.log(ct, ict, ot, iot);
+					*/
 				}
 			}
 		}
+		return obj;
 	}
-	if (children.length > 0) {
-		obj["children"] = children;
-	}
-	return obj;
 }
 
 
