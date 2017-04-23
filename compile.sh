8fd1ef75052538cbc2d1227edc03fee9ea63f549
John Carlson <yottzumm@gmail.com>
2017-04-22 22:03:47 -0400

    Added more stack space to compiler

--- compile.sh
+++ compile.sh
@@ -4,5 +4,5 @@
 
 . ./classpath
 
-ls *.java | xargs -L 1 -P 8 javac -J-Xss16m -J-Xmx4096M
-sh noncompiled.sh | xargs -L 1 -P 8 javac -J-Xss16m -J-Xmx4096M
+ls *.java | xargs -L 1 -P 8 javac -J-Xss1g -J-Xmx4g
+sh noncompiled.sh | xargs -L 1 -P 8 javac -J-Xss1g -J-Xmx4g
--- compilejava.sh
+++ compilejava.sh
@@ -5,9 +5,9 @@
 jar -cMf GoodJSON.zip classpath
 jar -cMf DiffJSON.zip classpath
 . ./classpath
-(ls *.java | grep -v RunSaxon; find www_web3d_org/ -name '*.java') | xargs -L 1 -P 8 javac -J-Xss16m -J-Xmx4096M 2> javacerrors.txt
+(ls *.java | grep -v RunSaxon; find www_web3d_org/ -name '*.java') | xargs -L 1 -P 8 javac -J-Xss1g -J-Xmx4g 2> javacerrors.txt
 
-(ls *.class | grep -v RunSaxon; find www_web3d_org/ -name '*.class') | sed 's/\.class$//' | xargs -L 1 -P 8 java -d64 -Xss16m -Xmx4096M 2> javaerrors.txt
+(ls *.class | grep -v RunSaxon; find www_web3d_org/ -name '*.class') | sed 's/\.class$//' | xargs -L 1 -P 8 java -d64 -Xss1g -Xmx4g 2> javaerrors.txt
 for NEW in `find . -name '*.new.json'`
 do
 	JSON=`dirname $NEW`/`basename $NEW .new.json`.json
