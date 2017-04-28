e19995eeb16c9dc281a1f2d6ae2694bac61ab93c
John Carlson <yottzumm@gmail.com>
2017-04-28 04:17:01 -0400

    Added X3dToES5.xslt

--- clean.sh
+++ clean.sh
@@ -2,4 +2,4 @@
 
 # clean up test files
 
-rm -f `ls *py | grep -v classes.py` *.sail.js *.x3d.new `ls *.class | grep -v RunSaxon` *.new.x3d *.intermediate.* *.diff *.x3d*.json *three.js *.java.bak *roundtrip* *Canonical* *.run*.txt
+rm -f `ls *py | grep -v classes.py` *.sail.js *.x3d.new `ls *.class | grep -v RunSaxon` *.new.x3d *.intermediate.* *.diff *.x3d*.json *three.js *.bak *roundtrip* *Canonical* *.run*.txt
--- execute.sh
+++ execute.sh
@@ -7,4 +7,4 @@
 
 ls *.class | grep -v '\$' | grep -v RunSaxon | sed 's/.class$//' | sed 's/\//./g' | xargs -L 1 -P 8 java -Xss16m -Xmx4096M
 sh nonexecuted.sh | xargs -L 1 -P 8 java -Xss16m -Xmx4096M
-sh nonexecutedjs.sh | xargs -L 1 -P 8 jjs -cp "${NASHORN_CLASSPATH}"
+sh nonexecutedjs.sh | xargs -L 1 -P 8 jjs -J-Xss1g -J-Xmx4g -cp "${NASHORN_CLASSPATH}"
