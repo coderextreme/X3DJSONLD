6b306577f15579d43ac7a38998e30921240c3a6b
John Carlson <yottzumm@gmail.com>
2017-04-30 07:10:58 -0400

    These examples seem to work now added error logging

--- runAndSend.js
+++ runAndSend.js
@@ -2,7 +2,7 @@ var runsaxon = require('./allsaxon');
 var fs = require("fs");
 
 function runAndSend(infile) {
-	console.log('converting', infile);
+	console.error('converting', infile);
 	runsaxon([infile]);
 	var outfile = infile.substr(0, infile.lastIndexOf("."))+".json";
 	if (outfile.lastIndexOf("savage.nps.edu") >= 0) {
@@ -11,10 +11,10 @@ function runAndSend(infile) {
 	if (outfile.lastIndexOf("www.web3d.org") >= 0) {
 		outfile = outfile.substring(outfile.lastIndexOf("www.web3d.org"));
 	}
-	console.log('reading 2 ', outfile);
+	console.error('reading 2 ', outfile);
 	var content = fs.readFileSync(outfile);
 	var json = JSON.parse(content.toString());
-	console.log('sending back', json);
+	console.error('sending back', json);
 	return json;
 }
 
