9535406531217c0ec37d1ae489038914ec19ae2f
John Carlson <yottzumm@gmail.com>
2017-05-06 03:18:14 -0400

    added x3d2py.sh

---
+++ x3d2py.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Run the Test Suite
+
+# accepts files with .x3d extension
+export PROCESSORS=${PROCESSORS-8}
+
+. ./classpath
+
+javac RunSaxon.java
+
+ls *.x3d | xargs -P $PROCESSORS java RunSaxon ---overwrite | xargs -P $PROCESSORS ${NODE} json2py.js
