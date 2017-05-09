92d2c8d798f1ee0a40405000ae66c8b665e6fd77
John Carlson <yottzumm@gmail.com>
2017-05-08 21:22:57 -0400

    Fixed up fuzz generator

--- clean.sh
+++ clean.sh
@@ -4,4 +4,4 @@
 
 # clean up test files
 
-rm -f `ls *py | grep -v classes.py` *.sail.js *.x3d.new `ls *.class *.java | grep -v RunSaxon` *.new.x3d *.intermediate.* *.diff *.x3d*.json *three.js *.bak *roundtrip* *Canonical* *.run*.txt [0-9]* *.new.json `ls *.json | grep -v Schema | grep -v package.json`
+rm -f `ls *py | grep -v classes.py | grep -v generate` *.sail.js *.x3d.new `ls *.class *.java | grep -v RunSaxon` *.new.x3d *.intermediate.* *.diff *.x3d*.json *three.js *.bak *roundtrip* *Canonical* *.run*.txt [0-9]* *.new.json `ls *.json | grep -v Schema | grep -v package.json`
