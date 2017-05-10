84f1656eab7412618279edd27c52985eafa8144b
John Carlson <yottzumm@gmail.com>
2017-05-10 05:40:22 -0400

    commented out illegal javascript

--- singleroute.js
+++ singleroute.js
@@ -1,1 +1,1 @@
-Removed.  See https://github.com/coderextreme/X3DJSONLD/blob/master/route.js , function test3().
+/* Removed.  See https://github.com/coderextreme/X3DJSONLD/blob/master/route.js , function test3(). */
