d89cb5b03c54e99288b309615121b0691a233adb
John Carlson <yottzumm@gmail.com>
2017-05-12 21:58:06 -0400

    Added ajv-i18n

--- convertJSON.js
+++ convertJSON.js
@@ -7,6 +7,8 @@ var mapToMethod2 = require('./mapToMethod2.js');
 var jsonlint = require('jsonlint');
 var fieldTypes = require('./fieldTypes.js');
 var Ajv = require('ajv');
+var ajv = new Ajv({ allErrors:true});
+var localize = require('ajv-i18n');
 
 var xmldom = require('xmldom');
 var DOMImplementation = new xmldom.DOMImplementation();
@@ -37,6 +39,8 @@ function doValidate(json, validated_version, file, success, failure) {
 		if (!valid) {
 			console.error("================================================================================");
 			console.error("File:", file);
+			localize.en(validated_version.errors);
+		        // error += ajv.errorsText(validated_version.errors, { separator: '\n'});
 			var errs = validated_version.errors;
 			for (var e in errs) {
 				error += "\r\n keyword: " + errs[e].keyword + "\r\n";
@@ -83,7 +87,6 @@ function loadSchema(json, file, doValidate, success, failure) {
 	}
 	var validated_version = validate[version];
         if (typeof validated_version === 'undefined') {
-		var ajv = new Ajv({ allErrors:true});
 		ajv.addFormat("uri", /^(?:[a-z][a-z0-9+\-.]*:)?(?:\/?\/(?:(?:[a-z0-9\-._~!$&'()*+,;=:]|%[0-9a-f]{2})*@)?(?:\[(?:(?:(?:(?:[0-9a-f]{1,4}:){6}|::(?:[0-9a-f]{1,4}:){5}|(?:[0-9a-f]{1,4})?::(?:[0-9a-f]{1,4}:){4}|(?:(?:[0-9a-f]{1,4}:){0,1}[0-9a-f]{1,4})?::(?:[0-9a-f]{1,4}:){3}|(?:(?:[0-9a-f]{1,4}:){0,2}[0-9a-f]{1,4})?::(?:[0-9a-f]{1,4}:){2}|(?:(?:[0-9a-f]{1,4}:){0,3}[0-9a-f]{1,4})?::[0-9a-f]{1,4}:|(?:(?:[0-9a-f]{1,4}:){0,4}[0-9a-f]{1,4})?::)(?:[0-9a-f]{1,4}:[0-9a-f]{1,4}|(?:(?:25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(?:25[0-5]|2[0-4]\d|[01]?\d\d?))|(?:(?:[0-9a-f]{1,4}:){0,5}[0-9a-f]{1,4})?::[0-9a-f]{1,4}|(?:(?:[0-9a-f]{1,4}:){0,6}[0-9a-f]{1,4})?::)|[Vv][0-9a-f]+\.[a-z0-9\-._~!$&'()*+,;=:]+)\]|(?:(?:25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(?:25[0-5]|2[0-4]\d|[01]?\d\d?)|(?:[a-z0-9\-._~!$&'()*+,;=]|%[0-9a-f]{2})*)(?::\d*)?(?:\/(?:[a-z0-9\-._~!$&'()*+,;=:@]|%[0-9a-f]{2})*)*|\/(?:(?:[a-z0-9\-._~!$&'()*+,;=:@]|%[0-9a-f]{2})+(?:\/(?:[a-z0-9\-._~!$&'()*+,;=:@]|%[0-9a-f]{2})*)*)?|(?:[a-z0-9\-._~!$&'()*+,;=:@]|%[0-9a-f]{2})+(?:\/(?:[a-z0-9\-._~!$&'()*+,;=:@]|%[0-9a-f]{2})*)*)?(?:\?(?:[a-z0-9\-._~!$&'()*+,;=:@\/?]|%[0-9a-f]{2})*)?(?:\#(?:[a-z0-9\-._~!$&'()*+,;=:@\/?]|%[0-9a-f]{2})*)?$/i);
 
 		
