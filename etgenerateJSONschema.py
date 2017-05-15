75b91d70b6f8976710be051a57599cbbc5c0c18c
John Carlson <yottzumm@gmail.com>
2017-05-15 04:04:21 -0400

    Changed json-schema.org to https

--- etgenerateJSONschema.py
+++ etgenerateJSONschema.py
@@ -531,7 +531,7 @@ class ClassPrinter:
         return str
 
 code = '''{
-        "$schema": "http://json-schema.org/draft-04/schema#",
+        "$schema": "https://json-schema.org/draft-04/schema#",
         "title": "JSON Schema X3D V3.3",
         "description": "Experimental JSON Schema for X3D V3.3 ",
         "type": "object",
