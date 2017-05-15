3dda98e8dbe33b9af666af73ebbb28600d34563a
John Carlson <yottzumm@gmail.com>
2017-05-15 19:54:04 -0400

    Updated shell scripts

--- don.sh
+++ don.sh
@@ -14,47 +14,22 @@ python classes.py
 echo translating to json
 (ls "$@" | grep -v intermediate | grep -v "\.new") | xargs -P $PROCESSORS java RunSaxon ---silent
 echo translating to ecmascript 5
-(ls "$@" | grep -v intermediate | grep -v "\.new") | xargs -P $PROCESSORS java RunSaxon --X3dToES5.xslt -sail.js
+(ls "$@" | grep -v intermediate | grep -v "\.new") | xargs -P $PROCESSORS java RunSaxon ---silent --X3dToES5.xslt -sail.js
 echo translating to java
-(ls "$@" | grep -v intermediate | grep -v "\.new") | xargs -P $PROCESSORS java RunSaxon --X3dToJava.xslt -java
+(ls "$@" | grep -v intermediate | grep -v "\.new") | xargs -P $PROCESSORS java RunSaxon ---silent --X3dToJava.xslt -java
 echo replacing NeedClassName
 (ls "$@" | grep -v intermediate | grep -v "\.new") | xargs -P $PROCESSORS sh replaceclass.sh
 echo compiling
 (ls "$@" | grep -v intermediate | grep -v "\.new") | sed 's/\.x3d$/.java/' | xargs -L 1 -P $PROCESSORS javac -J-Xss1g -J-Xmx4g
-echo running
-for i in `ls "$@" | grep -v intermediate | grep -v "\.new") | sed 's/\.x3d$//' | sed 's/^\.*\///'`
+echo running java
+for i in `ls "$@" | grep -v intermediate | grep -v "\.new" | sed 's/\.x3d$//' | sed 's/^\.+\///'`
 do
-	pushd `dirname $i`
+	pushd `dirname $i` > /dev/null
+	echo
+	echo $i
 	java -d64 -Xss1g -Xmx4g `basename $i` # sh runToError.sh
-done
-echo diffing
-for NEW in `(ls "$@" | grep -v intermediate | grep -v "\.new") | sed 's/\.x3d$/.new.json/'| sed 's/\/c\/x3d-code\/www.web3d.org/www_web3d_org/'`
-do
-	JSON=`dirname $NEW | sed 's/www_web3d_org/\/c\/x3d-code\/www.web3d.org/' `/`basename $NEW .new.json`.json
-	if [ -e $NEW ]
-	then if [ -n "`${NODE} jsondiff.js $JSON $NEW`" ]
-		then
-			echo ================================================================================
-			echo ${NODE} jsondiff.js $JSON $NEW
-			${NODE} jsondiff.js $JSON $NEW
-		fi
-	fi
+	popd > /dev/null
 done
 
 echo running jjs
 (ls "$@" | grep -v intermediate | grep -v "\.new") | sed "s/\.x3d$/.sail.js/" | sed 's/\/c\/x3d-code\/www.web3d.org/www_web3d_org/' | xargs -L 1 -P $PROCESSORS jjs -J-Xss1g -J-Xmx4g -cp "${NASHORN_CLASSPATH}"
-
-echo diffing
-for i in `(ls "$@" | grep -v intermediate | grep -v "\.new") | sed 's/\.x3d$/.new.x3d/'| sed 's/\/c\/x3d-code\/www.web3d.org/www_web3d_org/'`
-do
-	X3D=`dirname $i | sed 's/www_web3d_org/\/c\/x3d-code\/www.web3d.org/' `/`basename $i .new.x3d`.X3d
-	if [ -e $i ]
-	then
-		if [ -n "`${NODE} xmldiff.js $X3D $i`" ]
-		then
-			echo ================================================================================
-			echo ${NODE} xmldiff.js $X3D $i
-			${NODE} xmldiff.js $X3D $i
-		fi
-	fi
-done
--- unknown.sh
+++ unknown.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-grep UNKNOWN *runerr* | sed -e 's/.*details: //' -e 's/ TO /\nTO /'| grep UNKNOWN|sort -u
+ls *runerr* | xargs grep UNKNOWN | sed -e 's/.*details: //' -e 's/ TO /\nTO /'| grep UNKNOWN|sort -u
