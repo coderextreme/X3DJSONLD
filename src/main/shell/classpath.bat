c68a59da0f8a177cd766545ab429d0d7ebb55e37
John Carlson <yottzumm@gmail.com>
2017-05-16 16:57:07 -0400

    Check in in new location

---
+++ src/main/shell/classpath.bat
@@ -0,0 +1,4 @@
+set X3DJSONLD=C:\Users\coderextreme\pythonSAI
+set CLASSPATH=.;%X3DJSONLD%\saxon9he.jar;%X3DJSONLD%\jslint4java-2.0.5.jar;%X3DJSONLD%\json-schema-validator-2.2.6-lib.jar;%X3DJSONLD%\X3DJSAIL.3.3.full.jar
+set PYTHONSAI=..\pythonSAI
+set NASHORN_CLASSPATH=%PYTHONSAI%\X3DJSAIL.3.3.full.jar;.;%PYTHONSAI%\saxon9he.jar;%PYTHONSAI%\jslint4java-2.0.5.jar;%PYTHONSAI%\json-schema-validator-2.2.6-lib.jar
