bc222a69b09ca0cdebad5dd0c04b610e4f26e48c
John Carlson <yottzumm@gmail.com>
2017-05-16 11:20:19 -0400

    committed

--- src/main/node/allsaxon.js
+++ src/main/node/allsaxon.js
@@ -3,7 +3,7 @@ java.options.push("-Djava.awt.headless=true");
 java.options.push("-Xmx1000m");
 //java.options.push('-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005');
 java.classpath.push("saxon9he.jar");
-java.classpath.push(".");
+java.classpath.push("target");
 
 function translate(infiles) {
 	console.log("Calling static java method", infiles);
