033d92c175a7f38cb751e653d6b597ae2a90bff5
John Carlson <yottzumm@gmail.com>
2017-05-16 17:24:23 -0400

    removed excess files

--- src/main/shell/several.sh
+++ src/main/shell/several.sh
@@ -10,13 +10,14 @@ export PROCESSORS=${PROCESSORS-8}
 python ../python/classes.py
 
 X3DTOOUTPUT='s/\/x3d\//\/new\/json\//' 
+OUTPUTTOX3D='s/\/new\/json/\/x3d/'
 
 
 (ls -d "$@" | grep -v intermediate | grep -v "\.new") | xargs -P $PROCESSORS java net.coderextreme.RunSaxon ---overwrite ---silent --../xslt/X3dToJSON.xslt -json | xargs -P $PROCESSORS ${NODE} ${NODEDIR}/json2all.js
 
 for i in `(ls -d "$@" | grep -v intermediate | grep -v "\.new") | sed  -e 's/\.x3d$/.x3d.new/' -e $X3DTOOUTPUT`
 do
-	${NODE} ${NODEDIR}/xmldiff.js `dirname $i | sed 's/\/new\/json/\/x3d/'`/`basename $i .x3d.new`.x3d $i
+	${NODE} ${NODEDIR}/xmldiff.js `dirname $i | sed $OUTPUTTOX3D`/`basename $i .x3d.new`.x3d $i
 done
 
 for i in `ls -d "$@" | grep -v intermediate | grep -v "\.new" | sed -e 's/\.x3d$/.java/' -e 's/\/x3d\//\/java\/net\/coderextreme\/json\//' | xargs ls -d`
@@ -47,7 +48,7 @@ done
 
 for i in `ls -d "$@" | grep -v intermediate | grep -v "\.new" |  sed -e 's/\.x3d$/.new.json.intermediate.x3d/' -e $X3DTOOUTPUT`
 do
-	${NODE} ${NODEDIR}/xmldiff.js `dirname $i  | sed 's/\/new\/json/\/x3d/'`/`basename $i .new.json.intermediate.x3d`.x3d $i
+	${NODE} ${NODEDIR}/xmldiff.js `dirname $i  | sed $OUTPUTTOX3D`/`basename $i .new.json.intermediate.x3d`.x3d $i
 done
 
 
