1ddfc00eaf42435a7f51c3948393b7e52bc9368c
John Carlson <yottzumm@gmail.com>
2017-05-16 19:20:31 -0400

    Removed JSON files

--- src/main/shell/several.sh
+++ src/main/shell/several.sh
@@ -9,18 +9,16 @@ export PROCESSORS=${PROCESSORS-8}
 
 python ../python/classes.py
 
-X3DTOOUTPUT='s/\/orig\//\/new\/json\//' 
-X3DTODOMSER='s/\/orig\//\/new\/orig\//' 
-OUTPUTTOX3D='s/\/new\/json/\/orig/'
-DOMSERTOORG='s/\/new//'
+ORIGTOOUTPUT='s/\/orig\//\/new\/orig\//' 
+OUTPUTTOORIG='s/\/new\/orig/\/orig/'
 STYLESHEETDIR=../lib/stylesheets
 
 
 (ls -d "$@" | grep -v intermediate | grep -v "\.new") | xargs -P $PROCESSORS java net.coderextreme.RunSaxon ---overwrite --${STYLESHEETDIR}/X3dToJSON.xslt -json  | xargs -P $PROCESSORS ${NODE} ${NODEDIR}/json2all.js
 
-for i in `(ls -d "$@" | grep -v intermediate | grep -v "\.new") | sed  -e 's/\.x3d$/.x3d.new/' -e $X3DTODOMSER`
+for i in `(ls -d "$@" | grep -v intermediate | grep -v "\.new") | sed  -e 's/\.x3d$/.x3d.new/' -e $ORIGTOOUTPUT`
 do
-	${NODE} ${NODEDIR}/xmldiff.js `dirname $i | sed $DOMSERTOORG`/`basename $i .x3d.new`.x3d $i
+	${NODE} ${NODEDIR}/xmldiff.js `dirname $i | sed $OUTPUTTOORIG`/`basename $i .x3d.new`.x3d $i
 done
 
 for i in `ls -d "$@" | grep -v intermediate | grep -v "\.new" | sed -e 's/\.x3d$/.java/' -e 's/\/orig\//\/java\/net\/coderextreme\/orig\//' | xargs ls -d`
@@ -41,16 +39,16 @@ do
 	popd > /dev/null
 done
 
-for NEW in `ls -d "$@" | grep -v intermediate | grep -v "\.new" | sed -e 's/\.x3d$/.new.json/' -e $X3DTOOUTPUT`
+for NEW in `ls -d "$@" | grep -v intermediate | grep -v "\.new" | sed -e 's/\.x3d$/.new.json/' -e $ORIGTOOUTPUT`
 do
-	JSON=`dirname $NEW | sed -e $DOMSERTOORG `/`basename $NEW .new.json`.json
+	JSON=`dirname $NEW | sed -e $OUTPUTTOORIG `/`basename $NEW .new.json`.json
 	${NODE} ${NODEDIR}/jsondiff.js $JSON $NEW
 done
 
 
-for i in `ls -d "$@" | grep -v intermediate | grep -v "\.new" |  sed -e 's/\.x3d$/.new.json.intermediate.x3d/' -e $X3DTOOUTPUT`
+for i in `ls -d "$@" | grep -v intermediate | grep -v "\.new" |  sed -e 's/\.x3d$/.new.json.intermediate.x3d/' -e $ORIGTOOUTPUT`
 do
-	${NODE} ${NODEDIR}/xmldiff.js `dirname $i  | sed $OUTPUTTOX3D`/`basename $i .new.json.intermediate.x3d`.x3d $i
+	${NODE} ${NODEDIR}/xmldiff.js `dirname $i  | sed $OUTPUTTOORIG`/`basename $i .new.json.intermediate.x3d`.x3d $i
 done
 
 
@@ -63,7 +61,7 @@ do
 	popd > /dev/null
 done
 
-for NEW in `ls -d "$@" | grep -v intermediate | grep -v "\.new" | sed -e 's/\.x3d$/.new.x3d/' -e $X3DTOOUTPUT`
+for NEW in `ls -d "$@" | grep -v intermediate | grep -v "\.new" | sed -e 's/\.x3d$/.new.x3d/' -e $ORIGTOOUTPUT`
 do
 	X3D=`dirname $NEW | sed 's/\/new//' `/`basename $NEW .new.x3d`.x3d
 	${NODE} ${NODEDIR}/xmldiff.js $X3D $NEW
