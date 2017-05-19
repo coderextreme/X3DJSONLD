92654af9b675ddaf23278a9f684d23c2ec3c6872
John Carlson <yottzumm@gmail.com>
2017-05-18 23:54:04 -0400

    Worked on stylesheets.  No longer need FixNeedClassName.sh for RunSaxon.java

--- src/main/shell/don.sh
+++ src/main/shell/don.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-
 # Run the Test Suite
 
 # accepts files with .x3d extension
@@ -8,28 +7,23 @@ export PROCESSORS=${PROCESSORS-8}
 
 . ./classpath
 
-javac RunSaxon.java
-python classes.py
+python ../python/classes.py
 
 echo translating to json
-(ls "$@" | grep -v intermediate | grep -v "\.new") | xargs -P $PROCESSORS java RunSaxon ---silent
+(ls "$@" | grep -v intermediate | grep -v "\.new") | xargs -P $PROCESSORS java net.coderextreme.RunSaxon ---silent --../lib/stylesheets/X3dToJson.xslt ---../
 echo translating to ecmascript 5
-(ls "$@" | grep -v intermediate | grep -v "\.new") | xargs -P $PROCESSORS java RunSaxon ---silent --X3dToES5.xslt -sail.js
+(ls "$@" | grep -v intermediate | grep -v "\.new") | xargs -P $PROCESSORS java net.coderextreme.RunSaxon ---overwrite ---silent --../lib/stylesheets/X3dToES5.xslt -sail.js ---../nashorn/net/x3djsonld/
 echo translating to java
-(ls "$@" | grep -v intermediate | grep -v "\.new") | xargs -P $PROCESSORS java RunSaxon ---overwrite ---silent --X3dToJava.xslt -java
+(ls "$@" | grep -v intermediate | grep -v "\.new") | xargs -P $PROCESSORS java net.coderextreme.RunSaxon ---silent --../lib/stylesheets/X3dToJava.xslt -java ---../java/net/x3djsonld/
 echo replacing NeedClassName
-(ls "$@" | grep -v intermediate | grep -v "\.new") | xargs -P $PROCESSORS sh replaceclass.sh
+(ls "$@" | grep -v intermediate | grep -v "\.new") | xargs -P $PROCESSORS sh FixNeedClassName.sh
 echo compiling
-(ls "$@" | grep -v intermediate | grep -v "\.new") | sed 's/\.x3d$/.java/' | xargs -L 1 -P $PROCESSORS javac -J-Xss1g -J-Xmx4g
+pushd ../java/net/x3djsonld/data
+find . -name '*.java' | xargs -L 1 -P $PROCESSORS javac -J-Xss1g -J-Xmx4g
 echo running java
-for i in `ls "$@" | grep -v intermediate | grep -v "\.new" | sed 's/\.x3d$//' | sed 's/^\.+\///'`
-do
-	pushd `dirname $i` > /dev/null
-	echo
-	echo $i
-	java -d64 -Xss1g -Xmx4g `basename $i` # sh runToError.sh
-	popd > /dev/null
-done
-
+find . -name '*.java' | sed -e 's/\.\///' -e 's/\.java$//' | xargs -L 1 -P $PROCESSORS java -d64 -Xss1g -Xmx4g  # sh runToError.sh
+popd
 echo running jjs
-(ls "$@" | grep -v intermediate | grep -v "\.new") | sed "s/\.x3d$/.sail.js/" | sed 's/\/c\/x3d-code\/www.web3d.org/www_web3d_org/' | xargs -L 1 -P $PROCESSORS jjs -J-Xss1g -J-Xmx4g -cp "${NASHORN_CLASSPATH}"
+pushd ../nashorn
+find . -name '*.sail.js' | grep -v coderextreme | xargs -L 1 -P $PROCESSORS jjs -J-Xss1g -J-Xmx4g -cp "${NASHORN_CLASSPATH}"
+popd
