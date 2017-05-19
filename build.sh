5253d15b56fb2300419fcb2a51e1e8f21571d865
John Carlson <yottzumm@gmail.com>
2017-05-18 21:32:27 -0400

    added mvn clean

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 export PATH=${PATH}:~/apache-maven-3.5.0/bin
 pip install bs4
 pip install lxml
-mvn install || sh FixNeedClassName.sh && mvn install
+mvn clean install || sh FixNeedClassName.sh && mvn install
