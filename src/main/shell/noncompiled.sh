480f61821084eb6b80ebd1b1f55181196be656d0
John Carlson <yottzumm@gmail.com>
2017-05-19 15:26:23 -0400

    Checked in some files

---
+++ src/main/shell/noncompiled.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# way to get list of non compile files and compile them
+
+. ./classpath
+
+find ../java/net -name '*.java' | grep -v " "| sed 's/\.java$/.class/'| xargs ls 2>&1| grep "No such file or directory" | awk '{ print $4; }'| sed -e "s/^'\.\///" -e "s/\.class':/.java/" -e "s/^'//"
