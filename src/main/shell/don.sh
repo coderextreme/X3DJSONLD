46d9a8bbbbb9139f8de5b06266bbf56c40be6faa
John Carlson <yottzumm@gmail.com>
2017-05-19 00:57:45 -0400

    made some changes

--- src/main/shell/don.sh
+++ src/main/shell/don.sh
@@ -15,8 +15,6 @@ echo translating to ecmascript 5
 (ls "$@" | grep -v intermediate | grep -v "\.new") | xargs -P $PROCESSORS java net.coderextreme.RunSaxon ---overwrite ---silent --../lib/stylesheets/X3dToES5.xslt -sail.js ---../nashorn/net/x3djsonld/
 echo translating to java
 (ls "$@" | grep -v intermediate | grep -v "\.new") | xargs -P $PROCESSORS java net.coderextreme.RunSaxon ---overwrite ---silent --../lib/stylesheets/X3dToJava.xslt -java ---../java/net/x3djsonld/
-# echo replacing NeedClassName
-# (ls "$@" | grep -v intermediate | grep -v "\.new") | xargs -P $PROCESSORS sh FixNeedClassName.sh
 echo compiling
 pushd ../java
 find ./net/x3djsonld/data -name '*.java' | xargs -L 1 -P $PROCESSORS javac -J-Xss1g -J-Xmx4g
