30d19812d49d9b1df3b23d6a5f6caeabe6ae4cc1
John Carlson <yottzumm@gmail.com>
2017-05-18 21:27:48 -0400

    Worked on don's install

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
+export PATH=${PATH}:~/apache-maven-3.5.0/bin
 pip install bs4
 pip install lxml
-mvn install
-sh FixNeedClassName.sh
-mvn install
+mvn install || sh FixNeedClassName.sh && mvn install
