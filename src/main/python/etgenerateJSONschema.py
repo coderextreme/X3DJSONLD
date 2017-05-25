ca9334ff9726b86723190322a5367afe85bffcf0
John Carlson <yottzumm@gmail.com>
2017-05-24 21:23:16 -0400

    removed https from schema

--- src/main/python/etgenerateJSONschema.py
+++ src/main/python/etgenerateJSONschema.py
@@ -530,7 +530,7 @@ class ClassPrinter:
         return str
 
 code = '''{
-        "$schema": "https://json-schema.org/draft-04/schema#",
+        "$schema": "http://json-schema.org/draft-04/schema#",
         "title": "JSON Schema X3D V3.3",
         "description": "Experimental JSON Schema for X3D V3.3 ",
         "type": "object",
