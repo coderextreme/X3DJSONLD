73d087ae98426467e792fd1387f0575f35597377
John Carlson <yottzumm@gmail.com>
2017-12-11 03:58:26 -0500

    works a little better (not sure how much)

--- src/main/shell/json2json.sh
+++ src/main/shell/json2json.sh
@@ -12,7 +12,7 @@ python ../python/classes.py
 LOCALTOROOT='s/^\.\.\/x3d-code/\/c\/x3d-code/'
 ROOTTOLOCAL='s/www.web3d.org/www_web3d_org/' 
 
-ls -d "$@" | grep -v intermediate | grep -v "\.new" | xargs -P $PROCESSORS ${NODE} ${NODEDIR}/json2all.js
+ls -d "$@" | grep -v intermediate | grep -v "\.new" | xargs -P $PROCESSORS ${NODE} ${NODEDIR}/dom2json.js
 
 for i in `ls -d "$@" | grep -v intermediate | grep -v "\.new" | sed -e 's/\.json$/.json.new/' -e 's/-/_/g' -e $ROOTTOLOCAL -e 's/^\/c/../'`
 do
