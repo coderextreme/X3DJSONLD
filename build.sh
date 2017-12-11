c6f6d1e8c790cd41882c262ca8041e85395ad40b
John Carlson <yottzumm@gmail.com>
2017-12-10 20:10:39 -0500

    updated maven

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export PATH=${PATH}:~/apache-maven-3.5.0/bin
+export PATH=${PATH}:~/apache-maven-3.5.2/bin
 pip install bs4
 pip install lxml
 mvn clean install || sh FixNeedClassName.sh && mvn install
