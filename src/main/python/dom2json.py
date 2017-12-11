05551512492e4089e33c92d108909347fc3ac22b
John Carlson <yottzumm@gmail.com>
2017-12-11 17:33:18 -0500

    added dom2json.py

--- src/main/python/dom2json.py
+++ src/main/python/dom2json.py
@@ -1,4 +1,4 @@
-import xml.etree.ElementTree
+import xml.etree.ElementTree as ET
 import sys
 import json
 
@@ -11,26 +11,53 @@ def isString(obj):
     else:
         return isinstance(obj, basestring)
 
-def recurse(node):
+def recurse(node, parent):
     object = {}
     fields = {}
+    cf = "-children"
+    oldcf = cf
     children = []
     for attrib,value in node.attrib.items():
         fields['@'+attrib] = value
     if node.text and node.text.strip():
-        children.append(node.text.strip())
+        object["#sourceText"] = node.text.strip().split("\n")
     for element in node:
-        children.append(recurse(element))
-    if node.tail and node.tail.strip():
-        children.append(node.tail.strip())
+        try:
+            if element.attrib["containerField"]:
+                cf = '-'+element.attrib["containerField"]
+                children = []
+        except:
+            pass
+        if element.tag == "head":
+            cf = element.tag
+            children = {}
+        if element.tag == "meta" or element.tag == "unit" or element.tag == "component" or element.tag == "field" or element.tag == "fieldValue" or element.tag == "connect" or element.tag == "ROUTE":
+            cf = element.tag
+            if (oldcf != cf):
+                children = []
+        if element.tag == "Scene":
+            cf = "Scene"
+            children = {}
+        try:
+            children.append(recurse(element, node))
+        except:
+            fields[element.tag] = recurse(element, node)
+        if children:
+            fields[cf] = children
+        oldcf = cf
     if children:
-        fields["-children"] = children
-    object[node.tag] =  fields
+        fields[cf] = children
+    if node.tag == "meta" or node.tag == "unit" or node.tag == "component" or node.tag == "field" or node.tag == "fieldValue" or node.tag == "connect" or node.tag == "ROUTE" or node.tag == "head" or node.tag == "Scene":
+        object = fields
+    else:
+        object[node.tag] =  fields
+    if node.tail and node.tail.strip():
+        object["#sourceText"] = node.tail.strip().split("\n")
     return object
 
-soup = xml.etree.ElementTree.parse("../data/flowers.x3d").getroot()
+soup = ET.parse("../data/flowers.x3d").getroot()
 
 f = open('foo.json', 'w')
-json.dump(recurse(soup), f, indent=2)
+json.dump(recurse(soup, None), f, indent=2)
 f.close()
 
