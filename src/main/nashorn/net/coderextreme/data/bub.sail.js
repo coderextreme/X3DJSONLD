1e59474306155a395c4f6043a0f5afded1c6c340
John Carlson <yottzumm@gmail.com>
2018-02-11 17:49:50 -0500

    added nashorn files

---
+++ src/main/nashorn/net/coderextreme/data/bub.sail.js
@@ -0,0 +1,68 @@
+load('X3Dautoclass.js');
+var ConfigurationProperties = Packages.org.web3d.x3d.jsail.ConfigurationProperties;
+ConfigurationProperties.showDefaultAttributes = false;
+ConfigurationProperties.xsltEngine = ConfigurationProperties.XSLT_ENGINE_NATIVE_JAVA;
+ConfigurationProperties.deleteIntermediateFiles = false;
+ConfigurationProperties.setStripTrailingZeroes(true);
+var ProtoInstance0 = null;
+var ProtoInstance1 = null;
+var ProtoInstance2 = null;
+      var X3D0 =  new X3DObject().setProfile("Immersive").setVersion("3.3")
+      .setHead(new headObject()
+        .addComponent(new componentObject().setName("Shaders").setLevel(1))
+        .addComponent(new componentObject().setName("CubeMapTexturing").setLevel(1))
+        .addMeta(new metaObject().setName("title").setContent("bub.x3d"))
+        .addMeta(new metaObject().setName("creator").setContent("John Carlson"))
+        .addMeta(new metaObject().setName("description").setContent("3 prismatic spheres"))
+        .addMeta(new metaObject().setName("generator").setContent("X3D-Edit, https://savage.nps.edu/X3D-Edit"))
+        .addMeta(new metaObject().setName("identifier").setContent("https://coderextreme.net/X3DJSONLD/bub.x3d")))
+      .setScene(new SceneObject()
+        .addChild(new NavigationInfoObject())
+        .addChild(new BackgroundObject().setBackUrl(Java.to(["../resources/images/all_probes/stpeters_cross/stpeters_back.png","https://coderextreme.net/X3DJSONLD/images/all_probes/stpeters_cross/stpeters_back.png"], Java.type("java.lang.String[]"))).setBottomUrl(Java.to(["../resources/images/all_probes/stpeters_cross/stpeters_bottom.png","https://coderextreme.net/X3DJSONLD/images/all_probes/stpeters_cross/stpeters_bottom.png"], Java.type("java.lang.String[]"))).setFrontUrl(Java.to(["../resources/images/all_probes/stpeters_cross/stpeters_front.png","https://coderextreme.net/X3DJSONLD/images/all_probes/stpeters_cross/stpeters_front.png"], Java.type("java.lang.String[]"))).setLeftUrl(Java.to(["../resources/images/all_probes/stpeters_cross/stpeters_left.png","https://coderextreme.net/X3DJSONLD/images/all_probes/stpeters_cross/stpeters_left.png"], Java.type("java.lang.String[]"))).setRightUrl(Java.to(["../resources/images/all_probes/stpeters_cross/stpeters_right.png","https://coderextreme.net/X3DJSONLD/images/all_probes/stpeters_cross/stpeters_right.png"], Java.type("java.lang.String[]"))).setTopUrl(Java.to(["../resources/images/all_probes/stpeters_cross/stpeters_top.png","https://coderextreme.net/X3DJSONLD/images/all_probes/stpeters_cross/stpeters_top.png"], Java.type("java.lang.String[]"))))
+        .addChild(new ViewpointObject().setPosition(Java.to([0,0,20], Java.type("float[]"))).setDescription("Look at the bubbles flying"))
+        .addChild(new ProtoDeclareObject().setName("Bubble")
+          .setProtoBody(new ProtoBodyObject()
+            .addChild(new TransformObject().setDEF("transform")
+              .addChild(new ShapeObject().setDEF("myShape")
+                .setAppearance(new AppearanceObject()
+                  .setMaterial(new MaterialObject().setDiffuseColor(Java.to([0.7,0.7,0.7], Java.type("float[]"))).setSpecularColor(Java.to([0.5,0.5,0.5], Java.type("float[]"))))
+                  .setTexture(new ComposedCubeMapTextureObject().setDEF("texture")
+                    .setBack(new ImageTextureObject().setUrl(Java.to(["../resources/images/all_probes/stpeters_cross/stpeters_back.png","https://coderextreme.net/X3DJSONLD/images/all_probes/stpeters_cross/stpeters_back.png"], Java.type("java.lang.String[]"))))
+                    .setBottom(new ImageTextureObject().setUrl(Java.to(["../resources/images/all_probes/stpeters_cross/stpeters_bottom.png","https://coderextreme.net/X3DJSONLD/images/all_probes/stpeters_cross/stpeters_bottom.png"], Java.type("java.lang.String[]"))))
+                    .setFront(new ImageTextureObject().setUrl(Java.to(["../resources/images/all_probes/stpeters_cross/stpeters_front.png","https://coderextreme.net/X3DJSONLD/images/all_probes/stpeters_cross/stpeters_front.png"], Java.type("java.lang.String[]"))))
+                    .setLeft(new ImageTextureObject().setUrl(Java.to(["../resources/images/all_probes/stpeters_cross/stpeters_left.png","https://coderextreme.net/X3DJSONLD/images/all_probes/stpeters_cross/stpeters_left.png"], Java.type("java.lang.String[]"))))
+                    .setRight(new ImageTextureObject().setUrl(Java.to(["../resources/images/all_probes/stpeters_cross/stpeters_right.png","https://coderextreme.net/X3DJSONLD/images/all_probes/stpeters_cross/stpeters_right.png"], Java.type("java.lang.String[]"))))
+                    .setTop(new ImageTextureObject().setUrl(Java.to(["../resources/images/all_probes/stpeters_cross/stpeters_top.png","https://coderextreme.net/X3DJSONLD/images/all_probes/stpeters_cross/stpeters_top.png"], Java.type("java.lang.String[]")))))
+                  .addComments(new CommentsBlock('<ComposedShader DEF=\'gl\' language="GLSL"> <field name=\'cube\' type=\'SFInt32\' accessType="inputOutput"value=\'0\'/> <field name=\'chromaticDispertion\' type=\'SFVec3f\' accessType="inputOutput"value=\'0.98 1.0 1.033\'/> <field name=\'bias\' type=\'SFFloat\' accessType="inputOutput"value=\'0.5\'/> <field name=\'scale\' type=\'SFFloat\' accessType="inputOutput"value=\'0.5\'/> <field name=\'power\' type=\'SFFloat\' accessType="inputOutput"value=\'2.0\'/> <ShaderPart url=\'"../shaders/gl.vs" "https://coderextreme.net/X3DJSONLD/src/main/shaders/gl.vs"\' type=\'VERTEX\'></ShaderPart> <ShaderPart url=\'"../shaders/pc_bubbles.fs" "https://coderextreme.net/X3DJSONLD/src/main/shaders/pc_bubbles.fs"\' type=\'FRAGMENT\'></ShaderPart> </ComposedShader> <ComposedShader DEF=\'freewrl\' language="GLSL"> <field name=\'fw_textureCoodGenType\' type=\'SFInt32\' accessType="inputOutput"value=\'0\'/> <field name=\'chromaticDispertion\' type=\'SFVec3f\' accessType="inputOutput"value=\'0.98 1.0 1.033\'/> <field name=\'bias\' type=\'SFFloat\' accessType="inputOutput"value=\'0.5\'/> <field name=\'scale\' type=\'SFFloat\' accessType="inputOutput"value=\'0.5\'/> <field name=\'power\' type=\'SFFloat\' accessType="inputOutput"value=\'2.0\'/> <ShaderPart url=\'"../shaders/freewrl.vs" "https://coderextreme.net/X3DJSONLD/src/main/shaders/freewrl.vs"\' type=\'VERTEX\'></ShaderPart> <ShaderPart url=\'"../shaders/pc_bubbles.fs" "https://coderextreme.net/X3DJSONLD/src/main/shaders/pc_bubbles.fs"\' type=\'FRAGMENT\'></ShaderPart> </ComposedShader>'))
+                  .addComments(new CommentsBlock('<ComposedShader DEF=\'instant\' language="GLSL"> <field name=\'cube\' type=\'SFInt32\' accessType="inputOutput"value=\'0\'/> <field name=\'chromaticDispertion\' type=\'SFVec3f\' accessType="inputOutput"value=\'0.98 1.0 1.033\'/> <field name=\'bias\' type=\'SFFloat\' accessType="inputOutput"value=\'0.5\'/> <field name=\'scale\' type=\'SFFloat\' accessType="inputOutput"value=\'0.5\'/> <field name=\'power\' type=\'SFFloat\' accessType="inputOutput"value=\'2.0\'/> <ShaderPart url=\'"../shaders/instant.vs" "https://coderextreme.net/X3DJSONLD/src/main/shaders/instant.vs"\' type=\'VERTEX\'></ShaderPart> <ShaderPart url=\'"../shaders/pc_bubbles.fs" "https://coderextreme.net/X3DJSONLD/src/main/shaders/pc_bubbles.fs"\' type=\'FRAGMENT\'></ShaderPart> </ComposedShader>'))
+                  .addShaders(new ComposedShaderObject().setDEF("x3dom").setLanguage("GLSL")
+                    .addField(new fieldObject().setType(fieldObject.TYPE_SFINT32).setName("cube").setAccessType(fieldObject.ACCESSTYPE_INPUTOUTPUT).setValue("0"))
+                    .addField(new fieldObject().setType(fieldObject.TYPE_SFVEC3F).setName("chromaticDispertion").setAccessType(fieldObject.ACCESSTYPE_INPUTOUTPUT).setValue("0.98 1 1.033"))
+                    .addField(new fieldObject().setType(fieldObject.TYPE_SFFLOAT).setName("bias").setAccessType(fieldObject.ACCESSTYPE_INPUTOUTPUT).setValue("0.5"))
+                    .addField(new fieldObject().setType(fieldObject.TYPE_SFFLOAT).setName("scale").setAccessType(fieldObject.ACCESSTYPE_INPUTOUTPUT).setValue("0.5"))
+                    .addField(new fieldObject().setType(fieldObject.TYPE_SFFLOAT).setName("power").setAccessType(fieldObject.ACCESSTYPE_INPUTOUTPUT).setValue("2"))
+                    .addParts(new ShaderPartObject().setType("VERTEX").setUrl(Java.to(["../shaders/x3dom.vs","https://coderextreme.net/X3DJSONLD/src/main/shaders/x3dom.vs"], Java.type("java.lang.String[]"))))
+                    .addParts(new ShaderPartObject().setType("FRAGMENT").setUrl(Java.to(["../shaders/pc_bubbles.fs","https://coderextreme.net/X3DJSONLD/src/main/shaders/pc_bubbles.fs"], Java.type("java.lang.String[]")))))
+                  .addShaders(new ComposedShaderObject().setDEF("x_ite").setLanguage("GLSL")
+                    .addField(new fieldObject().setType(fieldObject.TYPE_SFNODE).setName("cube").setAccessType(fieldObject.ACCESSTYPE_INPUTOUTPUT)
+                      .addChild(new ComposedCubeMapTextureObject().setUSE("texture")))
+                    .addField(new fieldObject().setType(fieldObject.TYPE_SFVEC3F).setName("chromaticDispertion").setAccessType(fieldObject.ACCESSTYPE_INPUTOUTPUT).setValue("0.98 1 1.033"))
+                    .addField(new fieldObject().setType(fieldObject.TYPE_SFFLOAT).setName("bias").setAccessType(fieldObject.ACCESSTYPE_INPUTOUTPUT).setValue("0.5"))
+                    .addField(new fieldObject().setType(fieldObject.TYPE_SFFLOAT).setName("scale").setAccessType(fieldObject.ACCESSTYPE_INPUTOUTPUT).setValue("0.5"))
+                    .addField(new fieldObject().setType(fieldObject.TYPE_SFFLOAT).setName("power").setAccessType(fieldObject.ACCESSTYPE_INPUTOUTPUT).setValue("2"))
+                    .addParts(new ShaderPartObject().setType("VERTEX").setUrl(Java.to(["../shaders/x_ite.vs","https://coderextreme.net/X3DJSONLD/src/main/src/main/shaders/x_ite.vs"], Java.type("java.lang.String[]"))))
+                    .addParts(new ShaderPartObject().setType("FRAGMENT").setUrl(Java.to(["../shaders/pc_bubbles.fs","https://coderextreme.net/X3DJSONLD/src/main/shaders/pc.fs"], Java.type("java.lang.String[]"))))))
+                .setGeometry(new SphereObject())))
+            .addChild(new ScriptObject().setDEF("Bounce")
+              .addField(new fieldObject().setType(fieldObject.TYPE_SFVEC3F).setName("translation").setAccessType(fieldObject.ACCESSTYPE_INPUTOUTPUT).setValue("0 0 0"))
+              .addField(new fieldObject().setType(fieldObject.TYPE_SFVEC3F).setName("velocity").setAccessType(fieldObject.ACCESSTYPE_INPUTOUTPUT).setValue("0 0 0"))
+              .addField(new fieldObject().setType(fieldObject.TYPE_SFTIME).setName("set_fraction").setAccessType(fieldObject.ACCESSTYPE_INPUTONLY))
+              .setSourceCode("ecmascript: function initialize() { translation = new SFVec3f(0, 0, 0); velocity = new SFVec3f( Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5); } function set_fraction() { translation = new SFVec3f( translation.x + velocity.x, translation.y + velocity.y, translation.z + velocity.z); if (Math.abs(translation.x) > 10) { initialize(); } else if (Math.abs(translation.y) > 10) { initialize(); } else if (Math.abs(translation.z) > 10) { initialize(); } else { velocity.x += Math.random() * 0.2 - 0.1; velocity.y += Math.random() * 0.2 - 0.1; velocity.z += Math.random() * 0.2 - 0.1; } }\n"+
+""))
+            .addChild(new TimeSensorObject().setDEF("TourTime").setCycleInterval(0.15).setLoop(true))
+            .addChild(new ROUTEObject().setFromNode("TourTime").setFromField("cycleTime").setToNode("Bounce").setToField("set_fraction"))
+            .addChild(new ROUTEObject().setFromNode("Bounce").setFromField("translation_changed").setToNode("transform").setToField("set_translation"))))
+        .addChild(ProtoInstance0 = new ProtoInstanceObject().setName("Bubble"))
+        .addChild(ProtoInstance1 = new ProtoInstanceObject().setName("Bubble"))
+        .addChild(ProtoInstance2 = new ProtoInstanceObject().setName("Bubble")))      ;
+    X3D0.toFileX3D("../data/bub.new.x3d");
