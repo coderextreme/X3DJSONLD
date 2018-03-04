5f074a63db609d067259e2de2d81184b37d9f2d7
John Carlson <yottzumm@gmail.com>
2018-03-04 08:38:08 -0500

    added x_ite stuff to html folder

---
+++ src/main/html/x_ite/Browser/Shaders/Gouraud.fs
@@ -0,0 +1,117 @@
+// -*- Mode: C++; coding: utf-8; tab-width: 3; indent-tabs-mode: tab; c-basic-offset: 3 -*-
+
+precision mediump float;
+precision mediump int;
+
+uniform int x3d_GeometryType;
+
+uniform int  x3d_NumClipPlanes;
+uniform vec4 x3d_ClipPlane [x3d_MaxClipPlanes];
+
+uniform float x3d_LinewidthScaleFactor;
+uniform bool  x3d_Lighting;      // true if a X3DMaterialNode is attached, otherwise false
+uniform bool  x3d_ColorMaterial; // true if a X3DColorNode is attached, otherwise false
+
+uniform int         x3d_NumTextures;
+uniform int         x3d_TextureType [x3d_MaxTextures]; // x3d_None, x3d_TextureType2D or x3d_TextureTypeCubeMapTexture
+uniform sampler2D   x3d_Texture2D [x3d_MaxTextures];
+uniform samplerCube x3d_CubeMapTexture [x3d_MaxTextures];
+
+uniform x3d_FogParameters x3d_Fog;
+
+varying vec4 frontColor; // color
+varying vec4 backColor;  // color
+varying vec4 t;          // texCoord
+varying vec3 v;          // point on geometry
+
+void
+clip ()
+{
+	for (int i = 0; i < x3d_MaxClipPlanes; ++ i)
+	{
+		if (i == x3d_NumClipPlanes)
+			break;
+
+		if (dot (v, x3d_ClipPlane [i] .xyz) - x3d_ClipPlane [i] .w < 0.0)
+			discard;
+	}
+}
+
+vec4
+getTextureColor ()
+{
+	if (x3d_TextureType [0] == x3d_TextureType2D)
+	{
+		if (x3d_GeometryType == x3d_Geometry3D || gl_FrontFacing)
+			return texture2D (x3d_Texture2D [0], vec2 (t));
+		
+		// If dimension is x3d_Geometry2D the texCoords must be flipped.
+		return texture2D (x3d_Texture2D [0], vec2 (1.0 - t .s, t .t));
+	}
+
+ 	if (x3d_TextureType [0] == x3d_TextureTypeCubeMapTexture)
+	{
+		if (x3d_GeometryType == x3d_Geometry3D || gl_FrontFacing)
+			return textureCube (x3d_CubeMapTexture [0], vec3 (t));
+		
+		// If dimension is x3d_Geometry2D the texCoords must be flipped.
+		return textureCube (x3d_CubeMapTexture [0], vec3 (1.0 - t .s, t .t, t .z));
+	}
+ 
+	return vec4 (1.0, 1.0, 1.0, 1.0);
+}
+
+float
+getFogInterpolant ()
+{
+	// Returns 0.0 for fog color and 1.0 for material color.
+
+	if (x3d_Fog .type == x3d_None)
+		return 1.0;
+
+	if (x3d_Fog .visibilityRange <= 0.0)
+		return 0.0;
+
+	float dV = length (v);
+
+	if (dV >= x3d_Fog .visibilityRange)
+		return 0.0;
+
+	if (x3d_Fog .type == x3d_LinearFog)
+		return (x3d_Fog .visibilityRange - dV) / x3d_Fog .visibilityRange;
+
+	if (x3d_Fog .type == x3d_ExponentialFog)
+		return exp (-dV / (x3d_Fog .visibilityRange - dV));
+
+	return 1.0;
+}
+
+vec3
+getFogColor (in vec3 color)
+{
+	return mix (x3d_Fog .color, color, getFogInterpolant ());
+}
+
+void
+main ()
+{
+ 	clip ();
+
+	vec4 finalColor = gl_FrontFacing ? frontColor : backColor;
+
+	if (x3d_TextureType [0] != x3d_None)
+	{
+		if (x3d_Lighting)
+			finalColor *= getTextureColor ();
+		else
+		{
+			if (x3d_ColorMaterial)
+				finalColor *= getTextureColor ();
+			else
+				finalColor = getTextureColor ();
+		}
+	}
+
+	gl_FragColor .rgb = getFogColor (finalColor .rgb);
+	gl_FragColor .a   = finalColor .a;
+}
