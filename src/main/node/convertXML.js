0d640f7cc4c3670c37c4ebdd4d5e54ce46503e78
John Carlson <yottzumm@gmail.com>
2018-03-06 01:47:03 -0500

    Added stuff to convert XML to JSON

---
+++ src/main/node/convertXML.js
@@ -0,0 +1,64 @@
+"use strict";
+
+var fs = require('fs');
+var mkdirp = require('node-mkdirp');
+var mapToMethod = require('./mapToMethod.js');
+var mapToMethod2 = require('./mapToMethod2.js');
+var fieldTypes = require('./fieldTypes.js');
+var DOMParser = require('xmldom').DOMParser;
+
+for (var par in mapToMethod2) {
+	for (var child in mapToMethod2[par]) {
+		mapToMethod[par][child] = mapToMethod2[par][child];
+	}
+}
+
+function convertXML(options) {
+
+	var files = process.argv;
+	for (var f in files) {
+		var file = files[f];
+		if (file.match(/node_modules|package.json|JSONSchema/)) {
+			continue;
+		}
+		var basefile = file.substr(0, file.lastIndexOf("."));
+		var file = basefile+".x3d";
+		var str = fs.readFileSync(file).toString();
+		if (typeof str === 'undefined') {
+			throw("Read nothing, or possbile error");
+		}
+		var doc = null;
+		try {  
+			var domParser = new DOMParser();
+			doc = domParser.parseFromString (str, 'application/xml');
+
+		} catch (e) {
+			console.error("================================================================================");
+			console.error("File:", file);
+			console.error("Error:", e);
+			continue;
+		}
+		var element = doc.documentElement;
+		basefile = basefile.replace(/^C:\//, "")
+		basefile = basefile.replace(/^\.\.\//, "")
+		basefile = basefile.replace(/-|\.| /g, "_")
+		basefile = basefile.replace(/^(.*[\\\/])([0-9].*|default|switch|for)$/, "$1_$2")
+
+		for (var ser in options) {
+			var serializer = require(options[ser].serializer);
+			var co = options[ser].codeOutput+basefile;
+			str = new serializer().serializeToString(null, element, co, mapToMethod, fieldTypes)
+			if (typeof str !== 'undefined') {
+				var outfile = options[ser].folder+basefile+options[ser].extension
+				mkdirp(outfile.substr(0, outfile.lastIndexOf("/")));
+				fs.writeFileSync(outfile, str);
+			} else {
+				throw("Wrote nothing, serializer returned nothing");
+			}
+		}
+	}
+}
+
+if (typeof module === 'object')  {
+	module.exports = convertXML
+}
