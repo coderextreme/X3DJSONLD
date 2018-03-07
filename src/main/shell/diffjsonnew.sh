a2900c4dff78c5f2b8cd2a55640b1935e0fd4922
John Carlson <yottzumm@gmail.com>
2018-03-06 19:46:31 -0500

    added conversion

--- src/main/shell/diffjsonnew.sh
+++ src/main/shell/diffjsonnew.sh
@@ -2,6 +2,7 @@
 
 . ./classpath
 
+find ../data/*.x3d|grep -v new | xargs ${NODE} ../node/xml2all.js
 for i in ../data/*.json.new
 do
 	${NODE} ../node/jsondiff.js `dirname $i`/`basename $i .json.new`.json $i
