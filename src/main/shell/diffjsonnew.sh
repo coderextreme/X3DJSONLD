34a7a13c44cf24ed48b24184602c256aa46ce288
John Carlson <yottzumm@gmail.com>
2018-03-19 20:47:11 -0400

    Added an option

--- src/main/shell/diffjsonnew.sh
+++ src/main/shell/diffjsonnew.sh
@@ -6,7 +6,8 @@ export PROCESSORS=${PROCESSORS-8}
 STYLESHEETDIR=../lib/stylesheets
 
 # find  ../data/*.x3d | grep -v new | xargs -P $PROCESSORS java net.coderextreme.RunSaxon --- ---overwrite --${STYLESHEETDIR}/X3dToJSON.xslt -json | xargs -P $PROCESSORS ${NODE} ${NODEDIR}/json2all.js
-find  ../data/*.x3d | grep -v new | xargs -P $PROCESSORS java net.coderextreme.RunSaxon --- ---overwrite --${STYLESHEETDIR}/X3dToJSON.xslt -json | xargs ${NODE} ${NODEDIR}/xml2all.js | xargs -P $PROCESSORS ${NODE} ${NODEDIR}/json2all.js
+# find  ../data/*.x3d | grep -v new | xargs -P $PROCESSORS java net.coderextreme.RunSaxon --- ---overwrite --${STYLESHEETDIR}/X3dToJSON.xslt -json | xargs ${NODE} ${NODEDIR}/xml2all.js | xargs -P $PROCESSORS ${NODE} ${NODEDIR}/json2all.js
+find  ../data/*.x3d | grep -v new | xargs -P $PROCESSORS java net.coderextreme.RunSaxon --- ---overwrite --${STYLESHEETDIR}/X3dToJSON.xslt -json | xargs -P $PROCESSORS ${NODE} ${NODEDIR}/json2all.js
 for i in ../data/*.json.new
 do
 	${NODE} ../node/jsondiff.js `dirname $i`/`basename $i .json.new`.json $i
