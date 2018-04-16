8a6d829a0beae5da39f02f1a80349fc14b05297c
John Carlson <yottzumm@gmail.com>
2018-04-16 15:26:29 -0400

    Put Flattener inside of ProtoTypeExpander

--- src/main/node/PPP.js
+++ src/main/node/PPP.js
@@ -10,9 +10,6 @@ var Browser = X3DJSONLD.Browser;
 var PROTOS = require('./PrototypeExpander')
 PROTOS.setX3DJSONLD(X3DJSONLD);
 
-var FL = require('./Flattener')
-var flattener = FL.flattener;
-
 var Script = require('./Script');
 var LOG = Script.LOG;
 var processScripts = Script.processScripts;
@@ -29,7 +26,6 @@ var x3dom = require('./fields.js');
 function ProcessJSON(json, file) {
 	json = PROTOS.externalPrototypeExpander(file, json);
 	json = PROTOS.prototypeExpander(file, json, "");
-	json = flattener(json);
 	console.log(JSON.stringify(json));
 	var outfile = file.replace(/data/, "ppp");
 	try {
