3b5e8e45889e79dec60ceb3d6d7a197139d0edb9
John Carlson <yottzumm@gmail.com>
2018-04-16 01:21:16 -0400

    New version of JSON schema generator for multiple versions (stdin, stdout) New shell script driver for generating schemas from Object Model

---
+++ src/main/shell/generateSchema.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo 3.0
+python ../python/etgenerateJSONschema.py < /c/x3d-code/www.web3d.org/specifications/X3dUnifiedObjectModel-3.0.xml | jsonlint > ../schema/x3d-3.0-JSONSchema.json
+echo 3.1
+python ../python/etgenerateJSONschema.py < /c/x3d-code/www.web3d.org/specifications/X3dUnifiedObjectModel-3.1.xml | jsonlint > ../schema/x3d-3.1-JSONSchema.json
+echo 3.2
+python ../python/etgenerateJSONschema.py < /c/x3d-code/www.web3d.org/specifications/X3dUnifiedObjectModel-3.2.xml | jsonlint > ../schema/x3d-3.2-JSONSchema.json
+echo 3.3
+python ../python/etgenerateJSONschema.py < /c/x3d-code/www.web3d.org/specifications/X3dUnifiedObjectModel-3.3.xml | jsonlint > ../schema/x3d-3.3-JSONSchema.json
+echo 4.0
+python ../python/etgenerateJSONschema.py < /c/x3d-code/www.web3d.org/specifications/X3dUnifiedObjectModel-4.0.xml | jsonlint > ../schema/x3d-4.0-JSONSchema.json
