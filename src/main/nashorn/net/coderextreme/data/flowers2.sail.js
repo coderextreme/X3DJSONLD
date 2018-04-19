96f8f0971eb1f8f2fde93cb62544c31683f45868
John Carlson <yottzumm@gmail.com>
2018-04-19 09:59:00 -0400

    updated

--- src/main/nashorn/net/coderextreme/data/flowers2.sail.js
+++ src/main/nashorn/net/coderextreme/data/flowers2.sail.js
@@ -67,8 +67,8 @@ var ProtoInstance1 = null;
 "			function initialize() {\n"+
 "			     generateCoordinates();\n"+
 "			     var localci = [];\n"+
-"			     for ( i = 0; i < resolution-1; i++) {\n"+
-"				for ( j = 0; j < resolution-1; j++) {\n"+
+"			     for (var i = 0; i < resolution-1; i++) {\n"+
+"				for (var j = 0; j < resolution-1; j++) {\n"+
 "				     localci.push(i*resolution+j);\n"+
 "				     localci.push(i*resolution+j+1);\n"+
 "				     localci.push((i+1)*resolution+j+1);\n"+
