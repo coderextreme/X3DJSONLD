8da636ff5ce8f47c94935394ea6a789060d17067
John Carlson <yottzumm@gmail.com>
2018-04-25 15:15:03 -0400

    added scripts for calling CommandLine class.

---
+++ src/main/shell/commandLine.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Run the Test Suite
+
+# accepts files with .x3d extension
+export PROCESSORS=${PROCESSORS-8}
+
+. ./classpath
+
+echo export CLASSPATH='"'${CLASSPATH}'"'
+for i in `ls "$@" | grep -v new`
+do
+	echo '$' java org.web3d.x3d.jsail.CommandLine $i -validate
+	java -cp "${CLASSPATH}" org.web3d.x3d.jsail.CommandLine $i -validate
+done
---
+++ src/main/shell/commandlocal.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# run local files through testing process
+
+export PROCESSORS=1
+ls ../data/*.x3d | xargs -P $PROCESSORS ./commandLine.sh
+
