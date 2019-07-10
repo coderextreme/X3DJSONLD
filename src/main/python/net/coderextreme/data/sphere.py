b14a9007d3fd91d26af3ce5197bbe8b537769e39
John Carlson <yottzumm@gmail.com>
2019-07-10 18:20:13 -0500

    Modified python files

--- src/main/python/net/coderextreme/data/sphere.py
+++ src/main/python/net/coderextreme/data/sphere.py
@@ -1,47 +1,47 @@
 # -*- coding: UTF-8 -*-
 from x3dpsail import *
-X3D0 = X3DObject()
+X3D0 = X3D()
 X3D0.setProfile("Interchange")
 X3D0.setVersion("3.3")
-head1 = headObject()
-meta2 = metaObject()
+head1 = head()
+meta2 = meta()
 meta2.setName("title")
 meta2.setContent("sphere.x3d")
 
 head1.addMeta(meta2)
-meta3 = metaObject()
+meta3 = meta()
 meta3.setName("creator")
 meta3.setContent("John Carlson")
 
 head1.addMeta(meta3)
-meta4 = metaObject()
+meta4 = meta()
 meta4.setName("generator")
 meta4.setContent("manual")
 
 head1.addMeta(meta4)
-meta5 = metaObject()
+meta5 = meta()
 meta5.setName("identifier")
 meta5.setContent("https://coderextreme.net/X3DJSONLD/sphere.x3d")
 
 head1.addMeta(meta5)
-meta6 = metaObject()
+meta6 = meta()
 meta6.setName("description")
 meta6.setContent("a sphere")
 
 head1.addMeta(meta6)
 
 X3D0.setHead(head1)
-Scene7 = SceneObject()
-Group8 = GroupObject()
-Shape9 = ShapeObject()
-Appearance10 = AppearanceObject()
-Material11 = MaterialObject()
+Scene7 = Scene()
+Group8 = Group()
+Shape9 = Shape()
+Appearance10 = Appearance()
+Material11 = Material()
 Material11.setDiffuseColor([1,1,1])
 
 Appearance10.setMaterial(Material11)
 
 Shape9.setAppearance(Appearance10)
-Sphere12 = SphereObject()
+Sphere12 = Sphere()
 
 Shape9.setGeometry(Sphere12)
 
