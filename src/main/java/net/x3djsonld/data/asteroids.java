b612ba2ce3188dfea0c8b2d785ac20626d725f3b
John Carlson <yottzumm@gmail.com>
2019-07-10 18:31:32 -0500

    added 2 java files

---
+++ src/main/java/net/x3djsonld/data/asteroids.java
@@ -0,0 +1,154 @@
+package net.x3djsonld.data;
+
+import java.util.*;
+import org.web3d.x3d.jsail.Core.*;
+import org.web3d.x3d.jsail.fields.*;
+import org.web3d.x3d.jsail.Geometry3D.*;
+import org.web3d.x3d.jsail.Grouping.*;
+import org.web3d.x3d.jsail.Shape.*;
+
+// Javadoc annotations follow, see below for source.
+/**
+ * <p> asteroids. </p>
+ <p> Related links: asteroids.java source, <a href="http://www.web3d.org/x3d/content/examples/X3dResources.html" target="_blank">X3D Resources</a>, <a href="http://www.web3d.org/x3d/content/examples/X3dSceneAuthoringHints.html" target="_blank">X3D Scene Authoring Hints</a>, and <a href="http://www.web3d.org/x3d/content/X3dTooltips.html" target="_blank">X3D Tooltips</a>. </p>
+	<table style="color:black; border:0px solid; border-spacing:10px 0px;" summary="Scene Metadata">
+		<tr style="background-color:silver; border-color:silver;">
+			<td style="text-align:center; padding:10px 0px;"><i>meta tags</i></td>
+			<td style="text-align:left;   padding:10px 0px;">net.x3djsonld.data.asteroids&nbsp; Document Metadata </td>
+		</tr>
+
+		<tr>
+			<td style="text-align:right; vertical-align: text-top;"> <i> title </i> </td>
+			<td> <a href="https://coderextreme.net/X3DJSONLD/asteroids.x3d">asteroids.x3d</a> </td>
+		</tr>
+		<tr>
+			<td style="text-align:right; vertical-align: text-top;"> <i> creator </i> </td>
+			<td> John Carlson </td>
+		</tr>
+		<tr>
+			<td style="text-align:right; vertical-align: text-top;"> <i> generator </i> </td>
+			<td> manual </td>
+		</tr>
+		<tr>
+			<td style="text-align:right; vertical-align: text-top;"> <i> identifier </i> </td>
+			<td> <a href="https://coderextreme.net/X3DJSONLD/asteroids.x3d" target="_blank">https://coderextreme.net/X3DJSONLD/asteroids.x3d</a> </td>
+		</tr>
+		<tr>
+			<td style="text-align:right; vertical-align: text-top;"> <i> description </i> </td>
+			<td> asteroids </td>
+		</tr>
+		<tr style="background-color:silver; border-color:silver;">
+			<td style="text-align:center;" colspan="2">  &nbsp; </td>
+		</tr>
+	</table>
+
+	<p>
+		This program uses the
+		<a href="http://www.web3d.org/specifications/java/X3DJSAIL.html" target="_blank">X3D Java Scene Access Interface Library (X3DJSAIL)</a>.
+		It has been produced using the 
+		<a href="http://www.web3d.org/x3d/stylesheets/X3dToJava.xslt" target="_blank">X3dToJava.xslt</a>
+		stylesheet to create Java source code from an <code>.x3d</code> model.
+	</p>
+
+	* @author John Carlson
+ */
+
+public class asteroids
+{
+	/** Default constructor to create this object. */
+	public asteroids ()
+	{
+	  initialize();
+	}
+
+	/** Create and initialize the X3D model for this object. */
+	public final void initialize()
+	{
+  x3dModel = new X3DObject().setProfile(X3DObject.PROFILE_IMMERSIVE).setVersion(X3DObject.VERSION_3_3)
+  .setHead(new headObject()
+    .addMeta(new metaObject().setName(metaObject.NAME_TITLE        ).setContent("asteroids.x3d"))
+    .addMeta(new metaObject().setName(metaObject.NAME_CREATOR      ).setContent("John Carlson"))
+    .addMeta(new metaObject().setName(metaObject.NAME_GENERATOR    ).setContent("manual"))
+    .addMeta(new metaObject().setName(metaObject.NAME_IDENTIFIER   ).setContent("https://coderextreme.net/X3DJSONLD/asteroids.x3d"))
+    .addMeta(new metaObject().setName(metaObject.NAME_DESCRIPTION  ).setContent("asteroids")))
+  .setScene(new SceneObject()
+    .addChild(new ProtoDeclareObject().setName("anyShape")
+      .setProtoInterface(new ProtoInterfaceObject()
+        .addField(new fieldObject().setAccessType("inputOutput").setName("myShape").setType("MFNode")
+          .addChild(new ShapeObject()
+            .setGeometry(new SphereObject()))))
+      .setProtoBody(new ProtoBodyObject()
+        .addChild(new TransformObject()
+          .setIS(new ISObject()
+            .addConnect(new connectObject().setNodeField("children").setProtoField("myShape"))))))
+    .addChild(new ProtoInstanceObject().setName("anyShape")));
+    }
+	// end of initialize() method
+
+	/** The initialized model object, created within initialize() method. */
+	private X3DObject x3dModel;
+
+	/** Provide a 
+	 * <a href="https://dzone.com/articles/java-copy-shallow-vs-deep-in-which-you-will-swim" target="_blank">shallow copy</a>
+	 * of the X3D model.
+	 * @see <a href="http://www.web3d.org/specifications/java/javadoc/org/web3d/x3d/jsail/Core/X3DObject.html">X3DObject</a>
+	 * @return asteroids model
+	 */
+	public X3DObject getX3dModel()
+	{	  
+		return x3dModel;
+	}
+	   
+    /** Default main() method provided for test purposes, uses CommandLine to set global ConfigurationProperties for this object.
+     * @param args array of input parameters, provided as arguments
+	 * @see <a href="http://www.web3d.org/specifications/java/javadoc/org/web3d/x3d/jsail/Core/X3DObject.html#handleArguments-java.lang.String:A-">X3DObject.handleArguments(args)</a>
+	 * @see <a href="http://www.web3d.org/specifications/java/javadoc/org/web3d/x3d/jsail/Core/X3DObject.html#validationReport--">X3DObject.validationReport()</a>
+     * @see <a href="http://www.web3d.org/specifications/java/javadoc/org/web3d/x3d/jsail/CommandLine.html">CommandLine</a>
+     * @see <a href="http://www.web3d.org/specifications/java/javadoc/org/web3d/x3d/jsail/CommandLine.html#USAGE">CommandLine.USAGE</a>
+     * @see <a href="http://www.web3d.org/specifications/java/javadoc/org/web3d/x3d/jsail/ConfigurationProperties.html">ConfigurationProperties</a>
+     */
+    public static void main(String args[])
+    {
+        X3DObject thisExampleX3dObject = new asteroids().getX3dModel();
+
+		boolean hasArguments = (args != null) && (args.length > 0);
+		boolean validate = true; // default
+		boolean argumentsLoadNewModel = false;
+		String  fileName = new String();
+
+		if (args != null)
+		{
+			for (String arg : args)
+			{
+				if (arg.toLowerCase().startsWith("-v") || arg.toLowerCase().contains("validate"))
+				{
+					validate = true; // making sure
+				}
+				if (arg.toLowerCase().endsWith(X3DObject.FILE_EXTENSION_X3D) ||
+					arg.toLowerCase().endsWith(X3DObject.FILE_EXTENSION_CLASSICVRML) ||
+					arg.toLowerCase().endsWith(X3DObject.FILE_EXTENSION_X3DB) ||
+					arg.toLowerCase().endsWith(X3DObject.FILE_EXTENSION_VRML97) ||
+					arg.toLowerCase().endsWith(X3DObject.FILE_EXTENSION_EXI) ||
+					arg.toLowerCase().endsWith(X3DObject.FILE_EXTENSION_GZIP) ||
+					arg.toLowerCase().endsWith(X3DObject.FILE_EXTENSION_ZIP) ||
+					arg.toLowerCase().endsWith(X3DObject.FILE_EXTENSION_HTML) ||
+					arg.toLowerCase().endsWith(X3DObject.FILE_EXTENSION_XHTML))
+				{
+					argumentsLoadNewModel = true;
+					fileName = arg;
+				}
+			}
+		}
+		if      (argumentsLoadNewModel)
+			System.out.println("WARNING: \"asteroids\" model invocation is attempting to load file \"" + fileName + "\" instead of simply validating itself... file loading ignored.");
+		else if (hasArguments) // if no arguments provided, this method produces usage warning
+			thisExampleX3dObject.handleArguments(args);
+
+		if (validate)
+		{
+			System.out.print("Java program \"asteroids\" self-validation test results: ");
+			String validationResults = thisExampleX3dObject.validationReport();
+			System.out.println(validationResults);
+		}
+    }
+}
