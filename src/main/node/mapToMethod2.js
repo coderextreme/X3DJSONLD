bde0dfa2ece66ea41fb14fb7aa47b10114871491
John Carlson <yottzumm@gmail.com>
2019-08-12 12:25:41 -0500

    changed mapToMethod2.js to remove setIS

--- src/main/node/mapToMethod2.js
+++ src/main/node/mapToMethod2.js
@@ -12,65 +12,13 @@ var mapToMethod2 = {
 		"field" : "addField",
 	},
 	"Script" : {
-		"field" : "addField",
-		"IS" : "setIS"
+		"field" : "addField"
 	},
 	"MetadataSet" : {
 		"ProtoInstance" : "setMetadata"
 	},
-	"ProtoInstance" : {
-		"IS" : "setIS"
-	},
-	"X3DGroupingNode" : {
-		"IS" : "setIS"
-	},
-	"CADAssembly" : {
-		"IS" : "setIS"
-	},
-	"Billboard" : {
-		"IS" : "setIS"
-	},
-	"GeoLocation" : {
-		"IS" : "setIS"
-	},
-	"X3DViewportNode" : {
-		"IS" : "setIS"
-	},
-	"LayoutLayer" : {
-		"IS" : "setIS"
-	},
-	"Scene" : {
-		"IS" : "setIS"
-	},
-	"LOD" : {
-		"IS" : "setIS"
-	},
-	"Disk2D" : {
-		"IS" : "setIS"
-	},
-	"Anchor" : {
-		"IS" : "setIS"
-	},
-	"Group" : {
-		"IS" : "setIS"
-	},
 	"RigidBody" : {
 		"CollidableShape" : "addGeometry"
-	},
-	"Viewport" : {
-		"IS" : "setIS"
-	},
-	"GeoTransform" : {
-		"IS" : "setIS"
-	},
-	"ScreenGroup" : {
-		"IS" : "setIS"
-	},
-	"EspduTransform" : {
-		"IS" : "setIS"
-	},
-	"PickableGroup" : {
-		"IS" : "setIS"
 	},
 	"HAnimHumanoid" : {
 		"IS" : "addSkin",
@@ -84,44 +32,13 @@ var mapToMethod2 = {
 	"VolumePickSensor" : {
 		"IS" : "addPickedGeometry"
 	},
-	"Layer" : {
-		"IS" : "setIS"
-	},
-	"StaticGroup" : {
-		"IS" : "setIS"
-	},
 	"PointPickSensor" : {
 		"IS" : "addPickedGeometry"
-	},
-	"LayoutGroup" : {
-		"IS" : "setIS"
-	},
-	"HAnimSite" : {
-		"IS" : "setIS"
-	},
-	"field" : {
-		"IS" : "setIS"
-	},
-	"Transform" : {
-		"IS" : "setIS"
-	},
-	"ProtoBody" : {
-		"IS" : "setIS"
-	},
-	"Collision" : {
-		"IS" : "setIS"
 	},
 	"PrimitivePickSensor" : {
 		"IS" : "addPickedGeometry"
 	},
-	"CADLayer" : {
-		"IS" : "setIS"
-	},
-	"fieldValue" : {
-		"IS" : "setIS"
-	},
 	"GeoLOD" : {
-		"IS" : "setIS",
 		"IS" : "addRootNode"
 	},
 	"LinePickSensor" : {
@@ -129,12 +46,6 @@ var mapToMethod2 = {
 	},
 	"GeoMetadata" : {
 		"IS" : "addData"
-	},
-	"Switch" : {
-		"IS" : "setIS"
-	},
-	"HAnimSegment" : {
-		"IS" : "setIS"
 	},
 	"GeoLOD" : {
 		"GeoOrigin" : "setGeoOrigin"
