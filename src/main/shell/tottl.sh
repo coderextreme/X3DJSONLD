fcb37fc67b95b2395009ab911c2477bfcb53d364
John Carlson <yottzumm@gmail.com>
2019-10-09 13:35:19 -0500

    added TTL creators

---
+++ src/main/shell/tottl.sh
@@ -0,0 +1,4 @@
+export PROCESSORS=${PROCESSORS-8}
+export OVERWRITE=---overwrite
+STYLESHEETDIR=/x3d-code/www.web3d.org/x3d/stylesheets
+ls -d "$@" | grep -v intermediate | grep -v "\.new" | xargs -P $PROCESSORS java net.coderextreme.RunSaxon --- ${OVERWRITE} --${STYLESHEETDIR}/X3dToTurtle.xslt -ttl
