904ff7bb2655c9b643fec9037f9ffdbf66386197
John Carlson <yottzumm@gmail.com>
2020-04-19 00:12:15 -0500

    Added package-lock and shell files for ttl

---
+++ src/main/shell/future.nonprocessed.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# get list of files which have X3D but don't have JSON
+
+find /c/x3d-code/www.web3d.org/x3d/content/examples -name '*.x3d' | grep -v new | grep -v intermediate | grep -v " "| sed -e 's/\.x3d$/.newf.x3d/' | xargs ls 2>&1 | grep "No such file or directory" | awk '{ print $4; }' | sed -e "s/^'//" -e "s/'://"
+
