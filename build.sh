c5e5ea5f0391a9ef5ab3260a5b2bcbe19d0fcc9f
John Carlson <yottzumm@gmail.com>
2020-04-21 20:18:47 -0500

    fixed Fix NeedClassName pipe

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 export PATH=${PATH}:~/apache-maven-3.5.2/bin
 pip install bs4
 pip install lxml
-mvn clean install || sh FixNeedClassName.sh && mvn install
+mvn clean install && sh FixNeedClassName.sh && mvn install
