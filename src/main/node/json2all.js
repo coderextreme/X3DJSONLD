5dda294c3591f1275c2770f0dcf04ef6f1b976af
John Carlson <yottzumm@gmail.com>
2020-08-26 21:55:50 -0500

    Added ECMAScriptSerializer (unfinished).

--- src/main/node/json2all.js
+++ src/main/node/json2all.js
@@ -9,45 +9,51 @@ var convertJSON = require('./convertJSON.js').convertJSON;
 
 convertJSON([
 	{ 
-	serializer : './JavaSerializer.js',
+	serializer : "./JavaSerializer.js",
 	folder : "../java/net/coderextreme/",
 	extension : ".java",
-	codeOutput : "../",
+	codeOutput : "../"
+	},
+	{ 
+	serializer : "./ECMAScriptSerializer.js",
+	folder : "../node/net/ecma/",
+	extension : ".js",
+	codeOutput : "../"
 	},
 	/*
 	{ 
 	serializer : './DOM2JSONSerializer.js',
 	folder : "../",
-	extension : ".json.new",
+	extension : ".json.new"
 	},
 	*/
 	{ 
 	serializer : './DOMSerializer.js',
 	folder : "../",
-	extension : ".x3d.new",
+	extension : ".x3d.new"
 	},
 	{ 
 	serializer : './VRMLScriptSerializer.js',
 	folder : "../vrml/net/coderextreme/",
 	extension : ".js",
-	codeOutput : "../",
+	codeOutput : "../"
 	},
 	{ 
 	serializer : './NodeSerializer.js',
 	folder : "../node/net/coderextreme/",
 	extension : ".js",
-	codeOutput : "../",
+	codeOutput : "../"
 	},
 	{ 
 	serializer : './JavaScriptSerializer.js',
 	folder : "../nashorn/net/coderextreme/",
 	extension : ".js",
-	codeOutput : "../",
+	codeOutput : "../"
 	},
 	{ 
 	serializer : './PythonSerializer.js',
 	folder : "../python/net/coderextreme/",
 	extension : ".py",
-	codeOutput : "../",
+	codeOutput : "../"
 	}
 	]);
