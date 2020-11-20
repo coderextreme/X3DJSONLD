02d6a290d9f9f4b956e2ae5c86074b2e915c0222
John Carlson <yottzumm@gmail.com>
2020-11-20 09:21:06 -0600

    Checked in new files Installed GraalVM

--- src/main/shell/old/clean.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-. ./classpath
-
-# clean up test files
-
-rm -f `ls *py | grep -v classes.py | grep -v generate` *.js *.x3d.new `ls *.class *.java | grep -v RunSaxon` *.new.x3d *.intermediate.* *.diff *.x3d*.json *three.js *.bak *roundtrip* *Canonical* *.run*.txt [0-9]* *.new.json `ls *.json | grep -v Schema | grep -v package.json`
