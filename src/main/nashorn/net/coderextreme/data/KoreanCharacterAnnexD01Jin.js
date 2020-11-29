1e5b42917ffcbb6bbade456baa6d136904b0a89f
John Carlson <yottzumm@gmail.com>
2020-11-29 12:14:51 -0600

    Updated

---
+++ src/main/nashorn/net/coderextreme/data/KoreanCharacterAnnexD01Jin.js
@@ -0,0 +1,29 @@
+load('X3Dautoclass.js');
+var ConfigurationProperties = Packages.org.web3d.x3d.jsail.ConfigurationProperties;
+ConfigurationProperties.showDefaultAttributes = false;
+ConfigurationProperties.xsltEngine = ConfigurationProperties.XSLT_ENGINE_NATIVE_JAVA;
+ConfigurationProperties.deleteIntermediateFiles = false;
+ConfigurationProperties.setStripTrailingZeroes(true);
+      var X3D0 =  new X3D().setProfile("Immersive").setVersion("4.0")
+      .setHead(new head()
+        .addComponent(new component().setName("HAnim").setLevel(1))
+        .addMeta(new meta().setName("title").setContent("KoreanCharacterAnnexD01Jin.x3d"))
+        .addMeta(new meta().setName("creator").setContent("Jin Hoon Lee and Min Joo Lee"))
+        .addMeta(new meta().setName("translator").setContent("Chul Hee Jung and Myeong Won Lee"))
+        .addMeta(new meta().setName("created").setContent("31 March 2011"))
+        .addMeta(new meta().setName("translated").setContent("1 November 2014"))
+        .addMeta(new meta().setName("modified").setContent("28 May 2018"))
+        .addMeta(new meta().setName("description").setContent("Articulated 3D game character designed with a general graphics tool, then converted into an X3D HAnim model."))
+        .addMeta(new meta().setName("reference").setContent("KoreanCharacter00ReadMe.txt"))
+        .addMeta(new meta().setName("reference").setContent("KoreanCharacterHumanMotion_Infotech2014_140706.pdf"))
+        .addMeta(new meta().setName("reference").setContent("KoreanCharactersIllustrated.pdf"))
+        .addMeta(new meta().setName("identifier").setContent("http://www.web3d.org/x3d/content/examples/Basic/HumanoidAnimation/KoreanCharacterAnnexD01Jin.x3d"))
+        .addMeta(new meta().setName("generator").setContent("3DS MAX, http://www.autodesk.com/products/autodesk-3ds-max/overview"))
+        .addMeta(new meta().setName("generator").setContent("Suwon HAnim Converter"))
+        .addMeta(new meta().setName("generator").setContent("Gnu Image Manipulation Program, http://www.gimp.org"))
+        .addMeta(new meta().setName("generator").setContent("X3D-Edit 3.3, https://savage.nps.edu/X3D-Edit"))
+        .addMeta(new meta().setName("license").setContent("../license.html")))
+      .setScene(new Scene()
+        .addChild(new NavigationInfo().setSpeed(1.5))
+        .addChild(new Viewpoint().setCenterOfRotation(Java.to([0,1,0], Java.type("float[]"))).setDescription("AnnexD01Jin").setPosition(Java.to([0,1,3], Java.type("float[]")))))      ;
+    X3D0.toFileX3D("../data/KoreanCharacterAnnexD01Jin.new.x3d");
