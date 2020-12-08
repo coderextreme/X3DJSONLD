62ce93c1160ef505ce49d3b333256398ef33c85a
John Carlson <yottzumm@gmail.com>
2020-12-07 22:26:12 -0600

    Added skeletonJoe

---
+++ src/main/python/net/coderextreme/data/skeletonJoe.py
@@ -0,0 +1,190 @@
+import x3dpsail as x3d
+X3D0 = x3d.X3D()
+X3D0.setProfile("Immersive")
+X3D0.setVersion("4.0")
+head1 = x3d.head()
+component2 = x3d.component()
+component2.setName("HAnim")
+component2.setLevel(1)
+
+head1.addComponent(component2)
+meta3 = x3d.meta()
+meta3.setName("title")
+meta3.setContent("JohnBoy.x3d")
+
+head1.addMeta(meta3)
+meta4 = x3d.meta()
+meta4.setName("identifier")
+meta4.setContent("http://www.web3d.org/x3d/content/examples/HumanoidAnimation/JohnBoy.x3d")
+
+head1.addMeta(meta4)
+meta5 = x3d.meta()
+meta5.setName("description")
+meta5.setContent("An attempt at a standard LOA-4 skeleton")
+
+head1.addMeta(meta5)
+meta6 = x3d.meta()
+meta6.setName("generator")
+meta6.setContent("h.pl")
+
+head1.addMeta(meta6)
+meta7 = x3d.meta()
+meta7.setName("creator")
+meta7.setContent("John Carlson")
+
+head1.addMeta(meta7)
+meta8 = x3d.meta()
+meta8.setName("created")
+meta8.setContent("12 June 2020")
+
+head1.addMeta(meta8)
+meta9 = x3d.meta()
+meta9.setName("license")
+meta9.setContent("../license.html")
+
+head1.addMeta(meta9)
+
+X3D0.setHead(head1)
+Scene10 = x3d.Scene()
+Viewpoint11 = x3d.Viewpoint()
+Viewpoint11.setDescription("default")
+
+Scene10.addChildren(Viewpoint11)
+Transform12 = x3d.Transform()
+#DEF for markerfor XYZ axes
+Shape13 = x3d.Shape()
+Shape13.setDEF("AxisLinesShape")
+#RGB lines showing XYZ axes
+IndexedLineSet14 = x3d.IndexedLineSet()
+IndexedLineSet14.setColorIndex([0,1,2])
+IndexedLineSet14.setColorPerVertex(False)
+IndexedLineSet14.setCoordIndex([0,1,-1,0,2,-1,0,3,-1])
+Coordinate15 = x3d.Coordinate()
+Coordinate15.setPoint([0,0,0,0.1,0,0,0,0.1,0,0,0,0.1])
+
+IndexedLineSet14.setCoord(Coordinate15)
+Color16 = x3d.Color()
+Color16.setColor([1,0,0,0,0.6,0,0,0,1])
+
+IndexedLineSet14.setColor(Color16)
+
+Shape13.setGeometry(IndexedLineSet14)
+
+Transform12.addChildren(Shape13)
+
+Scene10.addChildren(Transform12)
+Group17 = x3d.Group()
+#DEFS for markers of skeleton joints, segments, and sites
+Transform18 = x3d.Transform()
+Transform19 = x3d.Transform()
+Transform19.setTranslation([0,2,0])
+Shape20 = x3d.Shape()
+Shape20.setDEF("HAnimRootShape")
+Sphere21 = x3d.Sphere()
+Sphere21.setRadius(0.02)
+
+Shape20.setGeometry(Sphere21)
+Appearance22 = x3d.Appearance()
+Material23 = x3d.Material()
+Material23.setDEF("HAnimRootMaterial")
+Material23.setDiffuseColor([0.8,0,0])
+Material23.setTransparency(0.3)
+
+Appearance22.setMaterial(Material23)
+
+Shape20.setAppearance(Appearance22)
+
+Transform19.addChildren(Shape20)
+
+Transform18.addChildren(Transform19)
+Transform24 = x3d.Transform()
+Transform24.setTranslation([0,2.1,0])
+Shape25 = x3d.Shape()
+Shape25.setDEF("HAnimJointShape")
+Sphere26 = x3d.Sphere()
+Sphere26.setRadius(0.02)
+
+Shape25.setGeometry(Sphere26)
+Appearance27 = x3d.Appearance()
+Material28 = x3d.Material()
+Material28.setDEF("HAnimJointMaterial")
+Material28.setDiffuseColor([0,0,0.8])
+Material28.setTransparency(0.3)
+
+Appearance27.setMaterial(Material28)
+
+Shape25.setAppearance(Appearance27)
+
+Transform24.addChildren(Shape25)
+
+Transform18.addChildren(Transform24)
+Transform29 = x3d.Transform()
+Transform29.setTranslation([0,2.05,0])
+Shape30 = x3d.Shape()
+Shape30.setDEF("HAnimSegmentLine")
+LineSet31 = x3d.LineSet()
+LineSet31.setVertexCount([2])
+ColorRGBA32 = x3d.ColorRGBA()
+ColorRGBA32.setDEF("HAnimSegmentLineColorRGBA")
+ColorRGBA32.setColor([1,1,0,1,1,1,0,0.1])
+
+LineSet31.setColor(ColorRGBA32)
+Coordinate33 = x3d.Coordinate()
+Coordinate33.setPoint([-0.05,0,0,0.05,0,0])
+
+LineSet31.setCoord(Coordinate33)
+
+Shape30.setGeometry(LineSet31)
+
+Transform29.addChildren(Shape30)
+
+Transform18.addChildren(Transform29)
+Transform34 = x3d.Transform()
+Transform34.setTranslation([0,2.1,0])
+Shape35 = x3d.Shape()
+Shape35.setDEF("HAnimSiteShape")
+IndexedFaceSet36 = x3d.IndexedFaceSet()
+IndexedFaceSet36.setDEF("DiamondIFS")
+IndexedFaceSet36.setCreaseAngle(0.5)
+IndexedFaceSet36.setSolid(False)
+IndexedFaceSet36.setCoordIndex([0,1,2,-1,0,2,3,-1,0,3,4,-1,0,4,1,-1,5,2,1,-1,5,3,2,-1,5,4,3,-1,5,1,4,-1])
+ColorRGBA37 = x3d.ColorRGBA()
+ColorRGBA37.setDEF("HAnimSiteColorRGBA")
+ColorRGBA37.setColor([1,1,0,1,1,1,0,0.1])
+
+IndexedFaceSet36.setColor(ColorRGBA37)
+Coordinate38 = x3d.Coordinate()
+Coordinate38.setPoint([0,0.01,0,-0.01,0,0,0,0,0.01,0.01,0,0,0,0,-0.01,0,-0.01,0])
+
+IndexedFaceSet36.setCoord(Coordinate38)
+
+Shape35.setGeometry(IndexedFaceSet36)
+Appearance39 = x3d.Appearance()
+Material40 = x3d.Material()
+Material40.setDiffuseColor([1,1,0])
+Material40.setTransparency(0.3)
+
+Appearance39.setMaterial(Material40)
+
+Shape35.setAppearance(Appearance39)
+
+Transform34.addChildren(Shape35)
+
+Transform18.addChildren(Transform34)
+
+Group17.addChildren(Transform18)
+
+Scene10.addChildren(Group17)
+NavigationInfo41 = x3d.NavigationInfo()
+NavigationInfo41.setSpeed(1.5)
+
+Scene10.addChildren(NavigationInfo41)
+Viewpoint42 = x3d.Viewpoint()
+Viewpoint42.setCenterOfRotation([0,1,0])
+Viewpoint42.setDescription("JohnBoy")
+Viewpoint42.setPosition([0,1,3])
+
+Scene10.addChildren(Viewpoint42)
+
+X3D0.setScene(Scene10)
+X3D0.toFileX3D("../data/skeletonJoe_RoundTrip.x3d")
