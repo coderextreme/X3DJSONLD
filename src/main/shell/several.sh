bb969ef60354dd3bf2fd99d635f2a2f1be28fa05
John Carlson <yottzumm@gmail.com>
2021-01-06 10:00:23 -0600

    Explorations of xslt3

--- src/main/shell/several.sh
+++ src/main/shell/several.sh
@@ -88,3 +88,10 @@ do
 	echo ${NODE} ${NODEDIR}/xmldiff.js $X3D $i
 	${NODE} ${NODEDIR}/xmldiff.js $X3D $i
 done
+for i in `ls -d "$@"| grep -v "\.new"`
+do
+echo ../../../node_modules/.bin/xslt3 -xsl:/c/x3d-code/www.web3d.org/x3d/stylesheets/X3dToJson.xslt -s:$i -o:`dirname $i`/`basename $i .x3d`.json2
+../../../node_modules/.bin/xslt3 -xsl:/c/x3d-code/www.web3d.org/x3d/stylesheets/X3dToJson.xslt -s:$i -o:`dirname $i`/`basename $i .x3d`.json2
+echo diff -w `dirname $i`/`basename $i x3d`json `dirname $i`/`basename $i x3d`json2
+diff -w `dirname $i`/`basename $i x3d`json `dirname $i`/`basename $i x3d`json2
+done
