a26f7ac23635a1130fe1729f9d191117354fb428
John Carlson <coderextreme@coderextreme-Kubuntu20.10>
2021-05-10 22:18:28 -0500

    Removed nashorn, ajv-i18n

--- src/main/nashorn/growl.sh
+++
@@ -1,2 +0,0 @@
-java -Dnashorn.args=--language=es6 -cp "../java;../../../target/X3DJSONLD-8.0-SNAPSHOT.jar;../../../../pythonSAI/X3DJSAIL.4.0.full.jar;../../../saxon9he.jar" net.x3djsonld.data.abox js growl.js
-jjs --language=es6 -cp "../../../target/X3DJSONLD-8.0-SNAPSHOT.jar;../../../../pythonSAI/X3DJSAIL.4.0.full.jar;../../../saxon9he.jar" growl.js
