89658a8245e5cc121da0633cc71d3462a4517f0c
John Carlson <coderextreme@coderextreme-Kubuntu20.10>
2021-05-27 20:20:18 -0500

    added JS, Py files

---
+++ src/main/nashorn/net/x3djsonld/data/HelloWorldProgramOutput.js
@@ -0,0 +1,146 @@
+load('X3Dautoclass.js');
+
+// Javadoc annotations follow, see below for source.
+/**
+ * <p> Example HelloWorldProgram creates an X3D model using the X3D Java Scene Access Interface Library (X3DJSAIL). </p>
+ <p> Related links: HelloWorldProgramOutput.java source, <a href="https://www.web3d.org/x3d/content/examples/X3dResources.html" target="_blank">X3D Resources</a>, <a href="https://www.web3d.org/x3d/content/examples/X3dSceneAuthoringHints.html" target="_blank">X3D Scene Authoring Hints</a> and <a href="https://www.web3d.org/x3d/content/X3dTooltips.html" target="_blank">X3D Tooltips</a>. </p>
+	<table style="color:black; border:0px solid; border-spacing:10px 0px;" summary="Scene Metadata">
+		<tr style="background-color:silver; border-color:silver;">
+			<td style="text-align:center; padding:10px 0px;"><i>meta tags</i></td>
+			<td style="text-align:left;   padding:10px 0px;">net.x3djsonld.data.HelloWorldProgramOutput&nbsp; Document Metadata </td>
+		</tr>
+
+		<tr>
+			<td style="text-align:right; vertical-align: text-top;"> <i> title </i> </td>
+			<td> <a href="https://www.web3d.org/specifications/java/examples/HelloWorldProgramOutput.x3d">HelloWorldProgramOutput.x3d</a> </td>
+		</tr>
+		<tr>
+			<td style="text-align:right; vertical-align: text-top;"> <i> info </i> </td>
+			<td> continued development and testing in progress </td>
+		</tr>
+		<tr>
+			<td style="text-align:right; vertical-align: text-top;"> <i> description </i> </td>
+			<td> Example HelloWorldProgram creates an X3D model using the X3D Java Scene Access Interface Library (X3DJSAIL) </td>
+		</tr>
+		<tr>
+			<td style="text-align:right; vertical-align: text-top;"> <i> reference </i> </td>
+			<td> <a href="https://www.web3d.org/specifications/java/X3DJSAIL.html" target="_blank">https://www.web3d.org/specifications/java/X3DJSAIL.html</a> </td>
+		</tr>
+		<tr>
+			<td style="text-align:right; vertical-align: text-top;"> <i> generator </i> </td>
+			<td> HelloWorldProgramOutput.java </td>
+		</tr>
+		<tr>
+			<td style="text-align:right; vertical-align: text-top;"> <i> created </i> </td>
+			<td> 6 September 2016 </td>
+		</tr>
+		<tr>
+			<td style="text-align:right; vertical-align: text-top;"> <i> modified </i> </td>
+			<td> 20 December 2020 </td>
+		</tr>
+		<tr>
+			<td style="text-align:right; vertical-align: text-top;"> <i> generator </i> </td>
+			<td> X3D Java Scene Access Interface Library (X3DJSAIL) </td>
+		</tr>
+		<tr>
+			<td style="text-align:right; vertical-align: text-top;"> <i> generator </i> </td>
+			<td> <a href="https://www.web3d.org/specifications/java/examples/HelloWorldProgram.java" target="_blank">https://www.web3d.org/specifications/java/examples/HelloWorldProgram.java</a> </td>
+		</tr>
+		<tr>
+			<td style="text-align:right; vertical-align: text-top;"> <i> generator </i> </td>
+			<td> Netbeans <a href="http://www.netbeans.org" target="_blank">http://www.netbeans.org</a> </td>
+		</tr>
+		<tr>
+			<td style="text-align:right; vertical-align: text-top;"> <i> creator </i> </td>
+			<td> Don Brutzman </td>
+		</tr>
+		<tr>
+			<td style="text-align:right; vertical-align: text-top;"> <i> reference </i> </td>
+			<td> <a href="https://sourceforge.net/p/x3d/code/HEAD/tree/www.web3d.org/x3d/stylesheets/java/examples/HelloWorldProgramOutput.x3d" target="_blank">https://sourceforge.net/p/x3d/code/HEAD/tree/www.web3d.org/x3d/stylesheets/java/examples/HelloWorldProgramOutput.x3d</a> </td>
+		</tr>
+		<tr>
+			<td style="text-align:right; vertical-align: text-top;"> <i> reference </i> </td>
+			<td> Console output, ClassicVRML encoding, VRML97 encoding and pretty-print documentation: </td>
+		</tr>
+		<tr>
+			<td style="text-align:right; vertical-align: text-top;"> <i> reference </i> </td>
+			<td> HelloWorldProgramOutput.txt </td>
+		</tr>
+		<tr>
+			<td style="text-align:right; vertical-align: text-top;"> <i> reference </i> </td>
+			<td> <a href="https://www.web3d.org/specifications/java/examples/HelloWorldProgramOutput.x3dv">HelloWorldProgramOutput.x3dv</a> </td>
+		</tr>
+		<tr>
+			<td style="text-align:right; vertical-align: text-top;"> <i> reference </i> </td>
+			<td> HelloWorldProgramOutput.wrl </td>
+		</tr>
+		<tr>
+			<td style="text-align:right; vertical-align: text-top;"> <i> reference </i> </td>
+			<td> <a href="https://www.web3d.org/specifications/java/examples/HelloWorldProgramOutput.html">HelloWorldProgramOutput.html</a> </td>
+		</tr>
+		<tr>
+			<td style="text-align:right; vertical-align: text-top;"> <i> reference </i> </td>
+			<td> <a href="https://savage.nps.edu/X3dValidator?url=https://www.web3d.org/specifications/java/examples/HelloWorldProgramOutput.x3d" target="_blank">https://savage.nps.edu/X3dValidator?url=https://www.web3d.org/specifications/java/examples/HelloWorldProgramOutput.x3d</a> </td>
+		</tr>
+		<tr>
+			<td style="text-align:right; vertical-align: text-top;"> <i> identifier </i> </td>
+			<td> <a href="https://www.web3d.org/specifications/java/examples/HelloWorldProgramOutput.x3d" target="_blank">https://www.web3d.org/specifications/java/examples/HelloWorldProgramOutput.x3d</a> </td>
+		</tr>
+		<tr>
+			<td style="text-align:right; vertical-align: text-top;"> <i> license </i> </td>
+			<td> <a href="https://www.web3d.org/specifications/java/examples/../license.html">../license.html</a> </td>
+		</tr>
+		<tr style="background-color:silver; border-color:silver;">
+			<td style="text-align:center;" colspan="2">  &nbsp; </td>
+		</tr>
+	</table>
+
+	<p>
+		This program uses the
+		<a href="https://www.web3d.org/specifications/java/X3DJSAIL.html" target="_blank">X3D Java Scene Access Interface Library (X3DJSAIL)</a>.
+		It has been produced using the 
+		<a href="https://www.web3d.org/x3d/stylesheets/X3dToES5.xslt" target="_blank">X3dToES5.xslt</a>
+		stylesheet to create ES5 source code from an <code>.x3d</code> scene.
+	</p>
+
+	* @author Don Brutzman
+ */
+
+function HelloWorldProgramOutput
+  /** Default constructor to create this object. */
+  ()
+  {
+    /** The initialized model object, created within initialize() method. */
+    this.x3dModel = {};
+    this.initialize();
+    return this;
+  }
+HelloWorldProgramOutput.prototype = {
+  /** Create and initialize the X3D model. */
+  initialize : function ()
+  {
+  this.x3dModel = new X3D().setProfile("Full").setVersion("4.0")
+  .addComments(" x3dVersionComparisonTest for this model: supportsX3dVersion(X3D.VERSION_3_0)=true ")
+  .setHead(new head()
+    .addComments(" comment #1 ")
+    .addComments(" comment #2 ")
+    .addComments(" comment #3 ")
+    .addComments(" comment #4 ")
+    .addComponent(new component().setName("Navigation").setLevel(3))
+    .addComponent(new component().setName("Shaders").setLevel(1))
+    .addComponent(new component().setName("CADGeometry").setLevel(2))
+    .addComponent(new component().setName("DIS").setLevel(2))
+    .addComponent(new component().setName("H-Anim").setLevel(1))
+    .addComponent(new component().setName("Grouping").setLevel(1))
+    .addComponent(new component().setName("Layering").setLevel(1))
+    .addUnit(new unit().setName("AngleUnitConversion").setConversionFactor(1.0).setCategory("angle"))
+    .addUnit(new unit().setName("LengthUnitConversion").setConversionFactor(1.0).setCategory("length"))
+    .addUnit(new unit().setName("ForceFromPoundsToNewtons").setConversionFactor(4.4482).setCategory("force"))
+    .addMeta(new meta().setName("title").setContent("HelloWorldProgramOutput.x3d"))
+    .addMeta(new meta().setName("info").setContent("continued development and testing in progress"))
+    .addMeta(new meta().setName("description").setContent("Example HelloWorldProgram creates an X3D model using the X3D Java Scene Access Interface Library (X3DJSAIL)"))
+    .addMeta(new meta().setName("reference").setContent("https://www.web3d.org/specifications/java/X3DJSAIL.html"))
+    .addMeta(new meta().setName("generator").setContent("HelloWorldProgramOutput.java"))
+    .addMeta(new meta().setName("created").setContent("6 September 2016"))
+    .addMeta(new meta().setName("modified").setContent("20 December 2020"))
+    .addMeta(new meta().setN
