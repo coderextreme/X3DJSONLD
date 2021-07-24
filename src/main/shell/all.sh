0a89dbd77aec219e23d66a40f8fa020c83c41dd5
coderextreme <yottzumm@gmail.com>
2021-07-24 18:29:36 -0500

    Added bash

--- src/main/shell/all.sh
+++ src/main/shell/all.sh
@@ -5,5 +5,5 @@
 # Run examples through test process
 
 export PROCESSORS=1
-find /c/x3d-code/www.web3d.org/x3d/content/examples/ -name '*.x3d' | xargs -P $PROCESSORS ./several.sh
+find /c/x3d-code/www.web3d.org/x3d/content/examples/ -name '*.x3d' | xargs -P $PROCESSORS bash ./several.sh
 
