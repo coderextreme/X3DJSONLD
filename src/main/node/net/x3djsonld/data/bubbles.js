4640bed596e487ea8a167d8eef3fbf1e9e211de0
coderextreme <yottzumm@gmail.com>
2021-08-16 15:27:35 -0500

    Updated

--- src/main/node/net/x3djsonld/data/bubbles.js
+++ src/main/node/net/x3djsonld/data/bubbles.js
@@ -69,7 +69,7 @@ bubbles.prototype = {
   {
   this.x3dModel = (new autoclass.X3D()).setProfile("Immersive").setVersion("4.0")
   .setHead((new autoclass.head())
-    .addComponent((new autoclass.component()).setName("EnvironmentalEffects").setLevel(1))
+    .addComments("component name='EnvironmentalEffects' level='1'/")
     .addComponent((new autoclass.component()).setName("EnvironmentalEffects").setLevel(3))
     .addComponent((new autoclass.component()).setName("Shaders").setLevel(1))
     .addComponent((new autoclass.component()).setName("CubeMapTexturing").setLevel(1))
