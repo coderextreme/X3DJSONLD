0505bbb82e8868d8bf80dae0d2de07100c190757
coderextreme <yottzumm@gmail.com>
2021-08-29 00:38:18 -0500

    Working on getting python working

---
+++ src/main/shell/donpython.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Run the Test Suite
+
+# accepts files with .x3d extension
+export PROCESSORS=${PROCESSORS-8}
+
+. ./classpath
+
+echo translating to python
+(find /c/x3d-code/www.web3d.org/x3d/content/examples -type f -name '*.x3d' | grep -v intermediate | grep -v "\.new") | xargs -P $PROCESSORS java net.coderextreme.RunSaxon ---overwrite ---silent --../lib/stylesheets/X3dToPython.xslt -py ---../python/net/x3djsonld/data/
+echo running python
+pushd ../python/net/x3djsonld/data
+export PYTHONPATH=`pwd`
+find c -name '*.py' | xargs -L 1 -P $PROCESSORS python3
+popd
