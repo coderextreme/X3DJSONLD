c3616a1b83cfa454c446348e66f16a24975aaec4
coderextreme <yottzumm@gmail.com>
2021-10-12 08:54:09 -0500

    More updates and fixed, actually validate against XMLSpy converted schema

--- src/main/python/2020-12validate.py
+++ src/main/python/2020-12validate.py
@@ -2,14 +2,24 @@ from jsonschema import validate
 import json
 import sys
 
-# validate stdin against the 2020-12 JSON schema
+# validate stdin against the X3D4 JSON schemas
 
 try:
     instance = json.loads(sys.stdin.read())
-    with open('../schema/2020-12-JSONSchema.json', 'r') as schemafile:
+    with open('../schema/x3d-4.0-JSONSchema.json', 'r') as schemafile:
         schema = json.load(schemafile)
         validate(instance=instance, schema=schema)
-        sys.stdout.write("Valid ")
+        sys.stdout.write("jwcValid ")
 except:
-        sys.stdout.write("Invalid ")
+        sys.stdout.write("jwcInvalid ")
+sys.stdout.flush()
+
+try:
+    instance = json.loads(sys.stdin.read())
+    with open('../schema/X3dXml4.0SchemaConvertedToJson2020-12Schema.json', 'r') as schemafile:
+        schema = json.load(schemafile)
+        validate(instance=instance, schema=schema)
+        sys.stdout.write("dbValid ")
+except:
+        sys.stdout.write("dbInvalid ")
 sys.stdout.flush()
