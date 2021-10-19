01874d3911238c93c37c37b50bb7e6eb7a519545
coderextreme <yottzumm@gmail.com>
2021-10-18 20:19:55 -0500

    Working on test.py subprograms

--- src/main/python/2020-12validate.py
+++ src/main/python/2020-12validate.py
@@ -2,6 +2,9 @@ from jsonschema import validate
 import json
 import sys
 
+v = []
+errors = []
+
 
 try:
     instance = json.loads(sys.stdin.read())
@@ -14,17 +17,24 @@ except:
 try:
     with open('../schema/x3d-4.0-JSONSchema.json', 'r') as schemafile:
         schema = json.load(schemafile)
-        validate(instance=instance, schema=schema)
+        v = validate(instance=instance, schema=schema)
+
         sys.stdout.write("jwcValid ")
 except:
         sys.stdout.write("jwcInvalid ")
+        errors = sorted(v.iter_errors(instance), key=lambda e: e.path)
+        for error in errors:
+            print(error.message)
 sys.stdout.flush()
 
 try:
     with open('../schema/X3dXml4.0SchemaConvertedToJson2020-12Schema.json', 'r') as schemafile:
         schema = json.load(schemafile)
-        validate(instance=instance, schema=schema)
+        v = validate(instance=instance, schema=schema)
         sys.stdout.write("dbValid ")
 except:
         sys.stdout.write("dbInvalid ")
+        errors = sorted(v.iter_errors(instance), key=lambda e: e.path)
+        for error in errors:
+            print(error.message)
 sys.stdout.flush()
