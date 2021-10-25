8091f9371cba2f477a8209de4f94b0085a1d040e
coderextreme <yottzumm@gmail.com>
2021-10-25 00:37:02 -0500

    Updated for non-virtual environment

---
+++ src/main/python/2020_12validate.py
@@ -0,0 +1,53 @@
+import os
+import sys
+from jsonschema import validate
+import json
+
+# validate the X3D Resources examples with draft 2020-12 JSON schema
+# validate one instance "instance" against X3D JSON Schema 4.0
+def validate_one40(schema, instance):
+    v = []
+    errors = []
+
+    v = validate(instance=instance, schema=schema)
+    try:
+        errors = sorted(v.iter_errors(instance), key=lambda e: e.path)
+        for error in errors:
+            print(error.message)
+    except:
+        pass
+
+for base, directories, filenames in list(os.walk("/c/x3d-code/www.web3d.org/x3d/content/examples")):
+    for filename in filenames:
+        if filename.endswith(".json"):
+            fullpath = os.path.join(base, filename)
+            try:
+                f = open(fullpath, "r")
+                instance = json.load(f)
+                f.close()
+                print("jsonValid", end=" ")
+            except:
+                print("jsonInvalid", end=" ")
+
+
+            schemapre = "jwc"
+            #try:
+            with open('../schema/x3d-4.0-JSONSchema.json', 'r') as schemafile:
+                schema = json.load(schemafile)
+                validate_one40(schema, instance)
+                print(f"{schemapre}Valid", end=" ")
+           # except:
+           #    print(f"{schemapre}Invalid", end=" ")
+
+
+
+            schemapre = "db"
+            #try:
+            #with open('../schema/X3dXml4.0SchemaConvertedToJson2020-12Schema.json', 'r') as schemafile:
+            #    schema = json.load(schemafile)
+            #    validate_one40(schema, instance)
+            #    print(f"{schemapre}Valid", end=" ")
+            #except:
+            #    print(f"{schemapre}Invalid", end=" ")
+
+            print(fullpath)
