dca5b4821bcb36d638091f0d9e7001318260a1bc
coderextreme <yottzumm@gmail.com>
2021-11-01 00:59:02 -0500

    Added JoeExample

---
+++ src/main/cplusplus/net/coderextreme/data/JoeExample.cpp
@@ -0,0 +1,124 @@
+/* delete next 5 lines for Visual Studio */
+#define FALSE false
+#define TRUE true
+#define WINAPI
+#define AFX_EXT_CLASS
+#define EXPORT32
+#include "/c/x3d-code/www.web3d.org/x3d/languages/cpp/X3DLib/X3DLib.h"
+int main(int argc, char ** argv) {
+CX3DScene m_pScene;
+CBrowser browser = X3D.getBrowser();
+CX3D* X3D0 = new CX3D();
+CGroup* group = (CGroup*)(m_pScene.createNode("Group"));
+group->addChildren(X3D0);
+X3D0->setVersion("3.0");
+X3D0->setProfile("Immersive");
+Chead* head1 = new Chead();
+Cmeta* meta2 = new Cmeta();
+meta2->setName("filename");
+meta2->setContent("Scripting.x3d");
+head1->addMeta(*meta2);
+
+X3D0->setHead(*head1);
+
+CScene* Scene3 = new CScene();
+CScript* Script4 = (CScript *)(m_pScene.createNode("Script"));
+Script4->setDEF("OpenVault");
+Cfield* field5 = new Cfield();
+field5->setName("openVault");
+field5->setType("SFTime");
+field5->setAccessType("inputOnly");
+Script4->addField(*field5);
+
+Cfield* field6 = new Cfield();
+field6->setName("combinationEntered");
+field6->setType("SFBool");
+field6->setAccessType("inputOnly");
+Script4->addField(*field6);
+
+Cfield* field7 = new Cfield();
+field7->setName("vaultUnlocked");
+field7->setType("SFTime");
+field7->setAccessType("outputOnly");
+Script4->addField(*field7);
+
+Cfield* field8 = new Cfield();
+field8->setName("unlocked");
+field8->setType("SFBool");
+field8->setValue("false");
+field8->setAccessType("initializeOnly");
+Script4->addField(*field8);
+
+
+Script4.setSourceCode(`ecmascript:\n"+
+"\n"+
+"      function combinationEntered (value) {\n"+
+"\n"+
+"        unlocked = value;\n"+
+"\n"+
+"      }\n"+
+"\n"+
+"      function openVault(value) {\n"+
+"\n"+
+"      if (unlocked) vaultUnlocked = value;\n"+
+"\n"+
+"      }`)
+group->addChildren(*Script4);
+
+CShape* Shape9 = (CShape *)(m_pScene.createNode("Shape"));
+CAppearance* Appearance10 = (CAppearance *)(m_pScene.createNode("Appearance"));
+CMaterial* Material11 = (CMaterial *)(m_pScene.createNode("Material"));
+Material11->setDiffuseColor(new float[3]{1,0,0});
+Appearance10->setMaterial(*Material11);
+
+Shape9->setAppearance(*Appearance10);
+
+CSphere* Sphere12 = (CSphere *)(m_pScene.createNode("Sphere"));
+Shape9->setGeometry(Sphere12);
+
+group->addChildren(*Shape9);
+
+CSound* Sound13 = (CSound *)(m_pScene.createNode("Sound"));
+Sound13->setMaxBack(1000);
+Sound13->setMaxFront(1000);
+Sound13->setMinBack(1000);
+Sound13->setMinFront(1000);
+CAudioClip* AudioClip14 = (CAudioClip *)(m_pScene.createNode("AudioClip"));
+AudioClip14->setDEF("Click");
+AudioClip14->setStopTime(1);
+AudioClip14->setDescription("clicking sound");
+AudioClip14->setUrl(new CString[1]{"click.wav"}, 1);
+Sound13->setSource(*AudioClip14);
+
+group->addChildren(*Sound13);
+
+CTouchSensor* TouchSensor15 = (CTouchSensor *)(m_pScene.createNode("TouchSensor"));
+TouchSensor15->setDEF("TS");
+group->addChildren(*TouchSensor15);
+
+CROUTE* ROUTE16 = new CROUTE();
+ROUTE16->setFromNode("TS");
+ROUTE16->setFromField("isOver");
+ROUTE16->setToNode("OpenVault");
+ROUTE16->setToField("combinationEntered");
+group->addChildren(*ROUTE16);
+
+CROUTE* ROUTE17 = new CROUTE();
+ROUTE17->setFromNode("TS");
+ROUTE17->setFromField("touchTime");
+ROUTE17->setToNode("OpenVault");
+ROUTE17->setToField("openVault");
+group->addChildren(*ROUTE17);
+
+CROUTE* ROUTE18 = new CROUTE();
+ROUTE18->setFromNode("OpenVault");
+ROUTE18->setFromField("vaultUnlocked");
+ROUTE18->setToNode("Click");
+ROUTE18->setToField("startTime");
+group->addChildren(*ROUTE18);
+
+X3D0->setScene(*Scene3);
+
+m_pScene.addRootNode(group);
+X3D0->toXMLString();
+}
---
+++ src/main/vrml/net/coderextreme/data/JoeExample.js
@@ -0,0 +1,113 @@
+let browser = X3D.getBrowser();
+let X3D0 = {};
+X3D0.version = "3.0";
+X3D0.profile = "Immersive";
+let head1 = browser.currentScene.createNode("head");
+let meta2 = browser.currentScene.createNode("meta");
+meta2.name = "filename";
+meta2.content = "Scripting.x3d";
+head1.meta = new MFNode();
+
+head1.meta[0] = meta2;
+
+head = head1;
+
+let Script4 = browser.currentScene.createNode("Script");
+Script4.DEF = "OpenVault";
+let field5 = browser.currentScene.createNode("field");
+field5.name = "openVault";
+field5.type = "SFTime";
+field5.accessType = "inputOnly";
+Script4.field = new MFNode();
+
+Script4.field[0] = field5;
+
+let field6 = browser.currentScene.createNode("field");
+field6.name = "combinationEntered";
+field6.type = "SFBool";
+field6.accessType = "inputOnly";
+Script4.field[1] = field6;
+
+let field7 = browser.currentScene.createNode("field");
+field7.name = "vaultUnlocked";
+field7.type = "SFTime";
+field7.accessType = "outputOnly";
+Script4.field[2] = field7;
+
+let field8 = browser.currentScene.createNode("field");
+field8.name = "unlocked";
+field8.type = "SFBool";
+field8.value = "false";
+field8.accessType = "initializeOnly";
+Script4.field[3] = field8;
+
+
+Script4.setSourceCode(`ecmascript:\n"+
+"\n"+
+"      function combinationEntered (value) {\n"+
+"\n"+
+"        unlocked = value;\n"+
+"\n"+
+"      }\n"+
+"\n"+
+"      function openVault(value) {\n"+
+"\n"+
+"      if (unlocked) vaultUnlocked = value;\n"+
+"\n"+
+"      }`)
+browser.currentScene.children = new MFNode();
+
+browser.currentScene.children[0] = Script4;
+
+let Shape9 = browser.currentScene.createNode("Shape");
+let Appearance10 = browser.currentScene.createNode("Appearance");
+let Material11 = browser.currentScene.createNode("Material");
+Material11.diffuseColor = new SFColor(new float[1,0,0]);
+Appearance10.material = Material11;
+
+Shape9.appearance = Appearance10;
+
+let Sphere12 = browser.currentScene.createNode("Sphere");
+Shape9.geometry = Sphere12;
+
+browser.currentScene.children[1] = Shape9;
+
+let Sound13 = browser.currentScene.createNode("Sound");
+Sound13.maxBack = 1000;
+Sound13.maxFront = 1000;
+Sound13.minBack = 1000;
+Sound13.minFront = 1000;
+let AudioClip14 = browser.currentScene.createNode("AudioClip");
+AudioClip14.DEF = "Click";
+AudioClip14.stopTime = 1;
+AudioClip14.description = "clicking sound";
+AudioClip14.url = new MFString(new java.lang.String["click.wav"]);
+Sound13.source = AudioClip14;
+
+browser.currentScene.children[2] = Sound13;
+
+let TouchSensor15 = browser.currentScene.createNode("TouchSensor");
+TouchSensor15.DEF = "TS";
+browser.currentScene.children[3] = TouchSensor15;
+
+let ROUTE16 = browser.currentScene.createNode("ROUTE");
+ROUTE16.fromNode = "TS";
+ROUTE16.fromField = "isOver";
+ROUTE16.toNode = "OpenVault";
+ROUTE16.toField = "combinationEntered";
+browser.currentScene.children[4] = ROUTE16;
+
+let ROUTE17 = browser.currentScene.createNode("ROUTE");
+ROUTE17.fromNode = "TS";
+ROUTE17.fromField = "touchTime";
+ROUTE17.toNode = "OpenVault";
+ROUTE17.toField = "openVault";
+browser.currentScene.children[5] = ROUTE17;
+
+let ROUTE18 = browser.currentScene.createNode("ROUTE");
+ROUTE18.fromNode = "OpenVault";
+ROUTE18.fromField = "vaultUnlocked";
+ROUTE18.toNode = "Click";
+ROUTE18.toField = "startTime";
+browser.currentScene.children[6] = ROUTE18;
+
