c9b118d081b63e5f6fb7e642eefc9ddf94e9b84e
coderextreme <yottzumm@gmail.com>
2022-01-03 18:02:35 -0600

    New JSON parser (very short!) replacing x3dinput.py

---
+++ src/main/python/x3djsonld.py
@@ -0,0 +1,91 @@
+print("from x3d import *")
+print("#", end="")
+import json
+import sys
+from x3d import *
+
+print("print(", end="")
+
+def parseArray(parent, data, indent=0):
+    out = ""
+    els = 0
+    for d in data:
+        if els > 0:
+                out += (",")
+        els += 1
+        if isinstance(d, dict):
+            out += parseObject(parent, d,indent+1)
+        else:
+            if isinstance(d, str):
+                out += "'"
+                out += str(d)
+                out += "'"
+            else:
+                out +=  str(d)
+    return out
+
+def parseObject(parent, data,indent=0):
+    out = "\n"+(("  ")*indent)
+    els = 0
+    if els == 0:
+        ndent = 0
+    else:
+        ndent = 1
+    if parent in ("meta", "field", "fieldValue", "connect"):
+        out += parent+"("
+    for k,v in data.items():
+        if k in ("encoding", "@xsd:noNamespaceSchemaLocation", "JSON schema"):
+            continue
+        if els > 0:
+            out += (", ")
+            ndent = 0
+        else:
+            ndent = 0
+        els += 1
+        key = k
+        if k.startswith("@") or k.startswith("-"):
+            key = k[1:]
+        if k.startswith("#"):
+            out +=  "\n"*ndent+(("  ")*ndent)+ "#" + str(v) +  "\n"+(("  ")*2)
+        elif isinstance(v, dict):
+            if key in ("head"):
+                out += "\n"*ndent+(("  ")*ndent)+key+"="+key+"("
+                out += "\n"*ndent+(("  ")*(ndent+1))+"children=["
+                out += parseObject(k, v,indent+2)
+                out += "]"
+                out += ")"
+            elif key in ("Scene", "IS", "ProtoBody", "ProtoInterface"):
+                out += "\n"*ndent+(("  ")*ndent)+key+"="+key+"("
+                out += parseObject(k, v,indent+1)
+                out += ")"
+            elif key in ("geometry", "appearance", "material", "texture", "fontStyle"):
+                out += "\n"*ndent+(("  ")*ndent)+key+"="
+                out += parseObject(k, v,indent+1)
+            else:
+                out += "\n"*ndent+(("  ")*ndent)+key+"("
+                out += parseObject(k, v,indent+1)
+                out += ")"
+        elif isinstance(v, list):
+            if key in ("children", "url", "justify", "string", "field", "fieldValue", "connect"):
+                out += "\n"*ndent+(("  ")*ndent)+key
+                out += "=["
+                out += parseArray(k, v,indent+1)
+                out += "]"
+            elif key in ("meta"):
+                out += parseArray(k, v,indent+1)
+            else:
+                out +=  "\n"*ndent+(("  ")*ndent)+key
+                out += "=("
+                out += parseArray(k, v,indent+1)
+                out += ")"
+        else:
+            out +=  "\n"*ndent+(("  ")*ndent)+str(key) + "='" + str(v) + "'"
+    if parent in ("meta", "field", "fieldValue", "connect"):
+        out += ")"
+    return out
+
+
+with open('../data/abox.json' if len(sys.argv) == 1 else sys.argv[1], encoding="utf-8") as json_file:
+    data = json.load(json_file)
+    print(parseObject("X3D", data))
+    print(".XML())")
