b7fb24a1defcad6ff152757d0e55c2640b028b2a
John Carlson <yottzumm@gmail.com>
2022-03-23 22:30:07 -0500

    Updated

--- src/main/python/net/coderextreme/data/TextSpecialCharacters.py
+++ src/main/python/net/coderextreme/data/TextSpecialCharacters.py
@@ -1,4 +1,1 @@
-#x3d.py package 4.0.60 loaded, have fun with X3D Graphics!
-from x3d import *
-from x3d import SFBool
-print(
+#
--- src/main/python/net/coderextreme/data/ifscube.py
+++ src/main/python/net/coderextreme/data/ifscube.py
@@ -1,18 +1,1 @@
-#x3d.py package 4.0.60 loaded, have fun with X3D Graphics!
-from x3d import *
-from x3d import SFBool
-print(
-X3D(profile="Interchange", version="3.3", 
-head=head(), 
-Scene=Scene(children=[
-Group(children=[
-Shape(
-geometry=
-IndexedFaceSet(creaseAngle=1.57, DEF="IndexedFaceSet", coordIndex=[int(0),int(0),int(1),int(-1),int(0),int(1),int(1),int(-1),int(2),int(2),int(3),int(3),int(-1),int(0),int(3),int(3),int(0),int(-1),int(0),int(3),int(2),int(1),int(-1),int(1),int(2),int(2),int(1),int(-1),int(1),int(2),int(3),int(0),int(-1)], normalIndex=[int(0),int(-1),int(0),int(-1),int(1),int(-1),int(2),int(-1),int(3),int(-1),int(4),int(-1),int(5),int(-1)], normalPerVertex=False, colorIndex=[int(0),int(0),int(0),int(-1),int(0),int(0),int(0),int(-1),int(0),int(0),int(0),int(0),int(-1),int(0),int(0),int(0),int(0),int(-1),int(0),int(0),int(0),int(0),int(-1),int(0),int(0),int(0),int(0),int(-1),int(0),int(0),int(0),int(0),int(-1)], 
-coord=
-Coordinate(point=[(0,0,1),(0,1,1),(1,1,1),(1,0,1)]), 
-normal=
-Normal(vector=[(1,0,0),(-1,0,0),(0,1,0),(0,0,-1),(0,-1,0),(0,0,1)]), 
-color=
-Color(color=[(0,1,0)])))])]))
-.XML())
+#
