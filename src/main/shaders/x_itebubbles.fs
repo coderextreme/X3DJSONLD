8f950ff9a362c69bd62ff8a32027914d3208e387
John Carlson <yottzumm@gmail.com>
2022-05-05 13:03:37 -0500

    Updated X_ITE shader to current spec

--- src/main/shaders/x_itebubbles.fs
+++ src/main/shaders/x_itebubbles.fs
@@ -12,7 +12,7 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-uniform samplerCube x3d_CubeMapTexture [x3d_MaxTextures];
+uniform samplerCube x3d_TextureCube [x3d_MaxTextures];
 
 in vec3  t;
 in vec3  tr;
@@ -25,12 +25,12 @@ out vec4 x3d_FragColor;
 void
 main ()
 {
-  vec4 ref = texture (x3d_CubeMapTexture [0], t);
+  vec4 ref = texture (x3d_TextureCube [0], t);
   vec4 ret = vec4 (1.0);
 
-  ret .r = texture (x3d_CubeMapTexture [0], tr) .r;
-  ret .g = texture (x3d_CubeMapTexture [0], tg) .g;
-  ret .b = texture (x3d_CubeMapTexture [0], tb) .b;
+  ret .r = texture (x3d_TextureCube [0], tr) .r;
+  ret .g = texture (x3d_TextureCube [0], tg) .g;
+  ret .b = texture (x3d_TextureCube [0], tb) .b;
 
   x3d_FragColor = mix (ref, ret, rfac);
 }
