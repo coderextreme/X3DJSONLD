a88b35fff6b5d82c022297cc0d3243688c65486b
John Carlson <yottzumm@gmail.com>
2022-07-24 02:28:06 -0500

    Updated

--- src/main/node/net/x3djsonld/data/bubbles.js
+++ src/main/node/net/x3djsonld/data/bubbles.js
@@ -11,11 +11,11 @@ var autoclass = require('./X3Dautoclass');
 // Javadoc annotations follow, see below for source.
 /**
  * <p> Bubble animation used by Nancy Diving example. </p>
- <p> Related links: Bubbles.java source, <a href="https://www.web3d.org/x3d/content/examples/X3dResources.html" target="_blank">X3D Resources</a>, <a href="https://www.web3d.org/x3d/content/examples/X3dSceneAuthoringHints.html" target="_blank">X3D Scene Authoring Hints</a> and <a href="https://www.web3d.org/x3d/content/X3dTooltips.html" target="_blank">X3D Tooltips</a>. </p>
+ <p> Related links: bubbles.java source, <a href="https://www.web3d.org/x3d/content/examples/X3dResources.html" target="_blank">X3D Resources</a>, <a href="https://www.web3d.org/x3d/content/examples/X3dSceneAuthoringHints.html" target="_blank">X3D Scene Authoring Hints</a> and <a href="https://www.web3d.org/x3d/content/X3dTooltips.html" target="_blank">X3D Tooltips</a>. </p>
 	<table style="color:black; border:0px solid; border-spacing:10px 0px;" summary="Scene Metadata">
 		<tr style="background-color:silver; border-color:silver;">
 			<td style="text-align:center; padding:10px 0px;"><i>meta tags</i></td>
-			<td style="text-align:left;   padding:10px 0px;">net.x3djsonld.data.Bubbles&nbsp; Document Metadata </td>
+			<td style="text-align:left;   padding:10px 0px;">net.x3djsonld.data.bubbles&nbsp; Document Metadata </td>
 		</tr>
 
 		<tr>
@@ -66,7 +66,7 @@ var autoclass = require('./X3Dautoclass');
 	* @author Etsuko Lippi
  */
 
-function Bubbles
+function bubbles
   /** Default constructor to create this object. */
   ()
   {
@@ -75,7 +75,7 @@ function Bubbles
     this.initialize();
     return this;
   }
-Bubbles.prototype = {
+bubbles.prototype = {
   /** Create and initialize the X3D model. */
   initialize : function ()
   {
@@ -181,7 +181,7 @@ Bubbles.prototype = {
 
 
   /** Provide a shallow copy of the X3D model.
-   * @return Bubbles model
+   * @return bubbles model
    */
   getX3dModel : function()
   {	  
@@ -229,8 +229,8 @@ Bubbles.prototype = {
      */
     main : function (argv)
     {
-		var testObject = new Bubbles();
-		console.log ("Bubbles execution self-validation test results: " + testObject.validateSelf());
+		var testObject = new bubbles();
+		console.log ("bubbles execution self-validation test results: " + testObject.validateSelf());
 	}
 }
-new Bubbles().main();
+new bubbles().main();
