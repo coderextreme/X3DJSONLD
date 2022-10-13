ff6ce322357d0c5cd204aee62e19c68fe09bf6eb
John Carlson <yottzumm@gmail.com>
2022-10-13 01:07:50 -0500

    Finished up Three.js stuff for now

---
+++ src/main/node/xml23.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// Convert X3D JSON to various formats
+
+process.argv.shift();
+process.argv.shift();
+
+var convertXML = require('./convertXML.js');
+
+convertXML([
+	{ 
+	serializer : './ThreeSerializer.js',
+
+	folder : "../three/",
+	extension : ".three.json",
+	}
+	]);
