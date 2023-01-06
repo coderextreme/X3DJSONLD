0d061666b75d10a92e7ca5c97c6495630dde796e
John Carlson <yottzumm@gmail.com>
2023-01-06 15:54:48 -0600

    Worked on build. scripts

--- src/main/shell/python.sh
+++ src/main/shell/python.sh
@@ -11,6 +11,5 @@ echo translating to python
 (find ../data -type f -name '*.x3d' | grep -v intermediate | grep -v "\.new") | xargs -P $PROCESSORS java net.coderextreme.RunSaxon ---overwrite ---silent --../lib/stylesheets/X3dToPython.xslt -py ---../python/net/x3djsonld/data/
 echo running python
 pushd ../python/net/x3djsonld/data
-export PYTHONPATH=`pwd`
 ls *.py | xargs -L 1 -P $PROCESSORS python3
 popd
