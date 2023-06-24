8bf28c567d4b9a40c3abf4d4a27d988b741ef7cb
John Carlson <yottzumm@gmail.com>
2023-06-24 18:20:49 -0500

    Updated Jin Canonical (pinky toe)

--- src/main/shell/jin.sh
+++ src/main/shell/jin.sh
@@ -5,4 +5,4 @@ node ../node/jin.js
 node ../node/json2all.js ../data/JinLOA4Canonical101.json
 mv ../data/JinLOA4Canonical101.x3d.new ../data/JinLOA4Canonical101.x3d
 ~/Downloads/view3dscene-4.3.0-win64-x86_64/view3dscene/view3dscene.exe ../data/JinLOA4Canonical101.x3d
-grep DEF=.hanim_._talocrural ../data/JinLOA4Canonical101.x3d
+grep DEF=.hanim_._tarsal_distal_interphalangeal_5 ../data/JinLOA4Canonical101.x3d
