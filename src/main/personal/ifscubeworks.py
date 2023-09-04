13d97fa546013219390443203168365a0876b917
John Carlson <yottzumm@gmail.com>
2023-09-04 05:07:23 -0500

    added

---
+++ src/main/personal/ifscubeworks.py
@@ -0,0 +1,33 @@
+# x3d.py package 4.0.64.4 loaded, have fun with X3D Graphics!
+print("<!--")
+from x3d import *
+print("-->")
+import json
+model = (
+X3D(profile="Interchange", version="3.3", 
+head=head(
+meta(name="title", content="ifscubeworks.x3d"),
+meta(name="identifier", content="http://coderextreme.net/X3DJSONLD/src/main/data/ifscubeworks.x3d"),
+meta(name="description", content="Template for an Indexed Face Set"),
+meta(name="creator", content="John Carlson"),
+meta(name="created", content="4 April 2017"),
+meta(name="license", content="https://www.web3d.org/x3d/content/examples/license.html"),
+meta(name="generator", content="X3D-Edit, https://savage.nps.edu/X3D-Edit"),
+meta(name="translated", content="26 August 2023"),
+meta(name="generator", content="X3dToJson.xslt, https://www.web3d.org/x3d/stylesheets/X3dToJson.html"),
+meta(name="reference", content="X3D JSON encoding: https://www.web3d.org/wiki/index.php/X3D_JSON_Encoding")
+]), 
+Scene=Scene(children=[
+WorldInfo(title="ifscubeworks.x3d"),
+Group(children=[
+Shape(
+geometry=
+IndexedFaceSet(DEF="IndexedFaceSet", coordIndex=[0,0,1,-1,0,1,1,-1,2,2,3,3,-1,0,3,3,0,-1,0,3,2,1,-1,1,2,2,1,-1,1,2,3,0,-1], normalIndex=[0,0,1,2,3,4,5], normalPerVertex=False, colorIndex=[0,0,0,-1,0,0,0,-1,0,0,0,0,-1,0,0,0,0,-1,0,0,0,0,-1,0,0,0,0,-1,0,0,0,0,-1], 
+coord=
+Coordinate(point=[(0,0,1),(0,1,1),(1,1,1),(1,0,1)]), 
+normal=
+Normal(vector=[(1,0,0),(-1,0,0),(0,1,0),(0,0,-1),(0,-1,0),(0,0,1)]), 
+color=(
+Color(color=[(0,1,0)]))))])])))
+output = model.JSON()
+json.loads(output)
---
+++ src/main/personal/text.py
@@ -0,0 +1,52 @@
+# x3d.py package 4.0.64.4 loaded, have fun with X3D Graphics!
+print("<!--")
+from x3d import *
+print("-->")
+import json
+model = (
+X3D(profile="Immersive", version="3.3", 
+head=head(
+meta(name="creator", content="John W Carlson"),
+meta(name="created", content="December 13 2015"),
+meta(name="title", content="text.x3d"),
+meta(name="identifier", content="https://coderextreme.net/X3DJSONLD/src/main/data/text.x3d"),
+meta(name="description", content="test 
+ text"),
+meta(name="generator", content="Vim, X3D-Edit, https://savage.nps.edu/X3D-Edit"),
+meta(name="translated", content="26 August 2023"),
+meta(name="generator", content="X3dToJson.xslt, https://www.web3d.org/x3d/stylesheets/X3dToJson.html"),
+meta(name="reference", content="X3D JSON encoding: https://www.web3d.org/wiki/index.php/X3D_JSON_Encoding")
+]), 
+Scene=Scene(children=[
+Transform(children=[
+Shape(
+geometry=
+Text(string=["Node\"\"\""], 
+fontStyle=
+FontStyle()), 
+appearance=
+Appearance(
+material=
+Material())),
+Shape(
+geometry=
+Text(string=["Node2","\\","\\\\","Node2"], 
+fontStyle=
+FontStyle()), 
+appearance=
+Appearance(
+material=
+Material())),
+Shape(
+geometry=
+Text(string=["Node3 \\\\ \\ ","Node3\"\"\""], 
+fontStyle=
+FontStyle()), 
+appearance=
+Appearance(
+material=
+Material())),
+Script(field=[field(name="frontUrls", type="MFString", accessType="initializeOnly", value=["rnl_front.png","uffizi_front.png"])
+], sourceCode="""['', '\t\t\t    ecmascript:', '\t\t\t    var me = \'"1" ""2" "\n3"\';', '\t\t\t    ', '\t\t\t']""",)])])))
+output = model.JSON()
+json.loads(output)
