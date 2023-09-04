877291a09d9212b4df0f1128100980e5ceac26f7
John Carlson <yottzumm@gmail.com>
2023-09-04 04:42:58 -0500

    Tryed to get rid of JQuery

--- src/main/shaders/x_ite.vs
+++ src/main/shaders/x_ite.vs
@@ -1,5 +1,5 @@
 #version 300 es
-precision highp float;
+precision mediump float;
 
 /*
 The MIT License (MIT)
---
+++ src/main/shell/runtidy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export IN=`pwd`/"$1" 
+export OUT=`pwd`/`dirname "$1"`/`basename "$1" .x3d`.x3dj
+echo "npx --yes x3d-tidy -i $IN -o $OUT" >&2
+npx x3d-tidy -i "$IN" -o "$OUT"
+echo "$OUT"
