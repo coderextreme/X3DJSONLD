9be893e3f8d30405f7890b54cc5aaca0b62ae20c
John Carlson <yottzumm@gmail.com>
2023-09-25 00:31:48 -0500

    First X3D HAnim LOA4 export of a skeleton

--- blend/exportAll.py
+++ blend/exportAll.py
@@ -1,7 +1,9 @@
 import bpy
 
-# bpy.ops.import_scene.fbx(filepath = "../../Tufani/LILY_7_3_BLEND.fbx")
-bpy.ops.wm.open_mainfile(filepath="LILY_7_3_BLEND.blend")
+objs = bpy.data.objects
+objs.remove(objs["Cube"], do_unlink=True)
+bpy.ops.import_scene.fbx(filepath = "../../Tufani/LILY_7_3_BLEND.fbx")
+# bpy.ops.wm.open_mainfile(filepath="LILY_7_3_BLEND.blend")
 #bpy.ops.export_scene.gltf(filepath="blenderLily.gltf", export_format='GLTF_EMBEDDED')
 # bpy.ops.export_scene.gltf(filepath="blenderLily2.gltf", export_format='GLTF_SEPARATE') #, export_texture_dir="./textures/")
 
--- blend/hier2x3d.blender.sh
+++ blend/hier2x3d.blender.sh
@@ -2,3 +2,5 @@
 echo "input file is Hierarchy.txt"
 perl hier2x3d.blender.pl < Hierarchy.txt > blenderskeleton.py
 echo "output file is blenderskeleton.py"
+echo "loading into Blender"
+c:/Program\ Files/Blender\ Foundation/Blender\ 3.6/blender.exe --python  blenderskeleton.py
