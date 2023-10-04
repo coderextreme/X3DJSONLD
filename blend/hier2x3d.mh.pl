024f7b3ebc6899db1a7cd0380ab257074ad4e4e6
John Carlson <yottzumm@gmail.com>
2023-10-03 19:56:14 -0500

    Checked in these files for good luck.  They might not work yet.

--- blend/hier2x3d.mh.pl
+++ blend/hier2x3d.mh.pl
@@ -10,7 +10,7 @@ my %joints = ();
 my $segments = "";
 
 sub printSegment {
-	my($jnt, $parent_joint, $parent_segment) = @_;
+	my($jnt, $parent_joint, $parent_segment, $grandparent_segment) = @_;
 	my $cenpj = $joints{$parent_joint};
 	my @cenpj = split(/[ ,\t]+/, $cenpj);
 	my $cencj = "0 0 0";
@@ -20,31 +20,41 @@ sub printSegment {
 		$cencj = $joints{$jnt->{joint}};
 	}
 	my @cencj = split(/[ ,\t]+/, $cencj);
-	$segments .= " " x $jnt->{indent}. '"'.$jnt->{segment}.'": {'."\n";
-	$segments .= '"head": {'."\n";
-	$segments .= 	'"cube_name": "'.$parent_joint.'",'."\n";
-	$segments .= 	'"default_position": ['."\n";
-	$segments .= 		$cenpj[0].",\n";
-	$segments .= 		$cenpj[2].",\n";
-	$segments .= 		$cenpj[1]."\n";
-	$segments .= 	'],'."\n";
-	$segments .= 	'"strategy": "CUBE" '."\n";
-	$segments .= '},'."\n";
-	$segments .= '"parent": "'.$parent_segment.'",'."\n";
-	$segments .= '"tail": {'."\n";
-	$segments .= 	'"cube_name": "'.$jnt->{joint}.'",'."\n";
-	$segments .= 	'"default_position": ['."\n";
-	$segments .= 		$cencj[0].",\n";
-	$segments .= 		$cencj[2].",\n";
-	$segments .= 		$cencj[1]."\n";
-	$segments .= 	'],'."\n";
-	$segments .= '"strategy": "CUBE" '."\n";
-	$segments .= '}'."\n";
-	$segments .= '},'."\n";
+	my $l1 = $jnt->{indent};
+	my $l2 = $l1 + 2;
+	my $l3 = $l2 + 2;
+	my $l4 = $l3 + 2;
+	$segments .= " " x $l1. '"'.$parent_segment.'": {'."\n";
+	$segments .= " " x $l2.'"head": {'."\n";
+	$segments .= " " x $l3.		'"cube_name": "'.$parent_joint.'",'."\n";
+	$segments .= " " x $l3.		'"default_position": ['."\n";
+	$segments .= " " x $l4.			$cenpj[0].",\n";
+	$segments .= " " x $l4.			$cenpj[2].",\n";
+	$segments .= " " x $l4.			$cenpj[1]."\n";
+	$segments .= " " x $l3.		'],'."\n";
+	$segments .= " " x $l3.		'"strategy": "CUBE" '."\n";
+	$segments .= " " x $l2.	'},'."\n";
+	$segments .= " " x $l2.	'"parent": "'.$grandparent_segment.'",'."\n";
+	$segments .= " " x $l2.	'"tail": {'."\n";
+	$segments .= " " x $l3.		'"cube_name": "'.$jnt->{joint}.'",'."\n";
+	$segments .= " " x $l3.		'"default_position": ['."\n";
+	$segments .= " " x $l4.			$cencj[0].",\n";
+	$segments .= " " x $l4.			$cencj[2].",\n";
+	$segments .= " " x $l4.			$cencj[1]."\n";
+	$segments .= " " x $l3.		'],'."\n";
+	$segments .= " " x $l3.		'"strategy": "CUBE",'."\n";
+        $segments .= " " x $l3.		'"inherit_scale": "FULL",'."\n";
+        $segments .= " " x $l3.		'"rigify": {},'."\n";
+        $segments .= " " x $l3.		'"roll": 3.1415927410125732,'."\n";
+        $segments .= " " x $l3.		'"use_connect": false,'."\n";
+        $segments .= " " x $l3.		'"use_inherit_rotation": true,'."\n";
+        $segments .= " " x $l3.		'"use_local_location": true'."\n";
+	$segments .= " " x $l2.	'}'."\n";
+	$segments .= " " x $l1.'},'."\n";
 }
 
 sub printPython {
-	my($prev) = @_;  # $prev is $child
+	my($prev, $grandparent_segment) = @_;  # $prev is $child
 	$prev->{line} =~ /([ |]*)(.*)[ \t]+:[ \t]+(.*)/;
 	my $parent_indent = length($1);
 	my $parent_joint = $2;
@@ -55,8 +65,8 @@ sub printPython {
 		foreach my $jnt (@{$prev->{children}}) {   # go through child joints
 			#if ($jnt->{joint} =~ m/_tarsal_interphalangeal_1$/) { # we need to take this out
 				# skip
-			&printSegment($jnt, $parent_joint, $parent_segment);
-			&printPython($jnt);
+			&printSegment($jnt, $parent_joint, $parent_segment, $grandparent_segment);
+			&printPython($jnt, $parent_segment);
 		}
 	}
 }
@@ -124,5 +134,5 @@ my $parent_segment = $3;
 $parent_joint =~ s/\r//;
 $parent_segment =~ s/\r//;
 # &printSegment($prev, $parent_joint, $parent_segment);
-&printPython($prev);
+&printPython($prev, $parent_segment);
 print STDOUT "{\n$segments\n}\n";
