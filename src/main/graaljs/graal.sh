487b4078a8055cf2f87e4b2020e8770e9b0c5423
John Carlson <yottzumm@gmail.com>
2024-02-29 19:28:55 -0600

    Added new GraalJS SAI examples, deleted *.new.json files (being replaced)

---
+++ src/main/graaljs/graal.sh
@@ -0,0 +1,1 @@
+C:/graaljs-community-jvm-23.1.2-windows-amd64/graaljs-community-23.1.2-windows-amd64/bin/js.exe --experimental-options --polyglot --vm.Djs.allowAllAccess=true --vm.Xss1g --vm.Xmx4g --jvm --vm.classpath="C:/Users/john/pythonSAI/X3DJSAIL.4.0.full.jar;." "$@"
