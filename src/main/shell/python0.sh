a84af745134a5c23928e1de525820d71b7a3c58a
John Carlson <yottzumm@gmail.com>
2024-03-03 04:24:17 -0600

    Now doing PyX3DJSAIL instead of x3djsonld.py

--- src/main/shell/python0.sh
+++ src/main/shell/python0.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -euo pipefail
+# set -euo pipefail
 IFS=$'\n\t'
 
 # Run the Test Suite
@@ -14,9 +14,12 @@ export PROCESSORS=${PROCESSORS-8}
 DATATOPYTHON='s/\/data\//\/python\/net\/coderextreme\/data\//' 
 ROOTTOPYTHON='s/\/x3d_code\/www.web3d.org\//\/python\/net\/coderextreme\/x3d_code\/www_web3d_org\//' 
 
+echo "running python"
 for i in `ls -d "$@" | grep -v intermediate | grep -v "\.new" | sed -e 's/\.x3d$/.json/' -e 's/-/_/g' -e 's/^\/c/../'`
 do
 	PY=`echo $i | sed -e $DATATOPYTHON -e $ROOTTOPYTHON -e 's/.json$/.py/'`
-	echo py ../python/x3djsonld.py $i $PY
-	py ../python/x3djsonld.py $i > $PY && py $PY > /dev/null && echo "$PY" || echo "Error: $PY failed to parse"
+	#echo python ../python/x3djsonld.py $i $PY
+	#python ../python/x3djsonld.py $i > $PY && python $PY > /dev/null && echo "$PY" || echo "Error: $PY failed to parse"
+	echo python "$PY"
+	python "$PY"
 done
