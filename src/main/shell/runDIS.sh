35d487358b7ef74c9bb77493dbf76f04646f7570
John Carlson <yottzumm@gmail.com>
2024-03-18 09:10:58 -0500

    Batch run a DIS demo

---
+++ src/main/shell/runDIS.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+"C:/Program Files (x86)/freeWRL/freeWRL.6.6/freeWRL.exe" "C:/Users/john/X3DJSONLD/src/main/data/bumpyfreewrlsliders.x3d" --DIS  --DISaddress 224.0.0.0 --DISport 3000 --DISsite 1 --DISapp 1 &
+"C:/Program Files (x86)/freeWRL/freeWRL.6.6/freeWRL.exe" "C:/Users/john/X3DJSONLD/src/main/data/bumpyfreewrlsliders.x3d" --DIS  --DISaddress 224.0.0.0 --DISport 3000 --DISsite 1 --DISapp 2 &
+"C:/Program Files (x86)/freeWRL/freeWRL.6.6/freeWRL.exe" "C:/Users/john/X3DJSONLD/src/main/data/bumpyfreewrlsliders.x3d" --DIS  --DISaddress 224.0.0.0 --DISport 3000 --DISsite 1 --DISapp 3 &
+"C:/Program Files (x86)/freeWRL/freeWRL.6.6/freeWRL.exe" "C:/Users/john/X3DJSONLD/src/main/data/bumpyfreewrlsliders.x3d" --DIS  --DISaddress 224.0.0.0 --DISport 3000 --DISsite 1 --DISapp 4 &
