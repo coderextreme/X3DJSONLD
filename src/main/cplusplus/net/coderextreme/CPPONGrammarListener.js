1646650fdef45bc2ae8ea408bea07858067cc9da
John Carlson <yottzumm@gmail.com>
2024-04-04 04:19:22 -0500

    Changed throws clause in CPP.java

---
+++ src/main/cplusplus/net/coderextreme/CPPONGrammarListener.js
@@ -0,0 +1,198 @@
+// Generated from CPPONGrammar.g4 by ANTLR 4.10.1
+// jshint ignore: start
+import antlr4 from 'antlr4';
+
+// This class defines a complete listener for a parse tree produced by CPPONGrammarParser.
+export default class CPPONGrammarListener extends antlr4.tree.ParseTreeListener {
+
+	// Enter a parse tree produced by CPPONGrammarParser#type.
+	enterType(ctx) {
+	}
+
+	// Exit a parse tree produced by CPPONGrammarParser#type.
+	exitType(ctx) {
+	}
+
+
+	// Enter a parse tree produced by CPPONGrammarParser#field.
+	enterField(ctx) {
+	}
+
+	// Exit a parse tree produced by CPPONGrammarParser#field.
+	exitField(ctx) {
+	}
+
+
+	// Enter a parse tree produced by CPPONGrammarParser#variable.
+	enterVariable(ctx) {
+	}
+
+	// Exit a parse tree produced by CPPONGrammarParser#variable.
+	exitVariable(ctx) {
+	}
+
+
+	// Enter a parse tree produced by CPPONGrammarParser#string.
+	enterString(ctx) {
+	}
+
+	// Exit a parse tree produced by CPPONGrammarParser#string.
+	exitString(ctx) {
+	}
+
+
+	// Enter a parse tree produced by CPPONGrammarParser#cstring.
+	enterCstring(ctx) {
+	}
+
+	// Exit a parse tree produced by CPPONGrammarParser#cstring.
+	exitCstring(ctx) {
+	}
+
+
+	// Enter a parse tree produced by CPPONGrammarParser#boolean.
+	enterBoolean(ctx) {
+	}
+
+	// Exit a parse tree produced by CPPONGrammarParser#boolean.
+	exitBoolean(ctx) {
+	}
+
+
+	// Enter a parse tree produced by CPPONGrammarParser#boolean_list.
+	enterBoolean_list(ctx) {
+	}
+
+	// Exit a parse tree produced by CPPONGrammarParser#boolean_list.
+	exitBoolean_list(ctx) {
+	}
+
+
+	// Enter a parse tree produced by CPPONGrammarParser#integer_list.
+	enterInteger_list(ctx) {
+	}
+
+	// Exit a parse tree produced by CPPONGrammarParser#integer_list.
+	exitInteger_list(ctx) {
+	}
+
+
+	// Enter a parse tree produced by CPPONGrammarParser#float_list.
+	enterFloat_list(ctx) {
+	}
+
+	// Exit a parse tree produced by CPPONGrammarParser#float_list.
+	exitFloat_list(ctx) {
+	}
+
+
+	// Enter a parse tree produced by CPPONGrammarParser#string_list.
+	enterString_list(ctx) {
+	}
+
+	// Exit a parse tree produced by CPPONGrammarParser#string_list.
+	exitString_list(ctx) {
+	}
+
+
+	// Enter a parse tree produced by CPPONGrammarParser#list.
+	enterList(ctx) {
+	}
+
+	// Exit a parse tree produced by CPPONGrammarParser#list.
+	exitList(ctx) {
+	}
+
+
+	// Enter a parse tree produced by CPPONGrammarParser#construct_array.
+	enterConstruct_array(ctx) {
+	}
+
+	// Exit a parse tree produced by CPPONGrammarParser#construct_array.
+	exitConstruct_array(ctx) {
+	}
+
+
+	// Enter a parse tree produced by CPPONGrammarParser#parameters.
+	enterParameters(ctx) {
+	}
+
+	// Exit a parse tree produced by CPPONGrammarParser#parameters.
+	exitParameters(ctx) {
+	}
+
+
+	// Enter a parse tree produced by CPPONGrammarParser#operator.
+	enterOperator(ctx) {
+	}
+
+	// Exit a parse tree produced by CPPONGrammarParser#operator.
+	exitOperator(ctx) {
+	}
+
+
+	// Enter a parse tree produced by CPPONGrammarParser#funccall.
+	enterFunccall(ctx) {
+	}
+
+	// Exit a parse tree produced by CPPONGrammarParser#funccall.
+	exitFunccall(ctx) {
+	}
+
+
+	// Enter a parse tree produced by CPPONGrammarParser#constructor.
+	enterConstructor(ctx) {
+	}
+
+	// Exit a parse tree produced by CPPONGrammarParser#constructor.
+	exitConstructor(ctx) {
+	}
+
+
+	// Enter a parse tree produced by CPPONGrammarParser#set_field.
+	enterSet_field(ctx) {
+	}
+
+	// Exit a parse tree produced by CPPONGrammarParser#set_field.
+	exitSet_field(ctx) {
+	}
+
+
+	// Enter a parse tree produced by CPPONGrammarParser#add_field.
+	enterAdd_field(ctx) {
+	}
+
+	// Exit a parse tree produced by CPPONGrammarParser#add_field.
+	exitAdd_field(ctx) {
+	}
+
+
+	// Enter a parse tree produced by CPPONGrammarParser#line.
+	enterLine(ctx) {
+	}
+
+	// Exit a parse tree produced by CPPONGrammarParser#line.
+	exitLine(ctx) {
+	}
+
+
+	// Enter a parse tree produced by CPPONGrammarParser#lines.
+	enterLines(ctx) {
+	}
+
+	// Exit a parse tree produced by CPPONGrammarParser#lines.
+	exitLines(ctx) {
+	}
+
+
+	// Enter a parse tree produced by CPPONGrammarParser#x3d.
+	enterX3d(ctx) {
+	}
+
+	// Exit a parse tree produced by CPPONGrammarParser#x3d.
+	exitX3d(ctx) {
+	}
+
+
+
+}
---
+++ src/main/cplusplus/net/coderextreme/CPPONGrammarVisitor.js
@@ -0,0 +1,136 @@
+// Generated from CPPONGrammar.g4 by ANTLR 4.10.1
+// jshint ignore: start
+import antlr4 from 'antlr4';
+
+// This class defines a complete generic visitor for a parse tree produced by CPPONGrammarParser.
+
+export default class CPPONGrammarVisitor extends antlr4.tree.ParseTreeVisitor {
+
+	// Visit a parse tree produced by CPPONGrammarParser#type.
+	visitType(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by CPPONGrammarParser#field.
+	visitField(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by CPPONGrammarParser#variable.
+	visitVariable(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by CPPONGrammarParser#string.
+	visitString(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by CPPONGrammarParser#cstring.
+	visitCstring(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by CPPONGrammarParser#boolean.
+	visitBoolean(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by CPPONGrammarParser#boolean_list.
+	visitBoolean_list(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by CPPONGrammarParser#integer_list.
+	visitInteger_list(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by CPPONGrammarParser#float_list.
+	visitFloat_list(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by CPPONGrammarParser#string_list.
+	visitString_list(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by CPPONGrammarParser#list.
+	visitList(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by CPPONGrammarParser#construct_array.
+	visitConstruct_array(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by CPPONGrammarParser#parameters.
+	visitParameters(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by CPPONGrammarParser#operator.
+	visitOperator(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by CPPONGrammarParser#funccall.
+	visitFunccall(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by CPPONGrammarParser#constructor.
+	visitConstructor(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by CPPONGrammarParser#set_field.
+	visitSet_field(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by CPPONGrammarParser#add_field.
+	visitAdd_field(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by CPPONGrammarParser#line.
+	visitLine(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by CPPONGrammarParser#lines.
+	visitLines(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by CPPONGrammarParser#x3d.
+	visitX3d(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+
+}
