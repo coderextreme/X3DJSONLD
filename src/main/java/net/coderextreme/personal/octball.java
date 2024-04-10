21bb305a4e36af9bd58aaa30fe90b466d55b0476
John Carlson <yottzumm@gmail.com>
2024-04-10 11:50:51 -0500

    More imports for Blender

--- src/main/java/net/coderextreme/personal/octball.java
+++ src/main/java/net/coderextreme/personal/octball.java
@@ -43,6 +43,8 @@ public class octball implements X3DRoots {
   public static void main(String[] args) {
     ConfigurationProperties.setXsltEngine(ConfigurationProperties.XSLT_ENGINE_NATIVE_JAVA);
     ConfigurationProperties.setDeleteIntermediateFiles(false);
+    ConfigurationProperties.setStripTrailingZeroes(true);
+    ConfigurationProperties.setStripDefaultAttributes(true);
     X3D model = new octball().getRootNodeList().get(0); // only get one root node
     System.out.print(model.validationReport().trim());
     model.toFileX3D("../personal/octball.new.java.x3d");
