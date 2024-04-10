e93037c8c2e18512d084104a2d5a0a2d438a9c43
John Carlson <yottzumm@gmail.com>
2024-04-10 11:23:57 -0500

    Updated CLASSPATH

--- src/main/shell/tsc.sh
+++ src/main/shell/tsc.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -cp "target/X3DJSONLD-1.0-SNAPSHOT.jar;../pythonSAI/X3DJSAIL.4.0.full.jar;saxon-he-12.1.jar" net.x3djsonld.data.TextSpecialCharacters json tsc.json
+java -cp "target/X3DJSONLD-1.0-SNAPSHOT.jar;../pythonSAI/X3DJSAIL.4.0.full.jar;saxon-he-12.4.jar" net.x3djsonld.data.TextSpecialCharacters json tsc.json
