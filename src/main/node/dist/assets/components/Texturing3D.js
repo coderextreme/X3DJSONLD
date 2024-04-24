b15f154c47e6be6e046d8905f2b4bc340b7157e0
John Carlson <yottzumm@gmail.com>
2024-04-24 01:38:00 -0500

    revamped places in instance

--- src/main/node/dist/assets/components/Texturing3D.js
+++ src/main/node/dist/assets/components/Texturing3D.js
@@ -1,7 +1,7 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
-/***/ 475:
+/***/ 696:
 /***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
 var __dirname = "/";
@@ -38,7 +38,7 @@ var Ib=[cx,_q,cr,Yr,as,fs,hs,Hu,Su,cx,cx,cx,cx,cx,cx,cx];var Jb=[dx,si,gi,Wh,Kh,
 
 /***/ }),
 
-/***/ 365:
+/***/ 514:
 /***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
 var __dirname = "/";
@@ -72,7 +72,7 @@ var _a=[yj,od,ef,yj];var $a=[zj,Li,di,bi,Kb,Lb,Mb,Nb,Rc,Sc,Uc,jd,xd,Ye,lf,yd,zd,
 
 /***/ }),
 
-/***/ 105:
+/***/ 810:
 /***/ (function(module, __unused_webpack_exports, __webpack_require__) {
 
 /*! dicom-parser - 1.8.12 - 2023-02-20 | (c) 2017 Chris Hafey | https://github.com/cornerstonejs/dicomParser */
@@ -4028,7 +4028,7 @@ module.exports = __WEBPACK_EXTERNAL_MODULE_zlib__;
 
 /***/ }),
 
-/***/ 93:
+/***/ 842:
 /***/ ((module) => {
 
 /* -*- tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- /
@@ -7334,10 +7334,10 @@ var Decoder = class _Decoder {
 
 //# sourceMappingURL=lossless.js.map
 ;// CONCATENATED MODULE: ./src/x_ite/Browser/Texturing3D/DICOMParser.js
-/* provided dependency */ var dicomParser = __webpack_require__(105);
-/* provided dependency */ var JpegImage = __webpack_require__(93);
-/* provided dependency */ var CharLS = __webpack_require__(475);
-/* provided dependency */ var OpenJPEG = __webpack_require__(365);
+/* provided dependency */ var dicomParser = __webpack_require__(810);
+/* provided dependency */ var JpegImage = __webpack_require__(842);
+/* provided dependency */ var CharLS = __webpack_require__(696);
+/* provided dependency */ var OpenJPEG = __webpack_require__(514);
 /*******************************************************************************
  *
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
