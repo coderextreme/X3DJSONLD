a40fce09e827b382a845235cae2f3ecd27f925cc
John Carlson <yottzumm@gmail.com>
2024-05-09 04:07:06 -0500

    Modified jin import

--- blend/importJin.py
+++ blend/importJin.py
@@ -3,7 +3,8 @@ import bpy
 objs = bpy.data.objects
 objs.remove(objs["Cube"], do_unlink=True)
 
-bpy.ops.import_scene.x3d(filepath="JinScaledV2L1LOA4Sites09k.x3d", axis_forward='Y', axis_up='Z')
+bpy.ops.import_scene.x3d(filepath="JinSpastic09x.x3d", axis_forward='Y', axis_up='Z')
+
 # bpy.ops.import_scene.x3d(filepath="JinLOA4JohnCamera.x3d", axis_forward='Y', axis_up='Z')
 #bpy.ops.export_scene.x3dv(filepath="JinBlender.x3d", export_format="X3D")
 # bpy.ops.export_scene.x3dv(filepath="JinBlender.x3d", export_round_precision=20, export_yup=True, export_normals=True, export_format="X3D")
