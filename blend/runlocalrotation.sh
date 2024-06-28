39b01fcc7b873e16c48f58252bdd858c4a758059
John Carlson <yottzumm@gmail.com>
2024-06-28 03:20:06 -0500

    Added rotate script

---
+++ blend/runlocalrotation.sh
@@ -0,0 +1,2 @@
+#/c/Program\ Files/Blender\ Foundation/Blender\ 4.1/blender.exe --background --python localrotation.py
+/c/Program\ Files/Blender\ Foundation/Blender\ 4.1/blender.exe --python localrotation.py
