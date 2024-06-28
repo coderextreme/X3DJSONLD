047aa10f31d1ba0f01b697a6f2beeb9ddff284fd
John Carlson <yottzumm@gmail.com>
2024-06-28 03:23:40 -0500

    Checkid in Jin and VRM driver

--- blend/runimportJin.sh
+++ blend/runimportJin.sh
@@ -1,2 +1,2 @@
-/c/Program\ Files/Blender\ Foundation/Blender\ 4.1/blender.exe --background --python importJin.py
-#/c/Program\ Files/Blender\ Foundation/Blender\ 4.1/blender.exe --python importJin.py
+#/c/Program\ Files/Blender\ Foundation/Blender\ 4.1/blender.exe --background --python importJin.py
+/c/Program\ Files/Blender\ Foundation/Blender\ 4.1/blender.exe --python importJin.py
