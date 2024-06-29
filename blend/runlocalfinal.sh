27a7ea15b08f01593e4cd6c6dfd5d3b2349a3f01
John Carlson <yottzumm@gmail.com>
2024-06-29 06:38:01 -0500

    Created final script (actually not final)

---
+++ blend/runlocalfinal.sh
@@ -0,0 +1,2 @@
+#/c/Program\ Files/Blender\ Foundation/Blender\ 4.1/blender.exe --background --python localfinal.py
+/c/Program\ Files/Blender\ Foundation/Blender\ 4.1/blender.exe --python localfinal.py
