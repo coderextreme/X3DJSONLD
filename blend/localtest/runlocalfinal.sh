d0c40ff5044459c8edda150a5a15386908c681e8
John Carlson <yottzumm@gmail.com>
2024-07-01 11:14:01 -0500

    Made easier to run

--- blend/localtest/runlocalfinal.sh
+++
@@ -1,2 +0,0 @@
-#/c/Program\ Files/Blender\ Foundation/Blender\ 4.1/blender.exe --background --python localfinal.py
-/c/Program\ Files/Blender\ Foundation/Blender\ 4.1/blender.exe --python localfinal.py
