68778625dd5a949f4c7f2966cc7eeace189bdbb1
John Carlson <yottzumm@gmail.com>
2024-07-05 13:18:18 -0500

    Okay, the Humanoid4 might be something we can get right

--- blend/localtest/runlocalfinal.bat
+++ blend/localtest/runlocalfinal.bat
@@ -1,4 +1,6 @@
 PATH=%HOMEDRIVE%\%HOMEPATH%\Downloads\castle-model-viewer-5.0.0-win64-x86_64\castle-model-viewer;"C:\Program Files\Blender Foundation\Blender 4.1";%PATH%
+castle-model-viewer.exe Humanoid4.x3d
+blender.exe --python localfinal.py
 castle-model-viewer.exe localcenters.x3d
 blender.exe --python localcenters.py
 castle-model-viewer.exe localcentersjoe.x3d
