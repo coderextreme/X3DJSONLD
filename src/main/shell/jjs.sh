acf92ddde39e04a7238cf6a5da4b525571346ac5
John W Carlson <yottzumm@gmail.com>
2024-10-09 17:36:04 -0500

    New versions of DIS scripts and jjs.sh

--- src/main/shell/jjs.sh
+++ src/main/shell/jjs.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/bash
 . ../shell/classpath
-C:/graaljs-community-jvm-23.1.2-windows-amd64/graaljs-community-23.1.2-windows-amd64/bin/js.exe --experimental-options --polyglot --vm.Djs.allowAllAccess=true --vm.Xss1g --vm.Xmx4g --jvm --vm.classpath="${GRAAL_CLASSPATH}" "$@"
+export GRAAL_CLASSPATH=C:/Users/jcarl/pythonSAI/X3DJSAIL.4.0.full.jar
+"C:\Users\jcarl\Downloads\graaljs-community-jvm-24.0.2-windows-amd64\graaljs-community-24.0.2-windows-amd64\bin\js.exe" --experimental-options --polyglot --vm.Djs.allowAllAccess=true --vm.Xss1g --vm.Xmx4g --jvm --vm.classpath="${GRAAL_CLASSPATH}" "$@"
+#C:/graaljs-community-jvm-23.1.2-windows-amd64/graaljs-community-23.1.2-windows-amd64/bin/js.exe --experimental-options --polyglot --vm.Djs.allowAllAccess=true --vm.Xss1g --vm.Xmx4g --jvm --vm.classpath="${GRAAL_CLASSPATH}" "$@"
