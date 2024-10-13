c3761d619dbf462110f329fe16573c1642ce8b8e
John W Carlson <yottzumm@gmail.com>
2024-10-13 14:28:02 -0500

    Check in a lot of working clojure code?

---
+++ src/main/clojure/net/coderextreme/data/trash/clloader.clj
@@ -0,0 +1,17 @@
+(import java.io.File)
+(import java.net.URL)
+(import java.net.URLClassLoader)
+
+;; Path to your JAR file
+(def jar-path "../../../../../../../pythonSAI/X3DJSAIL.4.0.full.jar")
+
+;; Create a File object for the JAR
+(def jar-file (File. jar-path))
+
+;; Create a URL object for the JAR
+(def jar-url (URL. (.toString (.toURI jar-file)))) 
+
+;; Load the JAR into the classpath
+(let [class-loader (URLClassLoader. (into-array URL [jar-url]) (ClassLoader/getSystemClassLoader))]
+  (let [current-thread (Thread/currentThread)]
+    (.setContextClassLoader current-thread class-loader)))
