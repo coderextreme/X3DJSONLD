cd8d84c4f9e22eba524e7ee66c39dc7bcfe5d0b8
John W Carlson <yottzumm@gmail.com>
2024-10-15 22:15:48 -0500

    Hopefully, this classpath works

--- blend/importJoe.py
+++ blend/importJoe.py
@@ -44,8 +44,8 @@ bpy.data.objects.remove(light, do_unlink=True)
 #############################################################
 
 
-bpy.ops.import_scene.x3d(filepath="JoeSkinTexcoordDisplacerKickUpdate2NoROUTE.x3d", axis_forward='Y', axis_up='Z')
-#bpy.ops.import_scene.x3d(filepath="JoeSkinTexcoordDisplacerKickUpdate2.x3d")
+#bpy.ops.import_scene.x3d(filepath="JoeSkinTexcoordDisplacerKickUpdate2NoROUTE.x3d", axis_forward='Y', axis_up='Z')
+bpy.ops.import_scene.x3d(filepath="JoeSkinTexcoordDisplacerKickUpdate2.x3d", axis_forward='Y', axis_up='Z')
 #bpy.ops.import_scene.x3d(filepath="JoeKick.x3dv", axis_forward='Y', axis_up='Z')
 #bpy.ops.export_scene.x3dv(filepath="JoeBlender.x3d", export_round_precision=6, export_yup=True, export_normals=True, export_format="X3D")
 #bpy.ops.export_scene.x3dv(filepath="JoeSkinTexcoordDisplacerKickUpdate2Export.x3d", export_hanim_prefix='Joe_',export_round_precision=6, export_yup=True, export_normals=True, export_format="X3D")
@@ -58,6 +58,10 @@ bpy.ops.export_scene.gltf(
     export_yup=False,
     # export_format="GLTF_SEPARATE",
     export_format="GLB",
+    export_nla_strips_merged_animation_name='Animation',
+    export_animation_mode="ACTIVE_ACTIONS", #"SCENE", 
+    export_influence_nb=40,
+    export_all_influences=True,
     use_active_collection=True
 )
 
