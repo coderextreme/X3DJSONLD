82f37cd702e838c4f58f866d0b5fb37e4662e941
John W Carlson <yottzumm@gmail.com>
2024-11-05 19:01:32 -0600

    These somewhat work

--- blend/runlocalfinal.sh
+++ blend/runlocalfinal.sh
@@ -1,2 +1,2 @@
-#/c/Program\ Files/Blender\ Foundation/Blender\ 4.2/blender.exe --background --python localfinal.py
+# /c/Program\ Files/Blender\ Foundation/Blender\ 4.2/blender.exe -b --python localfinal.py
 /c/Program\ Files/Blender\ Foundation/Blender\ 4.2/blender.exe --python localfinal.py
