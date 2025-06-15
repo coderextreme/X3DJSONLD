322f3cefa395903f369dc47ca6951be58131ed4a
John W Carlson <yottzumm@gmail.com>
2025-06-15 07:36:27 -0500

    Might need help around Shape...

--- src/main/node/GuraSerializer.js
+++ src/main/node/GuraSerializer.js
@@ -3,32 +3,21 @@
 const DOUBLE_SUFFIX = '';
 const FLOAT_SUFFIX = '';
 
-var fs = require("fs");
+let fs = require("fs");
 
 function GuraSerializer () {
-this.code = [];
-this.codeno = 0;
-this.preno = 0;
 }
 
 
 GuraSerializer.prototype = {
 	serializeToString : function(json, element, clazz, mapToMethod, fieldTypes) {
-		this.code = [];
-		this.codeno = 0;
-		this.preno = 0;
-		var stack = [];
-
-		var str = "";
-		stack.unshift(this.preno);
-		this.preno++;
+		let str = "";
 		str += element.nodeName+':\n';
-		str += this.subSerializeToString(element, mapToMethod, fieldTypes, 1, stack);
-		stack.shift();
+		str += this.subSerializeToString(element, mapToMethod, fieldTypes, 1);
 		return str;
 	},
 
-	printSubArray : function (attrType, type, values, co, j, lead, trail) {
+	printSubArray : function (attrType, type, values, j, lead, trail) {
                 if (type === "int") {
                         for (var v in values) {
 				if (values[v] > 0x7fffffff) {
@@ -44,7 +33,7 @@ GuraSerializer.prototype = {
                 }
 		/*
                 if (type === "boolean") {
-                        for (var v in values) {
+                        for (let v in values) {
 				if (values[v] === 'true') {
 					values[v] = "True";
 				} else if (values[v] === 'false') {
@@ -63,36 +52,40 @@ GuraSerializer.prototype = {
 		return '['+lead+values.join(j)+trail+']';
 	},
 
-	printParentChild : function (element, node, cn, mapToMethod, n) {
-		var prepre = "";
-		var addpre = "";
+	printParentChild : function (element, node, cn, mapToMethod) {
+		let prepre = ".";
+		let addpre = "set";
 		if (cn > 0 && node.nodeName !== 'IS') {
-			addpre = "";
+			addpre = "add";
 		}
 		if (node.nodeName === 'field') {
-			addpre = "";
+			addpre = "add";
 		}
 
-		var method = node.nodeName;
+		let method = node.nodeName;
 		if (typeof mapToMethod[element.nodeName] === 'object') {
 			if (typeof mapToMethod[element.nodeName][node.nodeName] === 'string') {
 				addpre = "";
-				method = mapToMethod[element.nodeName][node.nodeName].charAt(3).toLowerCase() + mapToMethod[element.nodeName][node.nodeName].slice(4);
+				method = mapToMethod[element.nodeName][node.nodeName];
+			} else {
+				method = method.charAt(0).toUpperCase() + method.slice(1);
 			}
 		} else if (typeof mapToMethod[element.nodeName] === 'string') {
 			addpre = "";
-			method = mapToMethod[element.nodeName].charAt(3).toLowerCase() + mapToMethod[element.nodeName].slice(4);
+			method = mapToMethod[element.nodeName];
+		} else {
+			method = method.charAt(0).toUpperCase() + method.slice(1);
 		}
-		for (var a in node.attributes) {
-			var attrs = node.attributes;
+		for (let a in node.attributes) {
+			let attrs = node.attributes;
 			try {
 				parseInt(a);
-				var attrsa = attrs[a];
-				var attr = attrsa.nodeName;
+				let attrsa = attrs[a];
+				let attr = attrsa.nodeName;
 				if (attrs.hasOwnProperty(a) && attrsa.nodeType == 2) {
 					if (attr === "containerField") {
 						if (method === "setShaders") {
-							method = "Shaders"
+							method = "addShaders"
 							addpre = "";
 						} else {
 							if (attrs[a].nodeValue === "joints" 
@@ -102,10 +95,10 @@ GuraSerializer.prototype = {
 								|| attrs[a].nodeValue === "skeleton" 
 								|| attrs[a].nodeValue === "segments" 
 							) {
-								method = attrs[a].nodeValue;
+								method = "add"+attrs[a].nodeValue.charAt(0).toUpperCase() + attrs[a].nodeValue.slice(1);
 							} else if (method === "addValue") {
 							} else {
-								method = attrs[a].nodeValue;
+								method = "set"+attrs[a].nodeValue.charAt(0).toUpperCase() + attrs[a].nodeValue.slice(1);
 							}
 							addpre = "";
 						}
@@ -117,29 +110,29 @@ GuraSerializer.prototype = {
 		}
 		if (node.nodeName === "IS") {
 			method = "IS";
-			addpre = "";
+			addpre = "set";
 		}
 		if (addpre+method === "setJoints") {
 			method = "Joints"
-			addpre = "";
+			addpre = "add";
 		}
 		if (addpre+method === "setSegments") {
 			method = "Segments"
-			addpre = "";
+			addpre = "add";
 		}
 		if (element.nodeName === 'Scene' && addpre+method === "setMetadata") {
 			method = "Metadata"
-			addpre = "";
+			addpre = "add";
 		}
 		if (node.nodeName === 'LayerSet' && addpre+method === "addChild") {
 			method = "LayerSet"
-			addpre = "";
+			addpre = "add";
 		}
 		return prepre+addpre+method;
 	},
 	stringValue : function(attrsa, attr, attrType, element) {
-		var strval;
-		var nodeValue = attrsa.nodeValue;
+		let strval;
+		let nodeValue = attrsa.nodeValue;
 		if (nodeValue === 'NULL') {
 			strval = "";
 		} else if (attrType === "SFString") {
@@ -187,13 +180,13 @@ GuraSerializer.prototype = {
 			}
 			strval = nodeValue+DOUBLE_SUFFIX;
 		} else if (attrType === "MFTime") {
-			strval = this.printSubArray(attrType, "double", nodeValue.split(/[ ,\t\r\n]+/), this.codeno, DOUBLE_SUFFIX+', ', '', DOUBLE_SUFFIX);
+			strval = this.printSubArray(attrType, "double", nodeValue.split(/[ ,\t\r\n]+/), DOUBLE_SUFFIX+', ', '', DOUBLE_SUFFIX);
 		} else if (attrType === "MFString") {
 			nodeValue = nodeValue.replace(/^ *(.*) *$/, "$1");
 			strval = this.printSubArray(attrType, "into",
 				nodeValue.substr(1, nodeValue.length-2).split(/"[ ,\t\r\n]+"/).
 				map(function(x) {
-					var y = x.
+					let y = x.
 					       replace(/(\\\\+)/g, '$1$1').
 					       replace(/\\\\"/g, '\\\"').
 					       replace(/""/g, '\\"\\"').
@@ -204,12 +197,12 @@ GuraSerializer.prototype = {
 						// console.error("Gura Replacing "+x+" with "+y);
 					}
 					return y;
-				}), this.codeno, '", "', '"', '"');
+				}), '", "', '"', '"');
 		} else if (
 			attrType === "MFInt32"||
 			attrType === "MFImage"||
 			attrType === "SFImage") {
-			strval = this.printSubArray(attrType, "int", nodeValue.split(/[ ,\t\r\n]+/), this.codeno, ', ', '', '');
+			strval = this.printSubArray(attrType, "int", nodeValue.split(/[ ,\t\r\n]+/), ', ', '', '');
 		} else if (
 			attrType === "SFColor"||
 			attrType === "MFColor"||
@@ -228,7 +221,7 @@ GuraSerializer.prototype = {
 			attrType === "SFRotation"||
 			attrType === "MFRotation"||
 			attrType === "MFFloat") {
-			strval = this.printSubArray(attrType, "float", nodeValue.split(/[ ,\t\r\n]+/), this.codeno, FLOAT_SUFFIX+', ', '', FLOAT_SUFFIX);
+			strval = this.printSubArray(attrType, "float", nodeValue.split(/[ ,\t\r\n]+/), FLOAT_SUFFIX+', ', '', FLOAT_SUFFIX);
 		} else if (
 			attrType === "SFVec2d"||
 			attrType === "SFVec3d"||
@@ -241,25 +234,25 @@ GuraSerializer.prototype = {
 			attrType === "MFMatrix3d"||
 			attrType === "MFMatrix4d"||
 			attrType === "MFDouble") {
-			strval = this.printSubArray(attrType, "double", nodeValue.split(/[ ,\t\r\n]+/), this.codeno, DOUBLE_SUFFIX+', ', '', DOUBLE_SUFFIX);
+			strval = this.printSubArray(attrType, "double", nodeValue.split(/[ ,\t\r\n]+/), DOUBLE_SUFFIX+', ', '', DOUBLE_SUFFIX);
 		} else if (attrType === "MFBool") {
-			strval = this.printSubArray(attrType, "boolean", nodeValue.split(/[ ,\t\r\n]+/), this.codeno, ', ', '', '');
+			strval = this.printSubArray(attrType, "boolean", nodeValue.split(/[ ,\t\r\n]+/), ', ', '', '');
 		} else {
 			strval = '"'+nodeValue.replace(/\n/g, '\\\\n').replace(/\\?"/g, "\\\"")+'"';
 		}
 		return strval;
 	},
-	subSerializeToString : function(element, mapToMethod, fieldTypes, n, stack) {
-		var str = "";
-		var attrType = "";
-		var nodename = "";
-		for (var a in element.attributes) {
-			var attrs = element.attributes;
+	subSerializeToString : function(element, mapToMethod, fieldTypes, n) {
+		let str = "";
+		let attrType = "";
+		let nodename = "";
+		for (let a in element.attributes) {
+			let attrs = element.attributes;
 			try {
 				parseInt(a);
-				var attrsa = attrs[a];
+				let attrsa = attrs[a];
 				if (attrs.hasOwnProperty(a) && attrsa.nodeType == 2) {
-					var attr = attrsa.nodeName;
+					let attr = attrsa.nodeName;
 					if (attr === 'containerField' && (
 						attrs[a].nodeValue === "joints" ||
 						attrs[a].nodeValue === "skeleton" ||
@@ -268,7 +261,6 @@ GuraSerializer.prototype = {
 						attrs[a].nodeValue === "skin" ||
 						attrs[a].nodeValue === "skinCoord" ||
 						attrs[a].nodeValue === "sites")) {
-						// console.log("################## FOUND", attr, attrs[a].nodeValue);
 						attr = "containerFieldOverride";
 					} else if (attr === "xmlns:xsd") {
 						continue;
@@ -302,8 +294,8 @@ GuraSerializer.prototype = {
 					if (typeof fieldTypes[element.nodeName] !== 'undefined') {
 						attrType = fieldTypes[element.nodeName][attr];
 					}
-					var strval = this.stringValue(attrsa, attr, attrType, element);
-					var method = attr;
+					let strval = this.stringValue(attrsa, attr, attrType, element);
+					let method = attr;
 					if (attr === "class") {
 						method = "CssClass";
 					}
@@ -317,44 +309,70 @@ GuraSerializer.prototype = {
 			}
 			attrType = "";
 		}
-		for (var cn in element.childNodes) {
-			var node = element.childNodes[cn];
+		let childrenNodes = [];
+		let first = true;
+		for (let cn in element.childNodes) {
+			let node = element.childNodes[cn];
+			let ch = "";
+			if (first) {
+				first = false;
+			} else {
+				if (element.nodeName === 'Shape'
+				    || element.nodeName === 'X3D') {
+					ch += "    ".repeat(n);
+				}
+				ch +=  ",\n";
+			}
 			if (element.childNodes.hasOwnProperty(cn) && node.nodeType == 1) {
-				stack.unshift(this.preno);
-				this.preno++;
-				var ch = "";
-				var pkg = "";
-				method = this.printParentChild(element, node, cn, mapToMethod, n);
-				ch += "    ".repeat(n)+node.nodeName+":\n";
+				let method = this.printParentChild(element, node, cn, mapToMethod);
+				let begin = "";
+				let end = "";
+				if (method.startsWith(".add")) {
+					begin = " [";
+					end = "]";
+				}
+				ch += "    ".repeat(n)+node.nodeName+":"+begin+"\n";
 
-				ch += this.subSerializeToString(node, mapToMethod, fieldTypes, n+1, stack);
-				str += ch;
-				stack.shift();
+				ch += this.subSerializeToString(node, mapToMethod, fieldTypes, n+1);
+				if (end !== "") {
+					ch += "    ".repeat(n)+end;
+				}
+				childrenNodes.push(ch);
 			} else if (element.childNodes.hasOwnProperty(cn) && node.nodeType == 8) {
-				var y = node.nodeValue.
+				let y = node.nodeValue.
 					replace(/\\/g, '\\\\').
 					replace(/"/g, '\\"');
-				// str += ".addComments(CommentsBlock(\"\"\""+y+"\"\"\")) \\\n";
-				str += y.split("\r\n").map(function(x) {
-					return x.replace(/^/g, '#');
-					}).join("\r\n");
-				str += "\r\n";
-				if (y !== node.nodeValue) {
-					// console.error("Java Comment Replacing "+node.nodeValue+" with "+y);
-				}
+				ch += y.split("\r\n").map(function(x) {
+					return x.replace(/^/g, '\n#');
+					}).join("\n")+"\n";
+				// no commas for comments, don't print a comma
+				first = true;
+				childrenNodes.push(ch);
 			} else if (element.childNodes.hasOwnProperty(cn) && node.nodeType == 4) {
-				str += '(.setSourceCode "'+node.nodeValue.split(/\r?\n/).map(function(x) {
+				ch += '\n#sourceCode "'+node.nodeValue.split(/\r?\n/).map(function(x) {
 					return x.
 					        replace(/\\/g, '\\\\').
 						replace(/"/g, '\\"').
 						replace(/$/g, '\\').
 						replace("ecmascript:", "");
-						/*
-						.replace(/\\n/g, "\\\\n")
-						*/
-					}).join('\\newline')+'\\newline")';
+					}).join('\\newline')+'\\newline"\n';
+				if (cn === 0) {
+					// if comment is first, don't print a comma
+					first = true;
+				}
+				childrenNodes.push(ch);
+			} else if (element.childNodes.hasOwnProperty(cn) && node.nodeType == 3 && node.nodeValue.trim() === '') {
+			} else if (typeof node.nodeType === 'undefined') {
+			} else {
+				console.error("Unhandled", node.nodeType, node.nodeName, node.nodeValue);
 			}
-	        		
+		}
+		let newcn = childrenNodes.join("");
+		if (newcn !== "") {
+			str += newcn;
+			if (!str.endsWith("\n")) {
+				str += "\n";
+			}
 		}
 		return str;
 	}
--- src/main/shell/runjson.sh
+++ src/main/shell/runjson.sh
@@ -3,18 +3,10 @@ set -euo pipefail
 IFS=$'\n\t'
 
 
-export X3DJSONLD=../X3DJSONLD
-
-cd ${X3DJSONLD}
-
 . ./classpath
-
-export X3DJSONLD=../X3DJSONLD
 
 export PROCESSORS=8
 
-javac RunSaxon.java
+# javac RunSaxon.java
 
-ls *.x3d |grep -v new | grep -v intermediate | xargs -P 8 java RunSaxon 2> /dev/null 1> /dev/null
-ls *.json| grep -v new | grep -v intermediate | grep -v package | xargs -P $PROCESSORS ${NODE} json2all.js 1> /dev/null
-ls *.x3d | sed 's/\.x3d$/.py/' | xargs ls
+ls ../data/*.json| grep -v new | grep -v intermediate | grep -v package | xargs -P $PROCESSORS ${NODE} ../node/json2all.js 1> /dev/null
