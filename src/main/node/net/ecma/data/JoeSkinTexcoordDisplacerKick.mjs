506fcedad6ac4c208afcaf136175f3b3751469b6
John W Carlson <yottzumm@gmail.com>
2025-06-23 15:24:51 -0500

    More version updates

--- src/main/node/net/ecma/data/JoeSkinTexcoordDisplacerKick.mjs
+++ src/main/node/net/ecma/data/JoeSkinTexcoordDisplacerKick.mjs
@@ -59,7 +59,7 @@ var ScalarInterpolator = require('./x3d.mjs');
 var X3D0 =  new X3D({
 
       profile : new SFString("Immersive"),
-      version : new SFString("3.3"),
+      version : new SFString("4.0"),
       head : new SFNode(
         new head({
           component : new SFNode(
--- src/main/node/net/ecma/data/LOA1_JumpAnimation.mjs
+++ src/main/node/net/ecma/data/LOA1_JumpAnimation.mjs
@@ -39,7 +39,7 @@ var SFColor = require('./x3d.mjs');
 var X3D0 =  new X3D({
 
       profile : new SFString("Immersive"),
-      version : new SFString("3.3"),
+      version : new SFString("4.0"),
       head : new SFNode(
         new head({
           meta : new MFNode([
