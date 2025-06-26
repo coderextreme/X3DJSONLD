24c19c35a13ccc8604c361bd1a4acc40ef6009db
John W Carlson <yottzumm@gmail.com>
2025-06-26 08:09:28 -0500

    Added examples

--- src/main/data/runDISlocal.sh
+++ src/main/data/runDISlocal.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-"C:/Program Files (x86)/freeWRL/freeWRL.6.6/freeWRL.exe" "../data/bumpyfreewrlsliders.x3d" --DIS  --DISaddress 224.0.0.0 --DISport 3000 --DISsite 1 --DISapp 1 &
-"C:/Program Files (x86)/freeWRL/freeWRL.6.6/freeWRL.exe" "../data/bumpyfreewrlsliders.x3d" --DIS  --DISaddress 224.0.0.0 --DISport 3000 --DISsite 1 --DISapp 2 &
-"C:/Program Files (x86)/freeWRL/freeWRL.6.6/freeWRL.exe" "../data/bumpyfreewrlsliders.x3d" --DIS  --DISaddress 224.0.0.0 --DISport 3000 --DISsite 1 --DISapp 3 &
-"C:/Program Files (x86)/freeWRL/freeWRL.6.6/freeWRL.exe" "../data/bumpyfreewrlsliders.x3d" --DIS  --DISaddress 224.0.0.0 --DISport 3000 --DISsite 1 --DISapp 4 &
+"C:/Program Files (x86)/freeWRL/freeWRL.6.7/freeWRL.exe" "../data/bumpyfreewrlsliders.x3d" --DIS  --DISaddress 224.0.0.0 --DISport 3000 --DISsite 1 --DISapp 1 &
+"C:/Program Files (x86)/freeWRL/freeWRL.6.7/freeWRL.exe" "../data/bumpyfreewrlsliders.x3d" --DIS  --DISaddress 224.0.0.0 --DISport 3000 --DISsite 1 --DISapp 2 &
+"C:/Program Files (x86)/freeWRL/freeWRL.6.7/freeWRL.exe" "../data/bumpyfreewrlsliders.x3d" --DIS  --DISaddress 224.0.0.0 --DISport 3000 --DISsite 1 --DISapp 3 &
+"C:/Program Files (x86)/freeWRL/freeWRL.6.7/freeWRL.exe" "../data/bumpyfreewrlsliders.x3d" --DIS  --DISaddress 224.0.0.0 --DISport 3000 --DISsite 1 --DISapp 4 &
