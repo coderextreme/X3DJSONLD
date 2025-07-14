ffc5801a2f67eda85eca3a0574db50099f6c579e
John W Carlson <yottzumm@gmail.com>
2025-07-14 05:19:46 -0500

    Got x3d python package mostly working

--- src/main/shell/python0.sh
+++ src/main/shell/python0.sh
@@ -15,9 +15,9 @@ DATATOPYTHON='s/\/data\//\/python\/net\/coderextreme\/data\//'
 ROOTTOPYTHON='s/\/x3d_code\/www.web3d.org\//\/python\/net\/coderextreme\/x3d_code\/www_web3d_org\//' 
 
 echo "running python"
-for i in `ls -d "$@" | grep -v intermediate | grep -v "\.new" | sed -e 's/\.x3d$/.json/' -e 's/-/_/g' -e 's/^\/c/../'`
+for i in `ls -d "$@" | grep -v intermediate | grep -v "\.new" | sed -e 's/\.x3d$/\.json/' -e 's/-/_/g' -e 's/^\/c/../'`
 do
-	PY=`echo $i | sed -e $DATATOPYTHON -e $ROOTTOPYTHON -e 's/.json$/.py/'`
+	PY=`echo $i | sed -e $DATATOPYTHON -e $ROOTTOPYTHON -e 's/\.json$/.py/'`
 	#echo ${PYTHON} ../python/x3djsonld.py $i $PY
 	#${PYTHON} ../python/x3djsonld.py $i > $PY && ${PYTHON} $PY > /dev/null && echo "$PY" || echo "Error: $PY failed to parse"
 	echo ${PYTHON} "$PY"
