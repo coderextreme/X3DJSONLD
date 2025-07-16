f76720447ef1cf2adb25f2703e1749e9b0e890af
John W Carlson <yottzumm@gmail.com>
2025-07-16 01:12:38 -0500

    Updates to JSON, etc

--- src/main/clojure/net/coderextreme/data/cl.sh
+++ src/main/clojure/net/coderextreme/data/cl.sh
@@ -1,2 +1,2 @@
-
+#!/usr/bin/bash
 clj -Scp "/usr/local/lib/clojure/libexec/clojure-tools-1.12.0.1479.jar:${HOME}/pythonSAI/X3DJSAIL.4.0.full.jar:${HOME}/.m2/repository/org/clojure/clojure/1.12.0/clojure-1.12.0.jar:${HOME}/.m2/repository/org/clojure/core.specs.alpha/0.4.74/core.specs.alpha-0.4.74.jar:${HOME}/.m2/repository/org/clojure/spec.alpha/0.5.238/spec.alpha-0.5.238.jar" -M abox.clj
