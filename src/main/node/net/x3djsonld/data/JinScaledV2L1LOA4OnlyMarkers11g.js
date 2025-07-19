81ce612d5acf6040f295bc77122478677b62939f
John W Carlson <yottzumm@gmail.com>
2025-07-19 05:17:18 -0500

    Updatesd some files

--- src/main/node/net/x3djsonld/data/JinScaledV2L1LOA4OnlyMarkers11g.js
+++ src/main/node/net/x3djsonld/data/JinScaledV2L1LOA4OnlyMarkers11g.js
@@ -1,12 +1,12 @@
-var java = require('java');
+import java from 'java';
 java.asyncOptions = {
   asyncSuffix: undefined,     // Don't generate node-style methods taking callbacks
   syncSuffix: "",              // Sync methods use the base name(!!)
   promiseSuffix: "Promise",   // Generate methods returning promises, using the suffix Promise.
-  promisify: require('util').promisify, // Needs Node.js version 8 or greater, see comment below
+  promisify: import('util').promisify, // Needs Node.js version 8 or greater, see comment below
   ifReadOnlySuffix: "_alt"
 };
-var autoclass = require('./X3Dautoclass');
+import autoclass from './X3Dautoclass';
 
 // Javadoc annotations follow, see below for source.
 /**
