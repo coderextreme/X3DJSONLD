d73f710566d15c372cc4f8a61745318aef6f2956
John W Carlson <yottzumm@gmail.com>
2025-07-26 22:35:26 -0500

    Checked in new shell scripts and Clojure output

---
+++ src/main/shell/tojson.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# set -euo pipefail
+IFS=$'\n\t'
+
+# Run the Stylesheet
+
+# accepts files with .x3d extension
+export PROCESSORS="${PROCESSORS-8}"
+
+. ./classpath
+
+# ${PYTHON} ../python/classes.py
+pushd ../java
+echo "export CLASSPATH=$CLASSPATH"
+# export CLASSPATH="C:/Users/jcarl/X3DJSONLD/X3DJSAIL.4.0.full.jar;C:/Users/jcarl/X3DJSONLD/saxon-he-12.4.jar;.;../java"
+echo "export CLASSPATH=$CLASSPATH"
+javac -proc:full -cp "${CLASSPATH}" net/coderextreme/RunSaxon.java net/coderextreme/X3DRoots.java
+popd
+# sudo pacman -Syu leiningen
+
+STYLESHEETDIR=../lib/stylesheets
+# OVERWRITE=
+OVERWRITE=---overwrite
+
+echo  "ls -d $@ | grep -v intermediate | grep -v  '\.new'  | tr '\n' '\0'| xargs -0 -P $PROCESSORS java net.coderextreme.RunSaxon --- ${OVERWRITE} --${STYLESHEETDIR}/X3dToJson.xslt -json | sed 's/^\(.*\)\$/\"\1\"/' | xargs -P $PROCESSORS ${NODE} ${NODEDIR}/json2all.js"
+ls -d "$@" | grep -v intermediate | grep -v '\.new' | tr '\n' '\0'| xargs -0 -P "$PROCESSORS" java net.coderextreme.RunSaxon --- "${OVERWRITE}" --"${STYLESHEETDIR}/X3dToJson.xslt" -json | sed 's/^\(.*\)$/"\1"/' | xargs -P "$PROCESSORS" "${NODE}" "${NODEDIR}/json2all.js"
+
