8fad15fead06987fe8335548961fb4566fd8bce9
John W Carlson <yottzumm@gmail.com>
2025-08-09 05:11:54 -0500

    Added schemani.sh

---
+++ schemani.sh
@@ -0,0 +1,28 @@
+#!/gbin/bash
+
+# watch out for too many CommandLines processes wiping out your database, check config-output-dir versus config-merge-dir
+~/Downloads/graalvm-community-jdk-24.0.2_windows-x64_bin/graalvm-community-openjdk-24.0.2+11.1/bin/java -agentlib:native-image-agent=config-output-dir=src/main/resources/META-INF/native-image2 \
+     -cp "src/main/java;C:/Users/jcarl/www.web3d.org/x3d/stylesheets/java/lib/xercesImpl.jar;C:/Users/jcarl/www.web3d.org/x3d/stylesheets/java/lib/xmlresolver-5.2.2-data.jar;C:/Users/jcarl/www.web3d.org/x3d/stylesheets/java/lib/xmlresolver-5.2.2.jar;C:/Users/jcarl/www.web3d.org/x3d/stylesheets/java/lib/saxon-he-12.5.jar;C:/Users/jcarl/www.web3d.org/x3d/tools/X3dQualityAssurance/X3dValidator/lib/slf4j-api-2.0.9.jar;C:/Users/jcarl/www.web3d/org/x3d/tools/X3dQualityAssurance/X3dValidator/lib/slf4j-jcl-1.7.36.jar;C:/Users/jcarl/www.web3d.org/x3d/tools/X3dQualityAssurance/X3dValidator/lib/slf4j-simple-2.0.12.jar;X3DJSAIL.4.0.full.jar" \
+     net.coderextreme.CoderSchematron
+
+# This merges all subsequent commands with the above database
+
+find src/main/data -name '*.x3d' | grep -v new | xargs ~/Downloads/graalvm-community-jdk-24.0.2_windows-x64_bin/graalvm-community-openjdk-24.0.2+11.1/bin/java -agentlib:native-image-agent=config-merge-dir=src/main/resources/META-INF/native-image2 \
+     -cp "src/main/java;C:/Users/jcarl/www.web3d.org/x3d/stylesheets/java/lib/xercesImpl.jar;C:/Users/jcarl/www.web3d.org/x3d/stylesheets/java/lib/xmlresolver-5.2.2-data.jar;C:/Users/jcarl/www.web3d.org/x3d/stylesheets/java/lib/xmlresolver-5.2.2.jar;C:/Users/jcarl/www.web3d.org/x3d/stylesheets/java/lib/saxon-he-12.5.jar;C:/Users/jcarl/www.web3d.org/x3d/tools/X3dQualityAssurance/X3dValidator/lib/slf4j-api-2.0.9.jar;C:/Users/jcarl/www.web3d/org/x3d/tools/X3dQualityAssurance/X3dValidator/lib/slf4j-jcl-1.7.36.jar;C:/Users/jcarl/www.web3d.org/x3d/tools/X3dQualityAssurance/X3dValidator/lib/slf4j-simple-2.0.12.jar;X3DJSAIL.4.0.full.jar" \
+     net.coderextreme.CoderSchematron
+
+
+# now compile CommandLine with that database in META-INF/native-image
+
+~/Downloads/graalvm-community-jdk-24.0.2_windows-x64_bin/graalvm-community-openjdk-24.0.2+11.1/bin/native-image.cmd \
+  --initialize-at-run-time=org.xmlresolver,net.sf.saxon,org.apache.xerces \
+  --initialize-at-run-time=org.xmlresolver.cache.ResourceCache \
+  -H:+ReportExceptionStackTraces \
+  --no-fallback \
+  --enable-url-protocols=https \
+  -march=native \
+  -cp "src/main/java;src/main/resources;C:/Users/jcarl/www.web3d.org/x3d/stylesheets/java/lib/xercesImpl.jar;C:/Users/jcarl/www.web3d.org/x3d/stylesheets/java/lib/xmlresolver-5.2.2-data.jar;C:/Users/jcarl/www.web3d.org/x3d/stylesheets/java/lib/xmlresolver-5.2.2.jar;C:/Users/jcarl/www.web3d.org/x3d/stylesheets/java/lib/saxon-he-12.5.jar;C:/Users/jcarl/www.web3d.org/x3d/tools/X3dQualityAssurance/X3dValidator/lib/slf4j-api-2.0.9.jar;C:/Users/jcarl/www.web3d.org/x3d/tools/X3dQualityAssurance/X3dValidator/lib/slf4j-jcl-1.7.36.jar;C:/Users/jcarl/www.web3d.org/x3d/tools/X3dQualityAssurance/X3dValidator/lib/slf4j-simple-2.0.12.jar;X3DJSAIL.4.0.full.jar" \
+  net.coderextreme.CoderSchematron \
+  -o CoderShematron
+
+# Nwo use CommandLines wherever you would use the `java net.CommandLines`
