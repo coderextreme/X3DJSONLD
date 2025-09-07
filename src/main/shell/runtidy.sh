96ed7e377e6387d51bbbce45b735355ad20c50d1
John W Carlson <yottzumm@gmail.com>
2025-09-07 00:11:24 -0500

    Replaced $1 with $i in loop (oops)

--- src/main/shell/runtidy.sh
+++ src/main/shell/runtidy.sh
@@ -5,8 +5,8 @@ IFS=$'\n\t'
 for i in "$@"
 do
 	export IN=`pwd`/"$i" 
-	export OUT=`pwd`/`dirname "$i"`/`basename "$1" .x3d`.x3dj
-	export FINAL=`pwd`/`dirname "$i"`/`basename "$1" .x3d`.json
+	export OUT=`pwd`/`dirname "$i"`/`basename "$i" .x3d`.x3dj
+	export FINAL=`pwd`/`dirname "$i"`/`basename "$i" .x3d`.json
 	echo "npx --yes x3d-tidy -i $IN -o $OUT" >&2
 	npx x3d-tidy -f -i "$IN" -o "$OUT"
 	cp "$OUT" "$FINAL"
