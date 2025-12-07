d81a57260161757368f3063aecc79bc7ad7598f2
John W Carlson <yottzumm@gmail.com>
2025-12-07 02:11:04 -0600

    Rotations were treated like colors

--- src/main/node/testPlyToJson.js
+++ src/main/node/testPlyToJson.js
@@ -5,7 +5,8 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 import convertPlyToJsonBinary from "./convertPlyToJsonBinary.js";
 
 let filenames = [
-	// "https://vr.csgrid.org/gsplats/soaring_eagle.ply",
+	//"https://vr.csgrid.org/gsplats/soaring_eagle.ply",
+	//"https://ifiweb.informatik.tu-freiberg.de/public/silversplat/.%2Fmodels%2Fwerner_amethyst.ply",
 	"https://vr.csgrid.org/gsplats/Merrifield_100.ply",
 	"https://vr.csgrid.org/gsplats/Merrifield_fireplace.ply",
 	"https://vr.csgrid.org/gsplats/mug_space.ply"
