4127c485a06b0451d9a70e0a1bbd669e77b3d2c2
John W Carlson <yottzumm@gmail.com>
2025-12-07 04:25:54 -0600

    First version of PLY to X3D

--- src/main/node/testPlyToX3d.js
+++ src/main/node/testPlyToX3d.js
@@ -2,11 +2,11 @@ import fs from "node:fs";
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
-import convertPlyToJsonBinary from "./convertPlyToJsonBinary.js";
+import convertPlyToX3d from "./convertPlyToX3d.js";
 
 let filenames = [
-	//"https://vr.csgrid.org/gsplats/soaring_eagle.ply",
-	//"https://ifiweb.informatik.tu-freiberg.de/public/silversplat/.%2Fmodels%2Fwerner_amethyst.ply",
+	"https://vr.csgrid.org/gsplats/soaring_eagle.ply",
+	"https://ifiweb.informatik.tu-freiberg.de/public/silversplat/.%2Fmodels%2Fwerner_amethyst.ply",
 	"https://vr.csgrid.org/gsplats/Merrifield_100.ply",
 	"https://vr.csgrid.org/gsplats/Merrifield_fireplace.ply",
 	"https://vr.csgrid.org/gsplats/mug_space.ply"
@@ -16,11 +16,11 @@ async function convertFile(filename) {
 	console.log(filename);
 	let data = await fetch(filename);
 	const reader_stream = data.body;
-	let jsonObj = await convertPlyToJsonBinary(data);
-	let newfilename = filename.substring(filename.lastIndexOf("/")+1,filename.lastIndexOf("."))+".json";
+	let x3d = await convertPlyToX3d(data);
+	let newfilename = filename.substring(filename.lastIndexOf("/")+1,filename.lastIndexOf("."))+".x3d";
 	console.log(newfilename);
 	const fd = fs.openSync(newfilename, 'w');
-	fs.writeSync(fd, JSON.stringify(jsonObj, null, 0));
+	fs.writeSync(fd, x3d);
 }
 
 async function processFiles(filenames) {
