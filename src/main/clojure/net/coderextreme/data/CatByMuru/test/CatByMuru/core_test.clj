55317cc8cc83a68a8a6dcf9a641c7e8875d5e6d2
John W Carlson <yottzumm@gmail.com>
2026-01-14 07:13:08 -0600

    Checked in clojure code`

---
+++ src/main/clojure/net/coderextreme/data/CatByMuru/test/CatByMuru/core_test.clj
@@ -0,0 +1,7 @@
+(ns CatByMuru.core-test
+  (:require [clojure.test :refer :all]
+            [CatByMuru.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
---
+++ src/main/clojure/net/coderextreme/data/HAnimPoseExample/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to HAnimPoseExample
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
