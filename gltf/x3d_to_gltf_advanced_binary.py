f53b47892d217ec2cbd907f40535e8c0b9b607fd
John Carlson <yottzumm@gmail.com>
2026-03-30 22:26:33 -0500

    Added two examples

--- gltf/x3d_to_gltf_advanced_binary.py
+++ gltf/x3d_to_gltf_advanced_binary.py
@@ -471,3 +471,5 @@ if __name__ == "__main__":
         convert_x3d_to_glb(f"{base}/LaughingSkeleton.x3d",               "LaughingSkeleton.glb")
         convert_x3d_to_glb(f"{base}/AnimatedAssembledHumanSkeleton.x3d", "AnimatedAssembledHumanSkeleton.glb")
         convert_x3d_to_glb(f"../../medicalbones/0scaled/0skeleton1AImapped.x3d", "0skeleton1.glb")
+        convert_x3d_to_glb(f"HumanoidComplete.x3d", "HumanoidComplete.glb")
+        convert_x3d_to_glb(f"JoeKickAnimation.x3d", "JoeKickAnimation.glb")
